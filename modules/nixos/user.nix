8ce743a3ed156c0b7e29597294178fa2ce2225cf
Ludovico Piero <ludovicopiero@pm.me>
2022-11-18 02:38:52 +1000

    enable gnome polkit

--- modules/nixos/user.nix
+++ modules/nixos/user.nix
@@ -3,10 +3,40 @@
   users.users.ludovico = {
     isNormalUser = true;
     description = "ludovico";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      firefox
-    ];
+    extraGroups = [ "networkmanager" "seatd" "wheel" ];
     shell = pkgs.fish;
+  };
+
+  # Enable Gnome Polkit
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wants = [ "graphical-session.target" ];
+      wantedBy = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart =
+          "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+
+    # Enable Seatd here
+    services = {
+      seatd = {
+        enable = true;
+        description = "Seat management daemon";
+        script = "${pkgs.seatd}/bin/seatd -g wheel";
+        serviceConfig = {
+          Type = "simple";
+          Restart = "always";
+          RestartSec = "1";
+        };
+        wantedBy = [ "multi-user.target" ];
+      };
+    };
   };
 }
