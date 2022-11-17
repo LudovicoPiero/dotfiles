2494d5a83194736a30cc1f2afd6ff571e51f667d
Ludovico Piero <ludovicopiero@pm.me>
2022-11-17 20:54:15 +1000

    Revert "remove 2 unused packages"
    This reverts commit d0a04750e3998a43a8adf2bacb7e59ac6834ea60.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -108,6 +108,8 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    cmake
+    pixman
   ];
 
   networking.hostName = "uwunix";
