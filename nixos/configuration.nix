d0a04750e3998a43a8adf2bacb7e59ac6834ea60
Ludovico Piero <ludovicopiero@pm.me>
2022-11-17 19:06:39 +1000

    remove 2 unused packages

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -108,8 +108,6 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-    cmake
-    pixman
   ];
 
   networking.hostName = "uwunix";
