57f69e8019d48bea2c987251a32e485c8278be30
Ludovico Piero <ludovicopiero@pm.me>
2022-11-18 20:50:53 +1000

    add git config

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -1,6 +1,18 @@
+{pkgs,...}:
 {
   programs.git = {
     userEmail = "ludovicopiero@pm.me";
     userName = "Ludovico Piero";
+    signing = {
+      key = "267C8AC8AB72BD95";
+      signByDefault = true;
+    };
+    extraConfig = {
+      init = { defaultBranch = "main"; };
+      core = { excludesfile = "$NIXOS_CONFIG_DIR/scripts/gitignore"; };
+      pull.rebase = false;
+      credential.helper = "${pkgs.git.override { withLibsecret = true; }
+        }/bin/git-credential-libsecret";
+    };
   };
 }
