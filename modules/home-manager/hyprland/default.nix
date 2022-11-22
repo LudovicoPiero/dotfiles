71cc63c8cc094459879b4309181dab073cbae46d
Ludovico Piero <ludovicopiero@pm.me>
2022-11-23 01:11:20 +1000

    nope

--- modules/home-manager/hyprland/default.nix
+++ modules/home-manager/hyprland/default.nix
@@ -26,43 +26,10 @@
     #hyprland (using flakes version)
   ];
 
-  # screen idle
-  # https://github.com/fufexan/dotfiles/blob/main/home/wayland/hyprland/default.nix
-  # services.swayidle = {
-  #   enable = true;
-  #   events = [
-  #     {
-  #       event = "before-sleep";
-  #       command = "${pkgs.swaylock-effects}/bin/swaylock -fF";
-  #     }
-  #     {
-  #       event = "lock";
-  #       command = "${pkgs.swaylock-effects}/bin/swaylock -fF";
-  #     }
-  #   ];
-  #   timeouts = [
-  #     {
-  #       timeout = 300;
-  #       command = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl dispatch dpms off";
-  #       resumeCommand = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl dispatch dpms on";
-  #     }
-  #     {
-  #       timeout = 310;
-  #       command = "${pkgs.systemd}/bin/loginctl lock-session";
-  #     }
-  #   ];
-  # };
-
-  # start swayidle as part of hyprland, not sway
-  # systemd.user.services.swayidle.Install.WantedBy = lib.mkForce [ "hyprland-session.target" ];
-
   home.sessionVariables = {
     NIXOS_OZONE_WL = "1";
     MOZ_ENABLE_WAYLAND = "1";
   };
-
-  # Enable Wayland
-  # wayland.windowManager.hyprland.enable = true;
 
   home.file = {
     hyprland = {
