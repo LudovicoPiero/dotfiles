b1d9206f7baa45c779b68d9ccdf44e7c88bad5cc
Ludovico Piero <ludovicopiero@pm.me>
2022-11-25 22:16:18 +1000

    modules/nixos: add tlp services

--- modules/nixos/system/desktop.nix
+++ modules/nixos/system/desktop.nix
@@ -13,6 +13,20 @@
     dbus.packages = [pkgs.gcr];
     udev.packages = with pkgs; [gnome.gnome-settings-daemon];
 
+    # TLP For Laptop
+    tlp.enable = true;
+    tlp.settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      # https://linrunner.de/en/tlp/docs/tlp-faq.html#battery
+      # use "tlp fullcharge" to override temporarily
+      START_CHARGE_THRESH_BAT0 = 85;
+      STOP_CHARGE_THRESH_BAT0 = 90;
+      START_CHARGE_THRESH_BAT1 = 85;
+      STOP_CHARGE_THRESH_BAT1 = 90;
+    };
+
     gnome.gnome-keyring.enable = true; # Keyring daemon
 
     xserver = {
