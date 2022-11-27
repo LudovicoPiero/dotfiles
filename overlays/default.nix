1921a6428728be1d577a174ba3ad11453dee7f3b
Ludovico Piero <ludovicopiero@pm.me>
2022-11-27 20:36:36 +1000

    fix waybar click not working

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,6 +9,9 @@
   modifications = final: prev: {
     waybar = prev.waybar.overrideAttrs (oldAttrs: {
       mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
+      patchPhase = ''
+        substituteInPlace src/modules/wlr/workspace_manager.cpp --replace "zext_workspace_handle_v1_activate(workspace_handle_);" "const std::string command = \"hyprctl dispatch workspace \" + name_; system(command.c_str());"
+      '';
     });
     discord-canary = prev.discord-canary.override {
       withOpenASAR = true;
