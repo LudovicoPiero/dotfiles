1696c54e05d447f49f5ba389b12ddf45013edede
Ludovico Piero <ludovicopiero@pm.me>
2022-11-28 01:43:58 +1000

    Hopefully this fixed my eww config

--- modules/home-manager/hyprland/default.nix
+++ modules/home-manager/hyprland/default.nix
@@ -41,8 +41,8 @@
   wayland.windowManager.hyprland = {
     enable = true;
     extraConfig = ''
-         #        name,resolution,offset,scale
-         monitor = eDP-1, 1366x768@60 , 0x0 , 1
+         #        name  , resolution  ,offset , scale
+         monitor = eDP-1, 1366x768@60 , 0x0   , 1
 
          $HYPR_FOLDER = /home/$(whoami)/.config/hypr
 
@@ -198,12 +198,22 @@
          bind = SUPER , 3 , workspace , 3
          bind = SUPER , 4 , workspace , 4
          bind = SUPER , 5 , workspace , 5
+         bind = SUPER , 6 , workspace , 6
+         bind = SUPER , 7 , workspace , 7
+         bind = SUPER , 8 , workspace , 8
+         bind = SUPER , 9 , workspace , 9
+         bind = SUPER , 0 , workspace , 10
 
          bind = SUPERSHIFT , 1 , movetoworkspacesilent , 1
          bind = SUPERSHIFT , 2 , movetoworkspacesilent , 2
          bind = SUPERSHIFT , 3 , movetoworkspacesilent , 3
          bind = SUPERSHIFT , 4 , movetoworkspacesilent , 4
          bind = SUPERSHIFT , 5 , movetoworkspacesilent , 5
+         bind = SUPERSHIFT , 6 , movetoworkspacesilent , 6
+         bind = SUPERSHIFT , 7 , movetoworkspacesilent , 7
+         bind = SUPERSHIFT , 8 , movetoworkspacesilent , 8
+         bind = SUPERSHIFT , 9 , movetoworkspacesilent , 9
+         bind = SUPERSHIFT , 0 , movetoworkspacesilent , 10
 
          # Media Keys
          bind = , XF86AudioNext , exec , playerctl next
@@ -218,7 +228,7 @@
          bind = , XF86MonBrightnessUp , exec , brightnessctl set +5%
          bind = , XF86MonBrightnessDown , exec , brightnessctl set 5%-
 
-        exec-once = eww open bar
+        exec-once = systemctl --user start eww; eww open bar
          exec-once = ${pkgs.mako}/bin/mako
          exec-once = ${pkgs.hyprpaper}/bin/hyprpaper
       #    exec-once = ${pkgs.waybar}/bin/waybar
