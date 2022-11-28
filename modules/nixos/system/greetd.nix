537c186b1e16b1433b106af52500e7c6ac4bea5c
Ludovico Piero <ludovicopiero@pm.me>
2022-11-29 06:13:25 +1000

    update greetd

--- modules/nixos/system/greetd.nix
+++ modules/nixos/system/greetd.nix
@@ -9,14 +9,11 @@
         command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd Hyprland";
         user = "ludovico";
       };
-      #   initial_session = {
-      #     command = "Hyprland";
-      #     user = "ludovico";
-      #   };
     };
   };
 
   environment.etc."greetd/environments".text = ''
     Hyprland
+    wayfire
   '';
 }
