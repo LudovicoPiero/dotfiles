9fbe02f878900cca251a6529f3974b35bf551635
Ludovico Piero <ludovicopiero@pm.me>
2022-11-29 02:28:56 +1000

    change dunst settings

--- modules/home-manager/apps/dunst/default.nix
+++ modules/home-manager/apps/dunst/default.nix
@@ -18,12 +18,26 @@
         offset = "30x50";
         origin = "top-right";
         transparency = 10;
-        frame_color = "#1E1E2E";
+        background = "#1E1E2E";
+        foreground = "#CDD6F4";
+        frame_color = "#89B4FA"; # border
         font = "JetBrains Mono Nerd Font 10";
         notification_limit = 5;
         layer = "overlay"; # bottom, top or overlay
         browser = "${pkgs.xdg-utils}/bin/xdg-open";
         corner_radius = 6;
+      };
+      urgency_low = {
+        frame_color = "#2D3039";
+        timeout = 3;
+      };
+      urgency_normal = {
+        frame_color = "#89DCEB";
+        timeout = 5;
+      };
+      urgency_critical = {
+        frame_color = "#FAB387";
+        timeout = 8;
       };
     };
   };
