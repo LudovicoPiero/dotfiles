a9a4a9887dd0118c9e1c68e6142b01bc8561dbbe
Ludovico Piero <ludovicopiero@pm.me>
2022-12-01 04:52:45 +1000

    HELPPPP

--- pkgs/google-sans.nix
+++ pkgs/google-sans.nix
@@ -1,26 +1,20 @@
 {
   pkgs,
-  stdenv,
   lib,
   fetchFromGitHub,
   ...
 }:
-stdenv.mkDerivation {
-  name = "ttf-google-sans";
-  version = "master";
+fetchFromGitHub {
+  pname = "Google-Sans-Fonts";
+  owner = "sahibjotsaggu";
+  repo = "Google-Sans-Fonts";
+  rev = "b1826355d8212378e5fd6094bbe504268fa6f85d";
+  fetchSubmodules = false;
+  sha256 = "KJsLM0NkhxGtJ2GGTzIUjh3lWIdQFZQoD5c3AG2ApTg=";
 
-  src = fetchFromGitHub {
-    owner = "sahibjotsaggu";
-    repo = "Google-Sans-Fonts";
-    rev = "b1826355d8212378e5fd6094bbe504268fa6f85d";
-    fetchSubmodules = false;
-    sha256 = "KJsLM0NkhxGtJ2GGTzIUjh3lWIdQFZQoD5c3AG2ApTg=";
-  };
-
-  phases = ["installPhase"];
-
-  installPhase = ''
+  postFetch = ''
     install -Dm644 $src/GoogleSans-*.ttf -t $out/share/fonts/ttf
+    echo "Installing fonts to $out/share/fonts/ttf"
   '';
 
   meta = with lib; {
