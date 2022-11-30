9ac2c4a80489ae943c27555e97b53451833344d5
Ludovico Piero <ludovicopiero@pm.me>
2022-12-01 09:10:28 +1000

    disable ssh

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -142,7 +142,7 @@
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
   services.openssh = {
-    enable = true;
+    enable = false;
     # Forbid root login through SSH.
     permitRootLogin = "no";
     # Use keys only. Remove if you want to SSH using password (not recommended)
