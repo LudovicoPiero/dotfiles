e53bf998524686a632654e680de29cd60c3f73b7
Ludovico Piero <ludovicopiero@pm.me>
2022-12-03 00:36:26 +1000

    fix: disable discord wayland only
    Sometime, discord is not even showing on screen

--- modules/home-manager/wayland/hyprland/config.nix
+++ modules/home-manager/wayland/hyprland/config.nix
@@ -113,7 +113,7 @@ in {
        windowrulev2 = rounding 0 , class:^(Code)$ # VSCODE
 
        # Variables
-       $discord = discordcanary  --enable-features=UseOzonePlatform --ozone-platform=wayland --ignore-gpu-blocklist --enable-features=VaapiVideoDecoder --use-gl=desktop --enable-gpu-rasterization --enable-zero-copy
+       $discord = discordcanary  --disable-features=UseOzonePlatform --ignore-gpu-blocklist --enable-features=VaapiVideoDecoder --use-gl=desktop --enable-gpu-rasterization --enable-zero-copy
        $menu = $HYPR_FOLDER/scripts/bemenu
     	 $powermenu = $HYPR_FOLDER/scripts/powermenu
 
