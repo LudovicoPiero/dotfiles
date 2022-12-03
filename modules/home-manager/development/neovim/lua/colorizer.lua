75bc20b35ab44cb8e5d197ce46884f379d750a36
Ludovico Piero <ludovicopiero@pm.me>
2022-12-04 05:51:17 +1000

    feat(nvim): update colorizer config

--- modules/home-manager/development/neovim/lua/colorizer.lua
+++ modules/home-manager/development/neovim/lua/colorizer.lua
@@ -1,7 +1,12 @@
-require("colorizer").setup {
-	"css",
-	"javascript",
-	html = {
-		mode = "foreground"
-	}
+require "colorizer".setup {
+	RGB = true, -- #RGB hex codes
+	RRGGBB = true, -- #RRGGBB hex codes
+	names = true, -- "Name" codes like Blue
+	RRGGBBAA = false, -- #RRGGBBAA hex codes
+	rgb_fn = false, -- CSS rgb() and rgba() functions
+	hsl_fn = false, -- CSS hsl() and hsla() functions
+	css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+	css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+	-- Available modes: foreground, background
+	mode = "background" -- Set the display mode.
 }
