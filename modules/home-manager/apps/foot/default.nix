36ba2f9309e9a518a6a5921b7d120c2f1aeae802
Ludovico Piero <ludovicopiero@pm.me>
2022-12-03 21:32:32 +1000

    feat(foot): change foot color to the nix way

--- modules/home-manager/apps/foot/default.nix
+++ modules/home-manager/apps/foot/default.nix
@@ -1,8 +1,11 @@
 {
   pkgs,
   lib,
+  config,
   ...
-}: {
+}: let
+  inherit (config.colorScheme) colors;
+in {
   programs.foot = {
     enable = true;
     settings = {
@@ -26,24 +29,24 @@
       };
       colors = {
         alpha = 0.77;
-        foreground = "cdd6f4"; # Text
-        background = "1e1e2e"; # Base
-        regular0 = "45475a"; # Surface 1
-        regular1 = "f38ba8"; # red
-        regular2 = "a6e3a1"; # green
-        regular3 = "f9e2af"; # yellow
-        regular4 = "89b4fa"; # blue
-        regular5 = "f5c2e7"; # pink
-        regular6 = "94e2d5"; # teal
-        regular7 = "bac2de"; # Subtext 1
-        bright0 = "585b70"; # Surface 2
-        bright1 = "f38ba8"; # red
-        bright2 = "a6e3a1"; # green
-        bright3 = "f9e2af"; # yellow
-        bright4 = "89b4fa"; # blue
-        bright5 = "f5c2e7"; # pink
-        bright6 = "94e2d5"; # teal
-        bright7 = "a6adc8"; # Subtext 0
+        background = "${colors.base00}";
+        foreground = "${colors.base05}";
+        regular0 = "${colors.base01}";
+        regular1 = "${colors.base08}";
+        regular2 = "${colors.base0B}";
+        regular3 = "${colors.base0A}";
+        regular4 = "${colors.base0D}";
+        regular5 = "${colors.base0E}";
+        regular6 = "${colors.base0C}";
+        regular7 = "${colors.base06}";
+        bright0 = "${colors.base02}";
+        bright1 = "${colors.base08}";
+        bright2 = "${colors.base0B}";
+        bright3 = "${colors.base0A}";
+        bright4 = "${colors.base0D}";
+        bright5 = "${colors.base0E}";
+        bright6 = "${colors.base0C}";
+        bright7 = "${colors.base07}";
       };
     };
   };
