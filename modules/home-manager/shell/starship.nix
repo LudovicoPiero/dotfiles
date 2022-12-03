2fdc9834279d05cec14db031722e39dcd93400e2
Ludovico Piero <ludovicopiero@pm.me>
2022-12-03 21:53:25 +1000

    feat(starship): add pure prompt

--- modules/home-manager/shell/starship.nix
+++ modules/home-manager/shell/starship.nix
@@ -5,5 +5,58 @@
 }: {
   programs.starship = {
     enable = true;
+    enableIonIntegration = false;
+    enableNushellIntegration = false;
+
+    # Pure Presets
+    settings = {
+      add_newline = false;
+      format = lib.concatStrings [
+        "$username"
+        "$hostname"
+        "$directory"
+        "$git_branch"
+        "$git_state"
+        "$git_status"
+        "$cmd_duration"
+        "$line_break"
+        "$python"
+        "$character"
+      ];
+      directory = {
+        style = "blue";
+      };
+      character = {
+        success_symbol = "[❯](purple)";
+        error_symbol = "[❯](red)";
+      };
+      git_branch = {
+        style = "bright-black";
+        format = "[$branch]($style)";
+      };
+      git_status = {
+        format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
+        style = "cyan";
+        conflicted = "​";
+        untracked = "​";
+        modified = "​";
+        staged = "​";
+        renamed = "​";
+        deleted = "​";
+        stashed = "≡";
+      };
+      git_state = {
+        format = "\([$state( $progress_current/$progress_total)]($style)\)";
+        style = "bright-black";
+      };
+      cmd_duration = {
+        format = "[$duration]($style) ";
+        style = "yellow";
+      };
+      python = {
+        format = "[$virtualenv]($style) ";
+        style = "bright-black";
+      };
+    };
   };
 }
