3150aee8b3af78cff08cad857c4a321b73e8c2bb
Ludovico Piero <ludovicopiero@pm.me>
2022-12-06 19:55:23 +1000

    feat(webcord): disable theme

--- modules/home-manager/apps/webcord/default.nix
+++ modules/home-manager/apps/webcord/default.nix
@@ -3,22 +3,21 @@
   lib,
   inputs,
   ...
-}: {
+}: let
+  catppuccin = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "discord";
+    rev = "714c2a2726012e806d93d5f9d978396fdf89f6b0";
+    sha256 = "sha256-cWpog52Ft4hqGh8sMWhiLUQp/XXipOPnSTG6LwUAGGA=";
+  };
+  CatpuccinMocha = "${catppuccin}/themes/mocha.theme.css";
+in {
   imports = [
     inputs.webcord.homeManagerModules.default
   ];
 
   programs.webcord = {
     enable = true;
-    themes = let
-      catppuccin = pkgs.fetchFromGitHub {
-        owner = "catppuccin";
-        repo = "discord";
-        rev = "714c2a2726012e806d93d5f9d978396fdf89f6b0";
-        sha256 = "sha256-cWpog52Ft4hqGh8sMWhiLUQp/XXipOPnSTG6LwUAGGA=";
-      };
-    in {
-      CatpuccinMocha = "${catppuccin}/themes/mocha.theme.css";
-    };
+    # themes = "${CatpuccinMocha}";
   };
 }
