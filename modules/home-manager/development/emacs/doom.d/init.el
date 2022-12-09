ae76cc79f28d833eb50a5248dd23cf38af3afd4e
Ludovico Piero <ludovicopiero@pm.me>
2022-12-09 21:15:47 +1000

    feat(emacs): add japanese input and enable lsp

--- modules/home-manager/development/emacs/doom.d/init.el
+++ modules/home-manager/development/emacs/doom.d/init.el
@@ -16,7 +16,7 @@
 
 (doom! :input
        ;;chinese
-       ;;japanese
+       japanese
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
@@ -94,7 +94,7 @@
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
-       ;;lsp               ; M-x vscode
+       lsp               ; M-x vscode
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
