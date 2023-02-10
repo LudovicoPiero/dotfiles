ee7bcb6b958d3e469b1ef840de0b139b1da3e14b
Ludovico <ludovicopiero@pm.me>
2023-02-11 02:31:35 +1000

    profiles/cachix: add hyprland cachix

---
+++ profiles/cachix/hyprland.nix
@@ -0,0 +1,6 @@
+{
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+}
