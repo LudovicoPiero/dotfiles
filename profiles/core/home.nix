90fdc9e1790834ed8bd9f3aa0e04b0ccff631f45
Ludovico <ludovicopiero@pm.me>
2023-02-11 02:38:27 +1000

    profiles/core/home: change sessionVariables

--- profiles/core/home.nix
+++ profiles/core/home.nix
@@ -2,18 +2,23 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  browser = "firefox";
+  tmpdir = "/tmp";
+  emacs-server = "${tmpdir}/emacs-emacs/server";
+  emacsclient = "emacsclient -s ${emacs-server}";
+in {
   home-manager.users."${config.vars.username}" = {
     home = {
       username = "${config.vars.username}";
       homeDirectory = "${config.vars.home}";
       inherit (config.system) stateVersion;
       sessionVariables = {
-        EDITOR = "nvim";
-        VISUAL = "nvim";
+        EDITOR = "${emacsclient}";
+        EMACS_SERVER_FILE = "${emacs-server}";
         PAGER = "less -R";
         TERM = "${config.vars.terminal}";
-        BROWSER = "firefox";
+        BROWSER = "${browser}";
       };
       packages = with pkgs; [
         neofetch
