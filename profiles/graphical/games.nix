07f529ef5794d03581e218a248b97dee5878aef6
Ludovico <ludovicopiero@pm.me>
2023-02-24 06:37:32 +1000

    refactor(games): move steam and gamemode to nixos options

--- profiles/graphical/games.nix
+++ profiles/graphical/games.nix
@@ -4,15 +4,23 @@
   inputs,
   ...
 }: {
+  programs.steam.enable = true;
+  programs.gamemode = {
+    enable = true;
+    settings = {
+      custom = {
+        start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+        end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+      };
+    };
+  };
   home-manager.users.${config.vars.username} = {
-    home.packages = with inputs.nix-gaming.packages.${pkgs.system};
+    home.packages =
       [
-        roblox-player
+        inputs.nix-gaming.packages.${pkgs.system}.roblox-player
       ]
       ++ (with pkgs; [
         gamescope
-        gamemode
-        steam
         inputs.self.packages.${pkgs.system}.TLauncher
       ]);
   };
