dc9a97d9b03009f9d180fc381ef058a2117fc0f4
Ludovico <ludovicopiero@pm.me>
2023-02-25 04:16:06 +1000

    feat(fish): remove unused settings

--- profiles/core/fish.nix
+++ profiles/core/fish.nix
@@ -14,7 +14,6 @@ in {
         gitignore = "curl -sL https://www.gitignore.io/api/$argv";
         fish_greeting = ""; # disable welcome text
         run = "nix run nixpkgs#$argv";
-        gadd = "git add $argv";
       };
       interactiveShellInit = with pkgs; ''
         ${_ starship} init fish | source
@@ -49,35 +48,7 @@ in {
         "..." = "cd ../..";
         ".." = "cd ..";
       };
-      plugins = [
-        {
-          name = "z";
-          src = pkgs.fetchFromGitHub {
-            owner = "jethrokuan";
-            repo = "z";
-            rev = "85f863f20f24faf675827fb00f3a4e15c7838d76";
-            sha256 = "1kaa0k9d535jnvy8vnyxd869jgs0ky6yg55ac1mxcxm8n0rh2mgq";
-          };
-        }
-        {
-          name = "fzf";
-          src = pkgs.fetchFromGitHub {
-            owner = "PatrickF1";
-            repo = "fzf.fish";
-            rev = "096dc8fff16cfbf54333fb7a9910758e818e239d";
-            sha256 = "183z8f7y8629nc78bc3gm5xgwyn813qvjrws4bx8vda2jchxzlb5";
-          };
-        }
-        #{
-        #  name = "pure";
-        #  src = pkgs.fetchFromGitHub {
-        #    owner = "rafaelrinaldi";
-        #    repo = "pure";
-        #    rev = "8c1f69d7f499469979cbecc7b7eaefb97cd6f509";
-        #    sha256 = "1bnp6124cgf4zb1wngb671d7lc4sapizk9jnaa8mdk594z0xzvf9";
-        #  };
-        #}
-      ];
+      plugins = []; #TODO: add plugins
     };
     programs.man.generateCaches = true; # For fish completions
   };
