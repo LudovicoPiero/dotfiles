332d901b512dd3f7818ce24bdde88c95a0312c2e
Ludovico <ludovicopiero@pm.me>
2023-03-02 16:13:22 +1000

    feat(sway): use waybar

--- profiles/windowManager/sway/bars.nix
+++ profiles/windowManager/sway/bars.nix
@@ -1,11 +1,13 @@
 {
   pkgs,
   colors,
+  lib,
+  inputs,
 }: [
   {
-    statusCommand = "i3status-rs ~/.config/i3status-rust/config-bottom.toml";
+    #statusCommand = "i3status-rs ~/.config/i3status-rust/config-bottom.toml";
     # statusCommand = "${pkgs.i3status}/bin/i3status";
-    command = "${pkgs.sway}/bin/swaybar";
+    command = "waybar";
     fonts = {
       names = ["Google Sans" "Font Awesome 6 Free" "Font Awesome 6 Brands"];
       size = 9.0;
