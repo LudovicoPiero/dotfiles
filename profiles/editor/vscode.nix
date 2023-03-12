096b64ea384ddae7e1bce5a9758c60968548632a
Ludovico <ludovicopiero@pm.me>
2023-03-13 00:12:34 +1000

    add vscode
    Signed-off-by: Ludovico <ludovicopiero@pm.me>

---
+++ profiles/editor/vscode.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}: {
+  home-manager.users.${config.vars.username} = {
+    home.packages = [
+      inputs.nil.packages.${pkgs.system}.default
+    ];
+    programs.vscode = {
+      enable = true;
+      # package = pkgs.vscodium; # use vscode because copilot no worky :(
+      extensions = with pkgs.vscode-extensions; [
+        ms-vscode.cpptools # C/C++
+        bbenoist.nix
+        kamikillerto.vscode-colorize # css background
+        catppuccin.catppuccin-vsc # Color theme
+        github.copilot
+        kamadorueda.alejandra
+        pkief.material-icon-theme # Icons theme
+        esbenp.prettier-vscode
+        eamodio.gitlens
+      ];
+    };
+  };
+}
