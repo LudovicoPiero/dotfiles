64a8d6112ab4bbda39e6841e56cbe3404a5d4d4d
Ludovico <ludovicopiero@pm.me>
2023-03-16 05:26:13 +1000

    Revert "fish: make git always sign-off"
    This reverts commit f3ea3b2d2a864746f57eca8ad67d64add936f819.

--- profiles/core/fish.nix
+++ profiles/core/fish.nix
@@ -39,7 +39,7 @@ in {
         "tree" = "exa --tree --icons";
         "nv" = "nvim";
         "mkdir" = "mkdir -p";
-        "g" = "git -s";
+        "g" = "git";
         "gcl" = "git clone";
         "gcm" = "cz c";
         "gd" = "git diff HEAD";
