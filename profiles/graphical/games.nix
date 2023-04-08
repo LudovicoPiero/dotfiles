9a4dc20fab56106ac04e97ad8c547b7f8371567f
Ludovico <ludovicopiero@pm.me>
2023-04-09 07:38:14 +1000

    graphical/games: remove settings for gamemode

--- profiles/graphical/games.nix
+++ profiles/graphical/games.nix
@@ -8,11 +8,6 @@
   programs.gamemode = {
     enable = true;
     settings = {
-      gpu = {
-        apply_gpu_optimisations = "accept-responsibility";
-        gpu_device = 0;
-        amd_performance_level = "high";
-      };
       custom = {
         start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
         end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
