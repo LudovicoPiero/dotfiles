5cd7f02d08010d9d86ab597aab4472dabf5f714d
Ludovico Piero <ludovicopiero@pm.me>
2023-05-01 04:49:19 +1000

    shared: add xdg mimeapps

--- users/shared/home.nix
+++ users/shared/home.nix
@@ -179,4 +179,39 @@
       Install.WantedBy = ["multi-user.target"];
     };
   };
+
+  xdg = let
+    browser = ["chromium-browser.desktop"];
+
+    # XDG MIME types
+    associations = {
+      "application/x-extension-htm" = browser;
+      "application/x-extension-html" = browser;
+      "application/x-extension-shtml" = browser;
+      "application/x-extension-xht" = browser;
+      "application/x-extension-xhtml" = browser;
+      "application/xhtml+xml" = browser;
+      "text/html" = browser;
+      "x-scheme-handler/about" = browser;
+      "x-scheme-handler/chrome" = ["chromium-browser.desktop"];
+      "x-scheme-handler/ftp" = browser;
+      "x-scheme-handler/http" = browser;
+      "x-scheme-handler/https" = browser;
+      "x-scheme-handler/unknown" = browser;
+
+      "audio/*" = ["mpv.desktop"];
+      "video/*" = ["mpv.dekstop"];
+      "image/*" = ["imv.desktop"];
+      "application/json" = browser;
+      "application/pdf" = ["org.pwmt.zathura.desktop.desktop"];
+      "x-scheme-handler/discord" = ["discordcanary.desktop"];
+      "x-scheme-handler/spotify" = ["spotify.desktop"];
+      "x-scheme-handler/tg" = ["telegramdesktop.desktop"];
+    };
+  in {
+    mimeApps = {
+      enable = true;
+      defaultApplications = associations;
+    };
+  };
 }
