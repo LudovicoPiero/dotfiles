8279317f5db251bb646e830b6b6da93297f8b24c
Ludovico Piero <ludovicopiero@pm.me>
2023-05-04 04:54:50 +1000

    waybar: use colors

--- users/ludovico/config/waybar/style.nix
+++ users/ludovico/config/waybar/style.nix
@@ -1,98 +1,94 @@
 {colorscheme}:
-#TODO: use colorscheme
 with colorscheme.colors; ''
   * {
-       border: none;
-       border-radius: 0;
-       font-family: Iosevka Nerd Font;
-       font-size: 14px;
-       min-height: 0;
-   }
+    border: none;
+    border-radius: 0;
+    font-family: Iosevka Nerd Font;
+    font-size: 14px;
+    min-height: 0;
+  }
 
-   window#waybar {
-       background: transparent;
-       color: white;
-   }
+  window#waybar {
+    background: transparent;
+    color: #${base05};
+  }
 
-   #workspaces {
-   	background-color: #24283b;
-   	margin: 5px;
-   	margin-left: 10px;
-   	border-radius: 5px;
-   }
-   #workspaces button {
-       padding: 5px 10px;
-       color: #c0caf5;
-   }
+  #workspaces {
+    background-color: #${base00};
+    margin: 5px;
+    margin-left: 10px;
+    border-radius: 5px;
+  }
 
-   #workspaces button.active {
-       color: #24283b;
-       background-color: #7aa2f7;
-       border-radius: 5px;
-   }
+  #workspaces button {
+    padding: 5px 10px;
+    color: #${base05};
+  }
 
-   #workspaces button:hover {
-   	color: #7dcfff;
-   	border-radius: 5px;
-   }
+  #workspaces button.active {
+    color: #${base00};
+    background-color: #${base0C};
+    border-radius: 5px;
+  }
 
-   #tray {
-       color: #24283b;
-       border-radius: 5px;
-   }
+  #workspaces button:hover {
+    color: #${base0C};
+    border-radius: 5px;
+  }
 
-   #custom-vpn {
-       color: #7dcfff;
-       border-radius: 5px;
-   	   border-radius: 5px 0px 0px 5px;
-   }
+  #tray {
+    color: #${base00};
+    border-radius: 5px;
+  }
 
-   #tray, #custom-date, #custom-vpn, #clock, #battery, #pulseaudio, #network {
-   	background-color: #24283b;
-   	padding: 5px 10px;
-   	margin: 5px 0px;
-   }
+  #custom-vpn {
+    color: #${base06};
+    border-radius: 5px;
+    border-radius: 5px 0px 0px 5px;
+  }
 
-   #custom-date {
-   	color: #7dcfff;
-   }
+  #tray,
+  #custom-date,
+  #custom-vpn,
+  #clock,
+  #battery,
+  #pulseaudio,
+  #network {
+    background-color: #${base00};
+    padding: 5px 10px;
+    margin: 5px 0px;
+  }
 
-   #custom-power {
-   	color: #24283b;
-   	background-color: #db4b4b;
-   	border-radius: 5px;
-   	margin-right: 10px;
-   	margin-top: 5px;
-   	margin-bottom: 5px;
-   	margin-left: 0px;
-   	padding: 5px 10px;
-   }
+  #custom-date {
+    color: #${base0E};
+  }
 
-   #clock {
-       color: #b48ead;
-       border-radius: 0px 5px 5px 0px;
-       margin-right: 10px;
-   }
+  #clock {
+    color: #${base0F};
+    border-radius: 0px 5px 5px 0px;
+    margin-right: 10px;
+  }
 
-   #battery {
-       color: #9ece6a;
-   }
+  #battery {
+    color: #${base0D};
+  }
 
-   #battery.charging {
-       color: #9ece6a;
-   }
+  #battery.charging {
+    color: #${base0B};
+  }
 
-   #battery.warning:not(.charging) {
-       background-color: #f7768e;
-       color: #24283b;
-       border-radius: 5px 5px 5px 5px;
-   }
+  #battery.warning:not(.charging) {
+    background-color: #${base08};
+    color: #${base00};
+    border-radius: 5px 5px 5px 5px;
+  }
 
-   #network {
-   	color: #f7768e;
-   }
+  #network {
+    color: #${base0B};
+  }
 
-   #pulseaudio {
-   	color: #e0af68;
-   }
+  #pulseaudio {
+    color: #${base0A};
+  }
+
 ''
