97bb908a3cc315897f97af3590351645e4abce00
Ludovico Piero <ludovicopiero@pm.me>
2023-05-06 15:41:40 +1000

    host/shared: add seatd

--- hosts/shared/configuration.nix
+++ hosts/shared/configuration.nix
@@ -192,6 +192,17 @@
 
   services.gnome.gnome-keyring.enable = true;
   systemd = {
+    services.seatd = {
+      enable = true;
+      description = "Seat Management Daemon";
+      script = "${pkgs.seatd}/bin/seatd -g wheel";
+      serviceConfig = {
+        Type = "simple";
+        Restart = "always";
+        RestartSec = 1;
+      };
+      wantedBy = ["multi-user.target"];
+    };
     user.services = {
       pipewire.wantedBy = ["default.target"];
       pipewire-pulse.wantedBy = ["default.target"];
