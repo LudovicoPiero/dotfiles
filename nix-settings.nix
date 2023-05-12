57c45e9ec6b5ac37167bfef319924682933fc133
Ludovico Piero <ludovicopiero@pm.me>
2023-05-13 06:27:52 +1000

    add garnix to substituters

--- nix-settings.nix
+++ nix-settings.nix
@@ -40,6 +40,7 @@
     # home-manager will attempt to rebuild the world otherwise...
     substituters = [
       "https://cache.nixos.org?priority=7"
+      "https://cache.garnix.io?priority=6"
       "https://nix-community.cachix.org?priority=5"
       "https://ezkea.cachix.org"
       "https://nixpkgs-wayland.cachix.org"
@@ -48,8 +49,9 @@
 
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
