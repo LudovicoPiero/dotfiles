7e0361fecf0421130b5715854b530d2cf50eafc2
Ludovico Piero <ludovicopiero@pm.me>
2023-05-14 03:52:03 +1000

    remove self spotify package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,4 @@
 {pkgs ? (import ../nixpkgs.nix) {}}: {
-  spotify = pkgs.callPackage ./spotify.nix {};
   multicolor-sddm-theme = pkgs.callPackage ./multicolor-sddm-theme.nix {};
   google-sans = pkgs.callPackage ./google-sans.nix {};
   material-symbols = pkgs.callPackage ./material-symbols.nix {};
