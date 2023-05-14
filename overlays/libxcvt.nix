c16f4d7ec49c6b18ff63f8c82e224453079ddf9d
Ludovico Piero <ludovicopiero@pm.me>
2023-05-14 13:45:23 +1000

    change sha for libxcvt

--- overlays/libxcvt.nix
+++ overlays/libxcvt.nix
@@ -3,7 +3,7 @@ self: super: {
     libxcvt = xsuper.libxcvt.overrideAttrs (oldAttrs: {
       src = super.fetchurl {
         url = "mirror://xorg/individual/lib/libxcvt-0.1.2.tar.xz";
-        sha256 = "BWFpBUR5biXPvXGAa6Gw15f/5GTpeWQREj55RQ9x2zg=";
+        sha256 = "0f6vf47lay9y288n8yg9ckjgz5ypn2hnp03ipp7javkr8h2njq85";
       };
     });
   });
