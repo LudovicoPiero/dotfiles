b4dbbdcc9073604260d9c6352d94c4fe62fe2eed
Ludovico Piero <ludovicopiero@pm.me>
2023-05-15 02:58:53 +1000

    rename chromium overlays to ungoogled-chromium

--- overlays/ungoogled-chromium.nix
+++ overlays/ungoogled-chromium.nix
@@ -29,38 +29,26 @@ final: prev: {
       "--enable-oop-rasterization"
       "--enable-zero-copy"
       "--ignore-gpu-blocklist"
-    ];
-  };
-  chromium = prev.chromium.override {
-    commandLineArgs = toString [
-      # Ungoogled features
-      "--disable-search-engine-collection"
-      "--extension-mime-request-handling=always-prompt-for-install"
-      "--fingerprinting-canvas-image-data-noise"
-      "--fingerprinting-canvas-measuretext-noise"
-      "--fingerprinting-client-rects-noise"
-      "--popups-to-tabs"
-      "--show-avatar-button=incognito-and-guest"
 
-      # Experimental features
-      "--enable-features=${
-        final.lib.concatStringsSep "," [
-          "BackForwardCache:enable_same_site/true"
-          "CopyLinkToText"
-          "OverlayScrollbar"
-          "TabHoverCardImages"
-          "VaapiVideoDecoder"
-        ]
-      }"
+      # Wayland
+      "--enable-features=UseOzonePlatform"
+      "--ozone-platform=wayland"
 
-      # Aesthetics
-      "--force-dark-mode"
-
-      # Performance
-      "--enable-gpu-rasterization"
-      "--enable-oop-rasterization"
-      "--enable-zero-copy"
-      "--ignore-gpu-blocklist"
+      # Etc
+      "--disk-cache=$XDG_RUNTIME_DIR/chromium-cache"
+      "--no-default-browser-check"
+      "--no-service-autorun"
+      "--disable-features=PreloadMediaEngagementData,MediaEngagementBypassAutoplayPolicies"
+      "--disable-reading-from-canvas"
+      "--no-pings"
+      "--no-first-run"
+      "--no-experiments"
+      "--no-crash-upload"
+      "--disable-wake-on-wifi"
+      "--disable-breakpad"
+      "--disable-sync"
+      "--disable-speech-api"
+      "--disable-speech-synthesis-api"
     ];
   };
 }
