c2fc6919df848e2868b2490a89c89c5a6dcd6c1b
Ludovico <ludovicopiero@pm.me>
2023-05-24 01:53:36 +1000

    pls work

--- hosts/nostra/configuration.nix
+++ hosts/nostra/configuration.nix
@@ -16,8 +16,21 @@
   sops = {
     defaultSopsFile = ./secrets.yaml;
     age.sshKeyPaths = ["/home/cosa/.ssh/id_rsa"];
-    secrets.cosa.neededForUsers = true;
-    secrets.root.neededForUsers = true;
+    secrets = let
+      default = lib.recursiveUpdate {
+        owner = "wireguard";
+        group = "wireguard";
+        mode = "0640";
+        reloadUnits = ["systemd-networkd.service"];
+      };
+    in {
+      cosa.neededForUsers = true;
+      root.neededForUsers = true;
+      wireguardPrivateKey = default;
+      wireguardPublicKey = default;
+      wireguardPreshared-pc = default;
+      wireguardPreshared-phone = default;
+    };
   };
 
   boot = {
@@ -60,10 +73,6 @@
     };
   };
 
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
   # Enable NAT
   networking.nat = {
     enable = true;
@@ -74,6 +83,50 @@
   # Open ports in the firewall
   networking.firewall = {
     allowedTCPPorts = [53 22];
-    allowedUDPPorts = [53 51820];
+    allowedUDPPorts = [53 52780];
+  };
+
+  # Wireguard Server
+  networking.wg-quick.interfaces = {
+    # "wg0" is the network interface name. You can name the interface arbitrarily.
+    wg0 = {
+      # Determines the IP/IPv6 address and subnet of the client's end of the tunnel interface
+      address = ["10.66.66.1/24" "fdc9:281f:04d7:9ee9::1/64"];
+      # The port that WireGuard listens to - recommended that this be changed from default
+      listenPort = 52780;
+      # Path to the server's private key
+      privateKeyFile = config.sops.secrets.wireguardPrivateKey.path;
+
+      # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
+      postUp = ''
+        ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.0.0.1/24 -o eth0 -j MASQUERADE
+        ${pkgs.iptables}/bin/ip6tables -A FORWARD -i wg0 -j ACCEPT
+        ${pkgs.iptables}/bin/ip6tables -t nat -A POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o eth0 -j MASQUERADE
+      '';
+
+      # Undo the above
+      preDown = ''
+        ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.1/24 -o eth0 -j MASQUERADE
+        ${pkgs.iptables}/bin/ip6tables -D FORWARD -i wg0 -j ACCEPT
+        ${pkgs.iptables}/bin/ip6tables -t nat -D POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o eth0 -j MASQUERADE
+      '';
+
+      peers = [
+        {
+          # pc
+          publicKey = config.sops.secrets.wireguardPublicKey.path;
+          presharedKeyFile = config.sops.secrets.wireguardPreshared-pc.path;
+          allowedIPs = ["10.66.66.2/32" "fdc9:281f:04d7:9ee9::2/128"];
+        }
+        {
+          # phone
+          publicKey = config.sops.secrets.wireguardPublicKey.path;
+          presharedKeyFile = config.sops.secrets.wireguardPreshared-phone.path;
+          allowedIPs = ["10.66.66.3/32" "fdc9:281f:04d7:9ee9::3/128"];
+        }
+      ];
+    };
   };
 }
--- hosts/nostra/hardware-configuration.nix
+++ hosts/nostra/hardware-configuration.nix
@@ -4,8 +4,6 @@
 {
   config,
   lib,
-  pkgs,
-  modulesPath,
   ...
 }: {
   imports = [];
