69897a6681a943038082bbc388ba79bfb35a4d6d
Ludovico <ludovicopiero@pm.me>
2023-05-27 06:35:17 +1000

    forgot to add pkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,6 @@
 {pkgs ? (import ../nixpkgs.nix) {}}: {
   multicolor-sddm-theme = pkgs.callPackage ./multicolor-sddm-theme.nix {};
   google-sans = pkgs.callPackage ./google-sans.nix {};
+  san-francisco-pro = pkgs.callPackage ./san-francisco-pro.nix {};
   material-symbols = pkgs.callPackage ./material-symbols.nix {};
 }
