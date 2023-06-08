066bdbf32433d7cd839a2794145ff8780f83de7f
Ludovico <ludovicopiero@pm.me>
2023-06-09 03:13:53 +1000

    change chromium overlays

--- overlays/ungoogled-chromium.nix
+++ overlays/ungoogled-chromium.nix
@@ -32,32 +32,9 @@ final: prev: {
       "--ignore-gpu-blocklist"
 
       # Wayland
-      /*
-      Disabled because hardware acceleration doesn't work
-      when disabling --use-gl=egl, it's not gonna show any emoji
-      and it's gonna be slow as hell
-
-      "--use-gl=egl"
-      */
-      "--ozone-platform=wayland"
-      "--enable-features=UseOzonePlatform"
-
-      # Etc
-      # "--gtk-version=4"
-      "--disk-cache=$XDG_RUNTIME_DIR/chromium-cache"
-      "--no-default-browser-check"
-      "--no-service-autorun"
-      "--disable-features=PreloadMediaEngagementData,MediaEngagementBypassAutoplayPolicies"
-      "--disable-reading-from-canvas"
-      "--no-pings"
-      "--no-first-run"
-      "--no-experiments"
-      "--no-crash-upload"
-      "--disable-wake-on-wifi"
-      "--disable-breakpad"
-      "--disable-sync"
-      "--disable-speech-api"
-      "--disable-speech-synthesis-api"
+      # "--use-gl=egl"
+      # "--ozone-platform=wayland"
+      # "--enable-features=UseOzonePlatform"
     ];
   };
 }
