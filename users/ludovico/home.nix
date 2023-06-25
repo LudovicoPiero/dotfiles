20d7220856d2db68734d18c2fcba7ec7413d68ae
Ludovico Piero <ludovicopiero@pm.me>
2023-06-25 19:49:59 +1000

    users/ludovico: set QT_QPA_PLATFORMTHEME to gtk3

--- users/ludovico/home.nix
+++ users/ludovico/home.nix
@@ -74,6 +74,7 @@ in {
       # needs qt5.qtwayland in systemPackages
       QT_QPA_PLATFORM = "wayland";
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+      QT_QPA_PLATFORMTHEME = "gtk3";
       # Fix for some Java AWT applications (e.g. Android Studio),
       # use this if they aren't displayed properly:
       _JAVA_AWT_WM_NONREPARENTING = "1";
