e7e7315745b73bce53fd4ab4a7960573add25749
Ludovico Piero <ludovicopiero@pm.me>
2023-06-27 17:41:21 +1000

    nix-settings: remove hyprland & add sforza cachix

--- nix-settings.nix
+++ nix-settings.nix
@@ -53,7 +53,7 @@
       "https://nix-community.cachix.org?priority=5"
       "https://ezkea.cachix.org"
       "https://nixpkgs-wayland.cachix.org"
-      "https://hyprland.cachix.org"
+      "https://sforza.cachix.org"
     ];
 
     trusted-public-keys = [
@@ -62,7 +62,7 @@
       "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "sforza.cachix.org-1:g/Zn9CI6svVm4gTGNQueJMsIPdyDAhh4TQZRwHS+TrU="
     ];
 
     trusted-users = ["root" "@wheel"];
