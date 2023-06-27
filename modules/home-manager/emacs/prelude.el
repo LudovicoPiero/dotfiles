fb260dfe982477f1a76eb06b60d94f0ee337b53b
Ludovico Piero <ludovicopiero@pm.me>
2023-06-28 02:40:05 +1000

    emacs: set backups directory

--- modules/home-manager/emacs/prelude.el
+++ modules/home-manager/emacs/prelude.el
@@ -26,6 +26,13 @@
 
 (fset 'yes-or-no-p 'y-or-n-p)
 
+;; Set Backup Directory
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+      backup-by-copying      t  ; Don't de-link hard links
+      version-control        t  ; Use version numbers on backups
+      delete-old-versions    t  ; Automatically delete excess backups:
+      kept-new-versions      10 ; how many of the newest versions to keep
+      kept-old-versions      5) ; and how many of the old
+
 ;; Long text goes below
 (global-visual-line-mode t)
-
