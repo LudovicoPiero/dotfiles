391c8a38ac8f7c72aa10788b2171f08109a689d8
Ludovico Piero <ludovicopiero@pm.me>
2023-06-28 02:35:21 +1000

    users/ludovico: move vscode settings

---
+++ users/ludovico/config/vscode/settings.nix
@@ -0,0 +1,34 @@
+{
+  "editor.cursorBlinking" = "phase";
+  "editor.cursorSmoothCaretAnimation" = "on";
+  "editor.fontFamily" = "'Iosevka Nerd Font', 'monospace', monospace";
+  "editor.fontSize" = 18;
+  "editor.formatOnSave" = true;
+  "editor.minimap.enabled" = false;
+  "editor.renderWhitespace" = "none";
+  "editor.scrollbar.vertical" = "hidden";
+  "editor.smoothScrolling" = true;
+  "extensions.autoCheckUpdates" = false;
+  "files.autoSave" = "onWindowChange";
+  "gitlens.currentLine.enabled" = false;
+  "gitlens.hovers.currentLine.over" = "line";
+  "nix.enableLanguageServer" = true;
+  "nix.serverPath" = "nil";
+  "nix.serverSettings" = {
+    "formatting" = {
+      "command" = "alejandra";
+    };
+  };
+  "update.mode" = "none";
+  "vscord.status.details.text.editing" = "In {full_directory_name}";
+  "vscord.status.idle.check" = false;
+  "vscord.status.problems.enabled" = false;
+  "vscord.status.state.text.editing" = "Working on {file_name}{file_extension}";
+  "workbench.colorTheme" = "Catppuccin Mocha";
+  "workbench.editor.limit.enabled" = true;
+  "workbench.editor.limit.perEditorGroup" = true;
+  "workbench.editor.limit.value" = 5;
+  "workbench.iconTheme" = "material-icon-theme";
+  "workbench.sideBar.location" = "right";
+  "workbench.startupEditor" = "none";
+}
--- users/ludovico/home.nix
+++ users/ludovico/home.nix
@@ -141,41 +141,7 @@ in {
             sha256 = "1nw3zvlw0bx9yih4z3i20irdw02zz444ncf84xjvjn6h5hw47i3x";
           }
         ];
-      userSettings = {
-        #TODO: move to config folder
-        "editor.cursorBlinking" = "phase";
-        "editor.cursorSmoothCaretAnimation" = "on";
-        "editor.fontFamily" = "'Iosevka Nerd Font', 'monospace', monospace";
-        "editor.fontSize" = 18;
-        "editor.formatOnSave" = true;
-        "editor.minimap.enabled" = false;
-        "editor.renderWhitespace" = "none";
-        "editor.scrollbar.vertical" = "hidden";
-        "editor.smoothScrolling" = true;
-        "extensions.autoCheckUpdates" = false;
-        "files.autoSave" = "onWindowChange";
-        "gitlens.currentLine.enabled" = false;
-        "gitlens.hovers.currentLine.over" = "line";
-        "nix.enableLanguageServer" = true;
-        "nix.serverPath" = "nil";
-        "nix.serverSettings" = {
-          "formatting" = {
-            "command" = "alejandra";
-          };
-        };
-        "update.mode" = "none";
-        "vscord.status.details.text.editing" = "In {full_directory_name}";
-        "vscord.status.idle.check" = false;
-        "vscord.status.problems.enabled" = false;
-        "vscord.status.state.text.editing" = "Working on {file_name}{file_extension}";
-        "workbench.colorTheme" = "Catppuccin Mocha";
-        "workbench.editor.limit.enabled" = true;
-        "workbench.editor.limit.perEditorGroup" = true;
-        "workbench.editor.limit.value" = 5;
-        "workbench.iconTheme" = "material-icon-theme";
-        "workbench.sideBar.location" = "right";
-        "workbench.startupEditor" = "none";
-      };
+      userSettings = import ./config/vscode/settings.nix;
     };
 
     obs-studio = {
