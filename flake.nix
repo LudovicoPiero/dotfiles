3031ef1d1407f195148a2ca0eb6cc485549a98a0
Ludovico <ludovicopiero@pm.me>
2023-07-06 08:04:29 +1000

    remove nixConfig

--- flake.nix
+++ flake.nix
@@ -1,17 +1,6 @@
 {
   description = "My NixOS Flake Configuration";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://nrdxp.cachix.org"
-      "https://nix-community.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nrdxp.cachix.org-1:Fc5PSqY2Jm1TrWfm88l6cvGWwz3s93c6IOifQWnhNW4="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
-
   inputs = {
     flake-compat = {
       url = "github:edolstra/flake-compat";
