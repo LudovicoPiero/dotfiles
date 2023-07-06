dbb3d6345166bda0c045e1019795be71800c2dba
Ludovico <ludovicopiero@pm.me>
2023-07-06 16:49:49 +1000

    firefox: fix wrong placement

--- profiles/browser/firefox.nix
+++ profiles/browser/firefox.nix
@@ -5,130 +5,130 @@
 }: {
   home-manager.users."${config.vars.username}".programs.firefox = {
     enable = true;
-    profiles.ludovico = {
-      id = 0;
-      isDefault = true;
-      name = "Ludovico";
-      extensions = with config.nur.repos.rycee.firefox-addons; [
-        ublock-origin
-        bitwarden
-        betterttv
-        # fastforward
-      ];
-      bookmarks = [
-        {
-          name = "ANIME"; # Bookmark Folder
-          bookmarks = [
-            {
-              name = "ANIMEPAHE";
-              url = "https://animepahe.com";
-            }
-            {
-              name = "KICKASSANIME";
-              url = "https://kickassanime.ro";
-            }
-            {
-              name = "9Anime";
-              url = "https://9anime.gs";
-            }
-          ];
-        }
-        {
-          name = "NixOS";
-          bookmarks = [
-            {
-              name = "Nix Package";
-              url = "https://search.nixos.org/packages?channel=unstable";
-            }
-            {
-              name = "Nix Options";
-              url = "https://search.nixos.org/options?channel=unstable";
-            }
-            {
-              name = "Home-Manager";
-              url = "https://rycee.gitlab.io/home-manager/options.html";
-            }
-          ];
-        }
-      ];
-      search = {
-        default = "Brave";
-        force = true;
-        engines = {
-          "Brave" = {
-            urls = [{template = "https://search.brave.com/search?q={searchTerms}";}];
-            definedAliases = ["@b"];
-          };
-          "Nix Packages" = {
-            urls = [
+    profiles.ludovico =
+      {
+        id = 0;
+        isDefault = true;
+        name = "Ludovico";
+        extensions = with config.nur.repos.rycee.firefox-addons; [
+          ublock-origin
+          bitwarden
+          betterttv
+          # fastforward
+        ];
+        bookmarks = [
+          {
+            name = "ANIME"; # Bookmark Folder
+            bookmarks = [
               {
-                template = "https://search.nixos.org/packages";
-                params = [
-                  {
-                    name = "channel";
-                    value = "unstable";
-                  }
-                  {
-                    name = "type";
-                    value = "packages";
-                  }
-                  {
-                    name = "query";
-                    value = "{searchTerms}";
-                  }
-                ];
+                name = "ANIMEPAHE";
+                url = "https://animepahe.com";
+              }
+              {
+                name = "KICKASSANIME";
+                url = "https://kickassanime.ro";
+              }
+              {
+                name = "9Anime";
+                url = "https://9anime.gs";
               }
             ];
+          }
+          {
+            name = "NixOS";
+            bookmarks = [
+              {
+                name = "Nix Package";
+                url = "https://search.nixos.org/packages?channel=unstable";
+              }
+              {
+                name = "Nix Options";
+                url = "https://search.nixos.org/options?channel=unstable";
+              }
+              {
+                name = "Home-Manager";
+                url = "https://rycee.gitlab.io/home-manager/options.html";
+              }
+            ];
+          }
+        ];
+        search = {
+          default = "Brave";
+          force = true;
+          engines = {
+            "Brave" = {
+              urls = [{template = "https://search.brave.com/search?q={searchTerms}";}];
+              definedAliases = ["@b"];
+            };
+            "Nix Packages" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    {
+                      name = "channel";
+                      value = "unstable";
+                    }
+                    {
+                      name = "type";
+                      value = "packages";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
 
-            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-            definedAliases = ["@np"];
-          };
-          "Home-Manager" = {
-            urls = [
-              {
-                template = "https://rycee.gitlab.io/home-manager/options.html";
-              }
-            ];
-            definedAliases = ["@hm"];
-          };
-          "NixOS Options" = {
-            urls = [
-              {
-                template = "https://search.nixos.org/options";
-                params = [
-                  {
-                    name = "channel";
-                    value = "unstable";
-                  }
-                  {
-                    name = "type";
-                    value = "packages";
-                  }
-                  {
-                    name = "query";
-                    value = "{searchTerms}";
-                  }
-                ];
-              }
-            ];
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = ["@np"];
+            };
+            "Home-Manager" = {
+              urls = [
+                {
+                  template = "https://rycee.gitlab.io/home-manager/options.html";
+                }
+              ];
+              definedAliases = ["@hm"];
+            };
+            "NixOS Options" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/options";
+                  params = [
+                    {
+                      name = "channel";
+                      value = "unstable";
+                    }
+                    {
+                      name = "type";
+                      value = "packages";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
 
-            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-            definedAliases = ["@no"];
-          };
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = ["@no"];
+            };
 
-          "NixOS Wiki" = {
-            urls = [
-              {
-                template = "https://nixos.wiki/index.php?search={searchTerms}";
-              }
-            ];
-            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-            definedAliases = ["@nw"];
+            "NixOS Wiki" = {
+              urls = [
+                {
+                  template = "https://nixos.wiki/index.php?search={searchTerms}";
+                }
+              ];
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = ["@nw"];
+            };
           };
         };
-      };
-      settings =
-        {
+        settings = {
           "browser.search.region" = "AU";
           "browser.search.isUS" = false;
           "distribution.searchplugins.defaultLocale" = "en-AU";
@@ -237,14 +237,14 @@
           "gnomeTheme.hideWebrtcIndicator" = true;
           # Use system theme icons instead of Adwaita icons included by theme.
           "gnomeTheme.systemIcons" = true;
-        }
-        // (let
-          inherit (config.nur.repos.federicoschonborn) firefox-gnome-theme;
-        in {
-          userChrome = ''@import "${firefox-gnome-theme}/userChrome.css";'';
-          userContent = ''@import "${firefox-gnome-theme}/userContent.css";'';
-          extraConfig = builtins.readFile "${firefox-gnome-theme}/configuration/user.js";
-        });
-    };
+        };
+      }
+      // (let
+        inherit (config.nur.repos.federicoschonborn) firefox-gnome-theme;
+      in {
+        userChrome = ''@import "${firefox-gnome-theme}/userChrome.css";'';
+        userContent = ''@import "${firefox-gnome-theme}/userContent.css";'';
+        extraConfig = builtins.readFile "${firefox-gnome-theme}/configuration/user.js";
+      });
   };
 }
