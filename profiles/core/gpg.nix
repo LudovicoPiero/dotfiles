ec1e46d77681f1c6e4afa0871752baf12b7ba997
Ludovico Piero <ludovicopiero@pm.me>
2023-07-12 07:47:39 +1000

    gpg: add extra config

--- profiles/core/gpg.nix
+++ profiles/core/gpg.nix
@@ -1,4 +1,14 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
+  /*
+  Beware that pinentry-gnome3 may not work on non-Gnome
+  systems.
+  */
+  services.dbus.packages = [pkgs.gcr];
+
   home-manager.users."${config.vars.username}" = {
     programs.gpg = {
       enable = true;
@@ -9,6 +19,10 @@
     services.gpg-agent = {
       enable = true;
       pinentryFlavor = "gnome3";
+      extraConfig = ''
+        allow-emacs-pinentry
+        allow-preset-passphrase
+      '';
     };
   };
 }
