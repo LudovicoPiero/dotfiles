ecd59a70180292e9d84cf8e4846a202f8f51b6ca
Ludovico Piero <ludovicopiero@pm.me>
2023-07-12 06:43:02 +1000

    change polkit agent to pantheon

--- profiles/core/common.nix
+++ profiles/core/common.nix
@@ -36,14 +36,14 @@
 
   services.gnome.gnome-keyring.enable = true;
   systemd = {
-    user.services.polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
+    user.services.pantheon-agent-polkit = {
+      description = "pantheon-agent-polkit";
       wantedBy = ["graphical-session.target"];
       wants = ["graphical-session.target"];
       after = ["graphical-session.target"];
       serviceConfig = {
         Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        ExecStart = "${pkgs.pantheon.pantheon-agent-polkit}/libexec/policykit-1-pantheon/io.elementary.desktop.agent-polkit";
         Restart = "on-failure";
         RestartSec = 1;
         TimeoutStopSec = 10;
