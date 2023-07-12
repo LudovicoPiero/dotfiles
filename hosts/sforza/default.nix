595a05bb370bfb41873f53dfdffa19a83c00aa66
Ludovico Piero <ludovicopiero@pm.me>
2023-07-12 17:28:11 +1000

    gtk: add gtk extra config

--- hosts/sforza/default.nix
+++ hosts/sforza/default.nix
@@ -94,6 +94,7 @@
     sway-kiosk = command: "${pkgs.sway}/bin/sway --config ${pkgs.writeText "kiosk.config" ''
       output * bg #000000 solid_color
       exec dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK
+      bindsym Mod4+Return wezterm
       exec "${command}; ${pkgs.sway}/bin/swaymsg exit"
     ''}";
   in {
