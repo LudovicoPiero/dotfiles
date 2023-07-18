39ca194a03fe3a7f8e97dd594d6bcf9f7181f185
Ludovico Piero <ludovicopiero@pm.me>
2023-07-19 01:42:48 +1000

    cachix: remove unused cachix
    * It's useless since I pin all the inputs.

--- profiles/cachix/nix-gaming.nix
+++
@@ -1,6 +0,0 @@
-{
-  nix.settings = {
-    substituters = ["https://nix-gaming.cachix.org"];
-    trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
-  };
-}
