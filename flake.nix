db1ef6c7654c888e03e4de7742581ca177630153
Ludovico Piero <ludovicopiero@pm.me>
2023-08-04 14:26:07 +1000

    flakes: move treefmt

--- flake.nix
+++ flake.nix
@@ -139,41 +139,12 @@
           };
 
         # configure treefmt
-        treefmt = {
+        treefmt = let
+          treefmt = import ./parts/treefmt;
+        in {
           projectRootFile = "flake.nix";
-
-          programs = {
-            alejandra.enable = true;
-            deadnix.enable = true;
-            shellcheck.enable = true;
-            stylua.enable = true;
-            rustfmt.enable = true;
-            shfmt.enable = true;
-            prettier.enable = true;
-          };
-
-          settings.formatter = {
-            stylua.options = [
-              "--indent-type"
-              "spaces"
-              "--indent-width"
-              "2"
-            ];
-            prettier = {
-              options = ["--write"];
-              includes = [
-                "*.json"
-                "*.yaml"
-                "*.md"
-              ];
-            };
-            shfmt.options = [
-              "-s"
-              "-w"
-              "-i"
-              "2"
-            ];
-          };
+          programs = treefmt.programs;
+          settings.formatter = treefmt.settingsFormatter;
         };
       };
     };
