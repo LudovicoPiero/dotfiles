3516fdc7d257c6641553b4ea9cba4a0ca1049ee6
Ludovico Piero <ludovicopiero@pm.me>
2023-08-06 07:52:27 +1000

    greetd: use tuigreet

--- modules/graphical/greetd.nix
+++ modules/graphical/greetd.nix
@@ -12,24 +12,25 @@
 
   services.greetd = let
     user = "ludovico";
-    gtkgreet = "${lib.getExe pkgs.greetd.gtkgreet}";
+    tuigreet = "${lib.getExe pkgs.greetd.tuigreet}";
 
-    sway-kiosk = command: "${pkgs.sway}/bin/sway --config ${pkgs.writeText "kiosk.config" ''
-      output * bg #000000 solid_color
-      exec ${pkgs.dbus}/bin/dbus-update-activation-environment  --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK
-
-      xwayland disable
-
-      # Just in case if greetd not working properly
-      bindsym Mod4+Return exec ${lib.getExe pkgs.wezterm}
-      exec "${command}; ${pkgs.sway}/bin/swaymsg exit"
-    ''}";
+    # sway-kiosk = command: "${pkgs.sway}/bin/sway --config ${pkgs.writeText "kiosk.config" ''
+    #   output * bg #000000 solid_color
+    #   exec ${pkgs.dbus}/bin/dbus-update-activation-environment  --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK
+    #
+    #   xwayland disable
+    #
+    #   # Just in case if greetd not working properly
+    #   bindsym Mod4+Return exec ${lib.getExe pkgs.wezterm}
+    #   exec "${command}; ${pkgs.sway}/bin/swaymsg exit"
+    # ''}";
   in {
     enable = true;
     vt = 7;
     settings = {
       default_session = {
-        command = sway-kiosk "${gtkgreet} -l -c 'Hyprland'";
+        # command = sway-kiosk "${tuigreet} --time --cmd Hyprland";
+        command = "${tuigreet} --time --cmd Hyprland";
         inherit user;
       };
     };
