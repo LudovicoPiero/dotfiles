262d794485685cfd085631f2310e4249edb5c0fe
Ludovico Piero <ludovicopiero@pm.me>
2023-08-07 02:49:13 +1000

    gamemode: change start and end script

--- modules/graphical/games/gamemode.nix
+++ modules/graphical/games/gamemode.nix
@@ -13,14 +13,14 @@
   startscript = pkgs.writeShellScript "gamemode-start" ''
     export PATH=$PATH:${programs}
     export HYPRLAND_INSTANCE_SIGNATURE=$(ls -w1 /tmp/hypr | tail -1)
-    hyprctl --batch 'keyword decoration:blur 0 ; keyword animations:enabled 0 ; keyword misc:no_vfr 0'
+    hyprctl --batch 'keyword decoration:blur:enabled 0 ; keyword animations:enabled 0'
     ${pkgs.libnotify}/bin/notify-send -a 'Gamemode' 'Optimizations activated'
   '';
 
   endscript = pkgs.writeShellScript "gamemode-end" ''
     export PATH=$PATH:${programs}
     export HYPRLAND_INSTANCE_SIGNATURE=$(ls -w1 /tmp/hypr | tail -1)
-    hyprctl --batch 'keyword decoration:blur 1 ; keyword animations:enabled 1 ; keyword misc:no_vfr 1'
+    hyprctl --batch 'keyword decoration:blur:enabled 1 ; keyword animations:enabled 1'
     ${pkgs.libnotify}/bin/notify-send -a 'Gamemode' 'Optimizations deactivated'
   '';
 in {
