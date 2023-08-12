1473cbfdf1da8e10489becfbebfa756c710fc82a
Ludovico Piero <ludovicopiero@pm.me>
2023-08-12 23:46:57 +1000

    gammastep: add brightness setting

--- homes/graphical/gammastep/default.nix
+++ homes/graphical/gammastep/default.nix
@@ -3,5 +3,11 @@ _: {
     enable = true;
     provider = "geoclue2";
     tray = true;
+    settings = {
+      general = {
+        brightness-day = "0.8";
+        brightness-night = "0.8";
+      };
+    };
   };
 }
