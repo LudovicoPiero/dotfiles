98e8717ead538ca6abee7fe05bd75e360a007095
Ludovico Piero <ludovicopiero@pm.me>
2023-08-22 00:35:28 +1000

    starship: change settings

--- homes/core/starship.nix
+++ homes/core/starship.nix
@@ -17,8 +17,13 @@
         "$git_status"
         "$cmd_duration"
         "$line_break"
-        "$python"
         "$character"
+      ];
+      right_format = lib.concatStrings [
+        "$sudo"
+        "$nix_shell"
+        "\${custom.direnv}"
+        "$time"
       ];
       command_timeout = 1000;
       # right_format = "$all";
@@ -26,8 +31,8 @@
         style = "blue";
       };
       character = {
-        success_symbol = "[❯](purple)";
-        error_symbol = "[❯](red)";
+        success_symbol = "[λ](purple)";
+        error_symbol = "[λ](red)";
       };
       git_branch = {
         style = "bright-black";
@@ -36,26 +41,30 @@
       git_status = {
         format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
         style = "cyan";
-        conflicted = "​";
-        untracked = "​";
-        modified = "​";
-        staged = "​";
-        renamed = "​";
-        deleted = "​";
+        conflicted = "=";
+        untracked = "?";
+        modified = "!";
+        staged = "+";
+        renamed = "»";
+        deleted = "✘";
         stashed = "≡";
       };
       git_state = {
         format = "\([$state( $progress_current/$progress_total)]($style)\)";
         style = "bright-black";
       };
+      nix_shell = {
+        format = "[󱄅 ](cyan)";
+        heuristic = true;
+      };
+      sudo = {
+        format = "[ ](red)";
+        disabled = false;
+      };
       cmd_duration = {
         format = "[$duration]($style) ";
         style = "yellow";
         min_time = 5000;
-      };
-      python = {
-        format = "[$virtualenv]($style) ";
-        style = "bright-black";
       };
     };
   };
