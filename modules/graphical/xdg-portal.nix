bdbca11a6c7168c50e428415a25224848ed9b1b0
Ludovico Piero <ludovicopiero@pm.me>
2023-08-25 19:14:51 +1000

    xdg-portal: enable xdgOpenUsePortal

--- modules/graphical/xdg-portal.nix
+++ modules/graphical/xdg-portal.nix
@@ -9,6 +9,7 @@
       # wlr disabled because i'm using xdg-desktop-portal-hyprland
       wlr.enable = lib.mkForce false;
       enable = true;
+      xdgOpenUsePortal = true;
       extraPortals = lib.mkForce [
         pkgs.xdg-desktop-portal-gtk
         (inputs.xdph.packages.${pkgs.system}.xdg-desktop-portal-hyprland.override {
