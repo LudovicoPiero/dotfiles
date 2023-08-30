02239ba218e766ff2a6d091f23a4f02b146e9cc8
Ludovico Piero <ludovicopiero@pm.me>
2023-08-30 17:44:55 +1000

    fix format error

--- homes/graphical/common.nix
+++ homes/graphical/common.nix
@@ -61,7 +61,7 @@ in {
 
   programs.obs-studio = {
     enable = true;
-    plugins = with pkgs.obs-studio-plugins;[
+    plugins = with pkgs.obs-studio-plugins; [
       wlrobs
       obs-pipewire-audio-capture
     ];
