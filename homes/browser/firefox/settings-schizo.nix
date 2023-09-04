7e0393c7e472e4313f3b9e59c5200cf15acc1cb4
Ludovico Piero <ludovicopiero@pm.me>
2023-09-04 19:28:27 +1000

    firefox: add schizo profiles

---
+++ homes/browser/firefox/settings-schizo.nix
@@ -0,0 +1,388 @@
+{
+  "browser.search.region" = "AU";
+  "browser.search.isUS" = false;
+  "distribution.searchplugins.defaultLocale" = "en-AU";
+  "general.useragent.locale" = "en-AU";
+  "browser.bookmarks.showMobileBookmarks" = true;
+  "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+  "browser.privatebrowsing.vpnpromourl" = "";
+
+  # DOH
+  /*
+  trr.mode 5 is no DOH.
+  2 is enable DOH.
+  3 is no failback to system dns
+  */
+  "network.trr.mode" = 3;
+  "network.trr.default_provider_uri" = "https://doh.tiarap.org/dns-query";
+  "network.trr.uri" = "https://doh.tiarap.org/dns-query";
+  "network.trr.custom_uri" = "https://doh.tiarap.org/dns-query";
+  # "network.trr.bootstrapAddress" = "1.1.1.1";
+
+  # Firefox GNOME Theme
+  # Hide the tab bar when only one tab is open.
+  "gnomeTheme.hideSingleTab" = false;
+  # Move Bookmarks toolbar under tabs.
+  "gnomeTheme.bookmarksToolbarUnderTabs" = true;
+  # Hide WebRTC indicator since GNOME provides their own privacy icons in the top right.
+  "gnomeTheme.hideWebrtcIndicator" = true;
+  # Use system theme icons instead of Adwaita icons included by theme.
+  "gnomeTheme.systemIcons" = true;
+
+  "app.update.auto" = false;
+  "browser.aboutConfig.showWarning" = false;
+  "browser.backspace_action" = 0;
+  "browser.disableResetPrompt" = true;
+  "browser.fixup.alternate.enabled" = false;
+  "browser.library.activity-stream.enabled" = false;
+  "browser.newtab.preload" = false;
+  "browser.newtabpage.activity-stream.enabled" = false;
+  "browser.onboarding.enabled" = false;
+  "browser.pagethumbnails.capturing_disabled" = true;
+  "browser.pocket.enabled" = false;
+  "browser.search.widget.inNavBar" = false;
+  "browser.sessionstore.interval" = 60000;
+  "browser.shell.defaultBrowserCheckCount" = 1;
+  "browser.startup.homepage" = "about:blank";
+  "browser.startup.page" = 0;
+  "browser.uiCustomization.state" = ''{"placements":{"widget-overflow-fixed-list":["87677a2c52b84ad3a151a4a72f5bd3c4_jetpack-browser-action","aws-extend-switch-roles_toshi_tilfin_com-browser-action","sort-bookmarks_heftig-browser-action","_96586e48-b9a2-45dd-b1a1-54fa85a97c91_-browser-action","_a4c4eda4-fb84-4a84-b4a1-f7c1cbf2a1ad_-browser-action","_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action","jid1-bofifl9vbdl2zq_jetpack-browser-action","jid1-kkzogwgsw3ao4q_jetpack-browser-action","jid1-tsgsxbhncspbwq_jetpack-browser-action","ublock0_raymondhill_net-browser-action"],"nav-bar":["back-button","forward-button","stop-reload-button","home-button","urlbar-container","downloads-button","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button","ublock0_raymondhill_net-browser-action","87677a2c52b84ad3a151a4a72f5bd3c4_jetpack-browser-action","aws-extend-switch-roles_toshi_tilfin_com-browser-action","sort-bookmarks_heftig-browser-action","_96586e48-b9a2-45dd-b1a1-54fa85a97c91_-browser-action","_a4c4eda4-fb84-4a84-b4a1-f7c1cbf2a1ad_-browser-action","_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action","jid1-bofifl9vbdl2zq_jetpack-browser-action","jid1-kkzogwgsw3ao4q_jetpack-browser-action","jid1-tsgsxbhncspbwq_jetpack-browser-action","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","toolbar-menubar","TabsToolbar","widget-overflow-fixed-list"],"currentVersion":17,"newElementCount":3}'';
+  "browser.uidensity" = 1;
+  "browser.urlbar.clickSelectsAll" = true;
+  "browser.urlbar.trimURLs" = false;
+  "browser.urlbar.update1" = false;
+  "browser.xul.error_pages.enabled" = false;
+  "browser.zoom.full" = false;
+  "datareporting.sessions.current.clean" = true;
+  "devtools.onboarding.telemetry.logged" = false;
+  "extensions.autoDisableScopes" = 0;
+  "extensions.formautofill.available" = "off";
+  "extensions.getAddons.discovery.api_url" = "";
+  "extensions.htmlaboutaddons.discover.enabled" = false;
+  "extensions.screenshots.upload-disabled" = true;
+  "findbar.modalHighlight" = true;
+  "font.default.x-western" = "sans-serif";
+  "gfx.font_rendering.fontconfig.max_generic_substitutions" = 127;
+  "layout.css.backdrop-filter.enabled" = true;
+  "layout.css.devPixelsPerpx" = 1;
+  "layout.frame_rate" = 144;
+  "layout.spellcheckDefault" = 2;
+  "layout.word_select.eat_space_to_next_word" = false;
+  "loop.enabled" = false;
+  "media.mediasource.webm.enabled" = true;
+  "network.dns.disableIPv6" = true;
+  "network.dnsCacheEntries" = 10000;
+  "network.http.http3.enabled" = true;
+  "network.http.max-urgent-start-excessive-connections-per-host" = 6;
+  "network.tcp.keepalive.idle_time" = 300;
+  "network.tcp.tcp_fastopen_enable" = true;
+  "plugin.state.flash" = 0;
+  "plugins.click_to_play" = true;
+  "privacy.trackingprotection.cryptomining.enabled" = true;
+  "privacy.trackingprotection.enabled" = true;
+  "privacy.userContext.enabled" = true;
+  "privacy.usercontext.about_newtab_segregation.enabled" = true;
+  "reader.parse-on-load.enabled" = false;
+  "reader.parse-on-load.force-enabled" = false;
+  "security.enterprise_roots.enabled" = true;
+  "security.webauthn.ctap2" = true;
+  "svg.context-properties.content.enabled" = true;
+  "toolkit.cosmeticAnimations.enabled" = false;
+  "toolkit.telemetry.hybridContent.enabled" = false;
+  "toolkit.telemetry.reportingpolicy.firstRun" = false;
+  "ui.systemUsesDarkTheme" = false;
+
+  /*
+  The remaining sections were takenfrom yokoffing/Betterfox
+  */
+
+  /*
+    ***************************************************************************
+   * SECTION: FASTFOX                                                         *
+  ***************************************************************************
+  */
+  "nglayout.initialpaint.delay" = 0;
+  "nglayout.initialpaint.delay_in_oopif" = 0;
+  "content.notify.interval" = 100000;
+  "browser.startup.preXulSkeletonUI" = false;
+
+  /*
+  * EXPERIMENTAL **
+  */
+  "layout.css.grid-template-masonry-value.enabled" = true;
+  "dom.enable_web_task_scheduling" = true;
+
+  /*
+  * GFX **
+  */
+  "gfx.webrender.all" = true;
+  "gfx.webrender.precache-shaders" = true;
+  "gfx.webrender.compositor" = true;
+  "layers.gpu-process.enabled" = true;
+  "media.hardware-video-decoding.enabled" = true;
+  "gfx.canvas.accelerated" = true;
+  "gfx.canvas.accelerated.cache-items" = 32768;
+  "gfx.canvas.accelerated.cache-size" = 4096;
+  "gfx.content.skia-font-cache-size" = 80;
+  "image.cache.size" = 10485760;
+  "image.mem.decode_bytes_at_a_time" = 131072;
+  "image.mem.shared.unmap.min_expiration_ms" = 120000;
+  "media.memory_cache_max_size" = 1048576;
+  "media.memory_caches_combined_limit_kb" = 2560000;
+  "media.cache_readahead_limit" = 9000;
+  "media.cache_resume_threshold" = 6000;
+
+  /*
+  * BROWSER CACHE **
+  */
+  "browser.cache.memory.max_entry_size" = 153600;
+
+  /*
+  * NETWORK **
+  */
+  "network.buffer.cache.size" = 262144;
+  "network.buffer.cache.count" = 128;
+  "network.http.max-connections" = 1800;
+  "network.http.max-persistent-connections-per-server" = 10;
+  "network.ssl_tokens_cache_capacity" = 32768;
+
+  /*
+    ***************************************************************************
+   * SECTION: SECUREFOX                                                       *
+  ***************************************************************************
+  */
+
+  /*
+  * TRACKING PROTECTION **
+  */
+  "browser.contentblocking.category" = "strict";
+  "urlclassifier.trackingSkipURLs" = "*.reddit.com, *.twitter.com, *.twimg.com, *.tiktok.com";
+  "urlclassifier.features.socialtracking.skipURLs" = "*.instagram.com, *.twitter.com, *.twimg.com";
+  "privacy.query_stripping.strip_list" = "__hsfp __hssc __hstc __s _hsenc _openstat dclid fbclid gbraid gclid hsCtaTracking igshid mc_eid ml_subscriber ml_subscriber_hash msclkid oft_c oft_ck oft_d oft_id oft_ids oft_k oft_lk oft_sk oly_anon_id oly_enc_id rb_clickid s_cid twclid vero_conv vero_id wbraid wickedid yclid";
+  "browser.uitour.enabled" = false;
+  "privacy.globalprivacycontrol.enabled" = true;
+  "privacy.globalprivacycontrol.functionality.enabled" = true;
+
+  /*
+  * OCSP & CERTS / HPKP **
+  */
+  "security.OCSP.enabled" = 0;
+  "security.remote_settings.crlite_filters.enabled" = true;
+  "security.pki.crlite_mode" = 2;
+  "security.cert_pinning.enforcement_level" = 2;
+
+  /*
+  * SSL / TLS **
+  */
+  "security.ssl.treat_unsafe_negotiation_as_broken" = true;
+  "browser.xul.error_pages.expert_bad_cert" = true;
+  "security.tls.enable_0rtt_data" = false;
+
+  /*
+  * DISK AVOIDANCE **
+  */
+  "browser.cache.disk.enable" = false;
+  "browser.privatebrowsing.forceMediaMemoryCache" = true;
+  "browser.sessionstore.privacy_level" = 2;
+
+  /*
+  * SHUTDOWN & SANITIZING **
+  */
+  "privacy.history.custom" = true;
+
+  /*
+  * SPECULATIVE CONNECTIONS **
+  */
+  "network.http.speculative-parallel-limit" = 0;
+  "network.dns.disablePrefetch" = true;
+  "browser.urlbar.speculativeConnect.enabled" = false;
+  "browser.places.speculativeConnect.enabled" = false;
+  "network.prefetch-next" = false;
+  "network.predictor.enabled" = false;
+  "network.predictor.enable-prefetch" = false;
+
+  /*
+  * SEARCH / URL BAR **
+  */
+  "browser.search.separatePrivateDefault.ui.enabled" = true;
+  "browser.urlbar.update2.engineAliasRefresh" = true;
+  "browser.search.suggest.enabled" = false;
+  "browser.urlbar.suggest.quicksuggest.sponsored" = false;
+  "browser.urlbar.suggest.quicksuggest.nonsponsored" = false;
+  "security.insecure_connection_text.enabled" = true;
+  "security.insecure_connection_text.pbmode.enabled" = true;
+  "network.IDN_show_punycode" = true;
+
+  /*
+  * HTTPS-FIRST MODE **
+  */
+  "dom.security.https_first" = true;
+
+  /*
+  * PROXY / SOCKS / IPv6 **
+  */
+  "network.proxy.socks_remote_dns" = true;
+  "network.file.disable_unc_paths" = true;
+  "network.gio.supported-protocols" = "";
+
+  /*
+  * PASSWORDS AND AUTOFILL **
+  */
+  "signon.formlessCapture.enabled" = false;
+  "signon.privateBrowsingCapture.enabled" = false;
+  "signon.autofillForms" = false;
+  "signon.rememberSignons" = false;
+  "editor.truncate_user_pastes" = false;
+
+  /*
+  * ADDRESS + CREDIT CARD MANAGER **
+  */
+  "extensions.formautofill.addresses.enabled" = false;
+  "extensions.formautofill.creditCards.enabled" = false;
+  "extensions.formautofill.heuristics.enabled" = false;
+  "browser.formfill.enable" = false;
+
+  /*
+  * MIXED CONTENT + CROSS-SITE **
+  */
+  "network.auth.subresource-http-auth-allow" = 1;
+  "pdfjs.enableScripting" = false;
+  "extensions.postDownloadThirdPartyPrompt" = false;
+  "permissions.delegation.enabled" = false;
+
+  /*
+  * HEADERS / REFERERS **
+  */
+  "network.http.referer.XOriginTrimmingPolicy" = 2;
+
+  /*
+  * CONTAINERS **
+  */
+  "privacy.userContext.ui.enabled" = true;
+
+  /*
+  * WEBRTC **
+  */
+  "media.peerconnection.ice.proxy_only_if_behind_proxy" = true;
+  "media.peerconnection.ice.default_address_only" = true;
+
+  /*
+  * SAFE BROWSING **
+  */
+  "browser.safebrowsing.downloads.remote.enabled" = false;
+
+  /*
+  * MOZILLA **
+  */
+  "accessibility.force_disabled" = 1;
+  "browser.tabs.firefox-view" = false;
+  "permissions.default.desktop-notification" = 2;
+  "permissions.default.geo" = 2;
+  "geo.provider.network.url" = "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%";
+  "geo.provider.use_gpsd" = false;
+  "geo.provider.use_geoclue" = false;
+  "permissions.manager.defaultsUrl" = "";
+  "webchannel.allowObject.urlWhitelist" = "";
+
+  /*
+  * TELEMETRY **
+  */
+  "toolkit.telemetry.unified" = false;
+  "toolkit.telemetry.enabled" = false;
+  "toolkit.telemetry.server" = "data:,";
+  "toolkit.telemetry.archive.enabled" = false;
+  "toolkit.telemetry.newProfilePing.enabled" = false;
+  "toolkit.telemetry.shutdownPingSender.enabled" = false;
+  "toolkit.telemetry.updatePing.enabled" = false;
+  "toolkit.telemetry.bhrPing.enabled" = false;
+  "toolkit.telemetry.firstShutdownPing.enabled" = false;
+  "toolkit.telemetry.coverage.opt-out" = true;
+  "toolkit.coverage.opt-out" = true;
+  "datareporting.healthreport.uploadEnabled" = false;
+  "datareporting.policy.dataSubmissionEnabled" = false;
+  "app.shield.optoutstudies.enabled" = false;
+  "browser.discovery.enabled" = false;
+  "breakpad.reportURL" = "";
+  "browser.tabs.crashReporting.sendReport" = false;
+  "browser.crashReports.unsubmittedCheck.autoSubmit2" = false;
+  "captivedetect.canonicalURL" = "";
+  "network.captive-portal-service.enabled" = false;
+  "network.connectivity-service.enabled" = false;
+  "default-browser-agent.enabled" = false;
+  "app.normandy.enabled" = false;
+  "app.normandy.api_url" = "";
+  "browser.ping-centre.telemetry" = false;
+  "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+  "browser.newtabpage.activity-stream.telemetry" = false;
+
+  /*
+   ***************************************************************************
+   * SECTION: PESKYFOX                                                       *
+  ***************************************************************************
+  */
+
+  /*
+  * MOZILLA UI **
+  */
+  "layout.css.prefers-color-scheme.content-override" = 0; # 0=dark, 1=light, 2=system, 3=browser
+  "app.update.suppressPrompts" = true;
+  "browser.compactmode.show" = true;
+  "extensions.getAddons.showPane" = false;
+  "extensions.htmlaboutaddons.recommendations.enabled" = false;
+  "browser.shell.checkDefaultBrowser" = false;
+  "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons" = false;
+  "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features" = false;
+  "browser.preferences.moreFromMozilla" = false;
+  "browser.tabs.tabmanager.enabled" = false;
+  "browser.aboutwelcome.enabled" = false;
+  "findbar.highlightAll" = true;
+  "middlemouse.contentLoadURL" = false;
+  "browser.privatebrowsing.enable-new-indicator" = false;
+
+  /*
+  * FULLSCREEN **
+  */
+  "full-screen-api.transition-duration.enter" = "0 0";
+  "full-screen-api.transition-duration.leave" = "0 0";
+  "full-screen-api.warning.delay" = -1;
+  "full-screen-api.warning.timeout" = 0;
+
+  /*
+  * URL BAR **
+  */
+  "browser.urlbar.suggest.engines" = false;
+  "browser.urlbar.suggest.topsites" = false;
+  "browser.urlbar.suggest.calculator" = true;
+  "browser.urlbar.unitConversion.enabled" = true;
+
+  /*
+  * NEW TAB PAGE **
+  */
+  "browser.newtabpage.activity-stream.feeds.topsites" = false;
+  "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+
+  /*
+  ** POCKET **
+  */
+  "extensions.pocket.enabled" = false;
+
+  /*
+  * DOWNLOADS **
+  */
+  "browser.download.useDownloadDir" = false;
+  "browser.download.alwaysOpenPanel" = false;
+  "browser.download.manager.addToRecentDocs" = false;
+  "browser.download.always_ask_before_handling_new_types" = true;
+
+  /*
+  * PDF **
+  */
+  "browser.download.open_pdf_attachments_inline" = true;
+
+  /*
+  * TAB BEHAVIOR **
+  */
+  "browser.bookmarks.openInTabClosesMenu" = false;
+  "layout.css.has-selector.enabled" = true;
+  "cookiebanners.service.mode" = 2;
+  "cookiebanners.service.mode.privateBrowsing" = 2;
+}
