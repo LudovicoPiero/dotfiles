a405716c804407e2966076d9e299fd3a43ad4ae0
Ludovico Piero <ludovicopiero@pm.me>
2023-09-04 23:46:16 +1000

    firefox: update bookmarks

--- homes/browser/firefox/bookmarks.nix
+++ homes/browser/firefox/bookmarks.nix
@@ -1,35 +1,90 @@
 [
   {
     name = "ANIME"; # Bookmark Folder
+    toolbar = true;
     bookmarks = [
       {
         name = "ANIMEPAHE";
+        keyword = "ah";
         url = "https://animepahe.com";
       }
       {
         name = "KICKASSANIME";
+        keyword = "kaa";
         url = "https://kickassanime.ro";
       }
       {
-        name = "9Anime";
-        url = "https://9anime.gs";
+        name = "ANIWAVE";
+        keyword = "aw";
+        url = "https://aniwave.to";
       }
     ];
   }
   {
     name = "NixOS";
+    toolbar = true;
     bookmarks = [
       {
         name = "Nix Package";
+        keyword = "nw";
         url = "https://search.nixos.org/packages?channel=unstable";
       }
       {
         name = "Nix Options";
+        keyword = "no";
         url = "https://search.nixos.org/options?channel=unstable";
       }
       {
         name = "Home-Manager";
+        keyword = "hm";
         url = "https://rycee.gitlab.io/home-manager/options.html";
+      }
+    ];
+  }
+  {
+    name = "1337";
+    toolbar = true;
+    bookmarks = [
+      {
+        name = "GitHub";
+        keyword = "gh";
+        url = "https://github.com";
+      }
+      {
+        name = "GitLab";
+        keyword = "gl";
+        url = "https://gitlab.com";
+      }
+      {
+        name = "SourceHut";
+        keyword = "sh";
+        url = "https://git.sr.ht";
+      }
+    ];
+  }
+  {
+    name = "lol";
+    toolbar = true;
+    bookmarks = [
+      {
+        name = "Google";
+        keyword = "g";
+        url = "https://google.com";
+      }
+      {
+        name = "DuckDuckGo";
+        keyword = "dg";
+        url = "https://DuckDuckGo.com";
+      }
+      {
+        name = "Twitter";
+        keyword = "x";
+        url = "https://x.com";
+      }
+      {
+        name = "YouTube";
+        keyword = "yt";
+        url = "https://YouTube.com";
       }
     ];
   }
