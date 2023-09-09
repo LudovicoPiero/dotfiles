15075318ddb182cfcf09c59aa60ba110dffd9d7b
Ludovico Piero <ludovicopiero@pm.me>
2023-09-09 12:54:09 +1000

    desktop: add seatd

--- modules/graphical/desktop.nix
+++ modules/graphical/desktop.nix
@@ -12,6 +12,20 @@
     pam.services.swaylock.text = "auth include login";
   };
 
+  systemd.services = {
+    seatd = {
+      enable = true;
+      description = "Seat Management Daemon";
+      script = "${pkgs.seatd}/bin/seatd -g wheel";
+      serviceConfig = {
+        Type = "simple";
+        Restart = "always";
+        RestartSec = 1;
+      };
+      wantedBy = ["multi-user.target"];
+    };
+  };
+
   services = {
     xserver.displayManager.sessionPackages = [
       inputs.hyprland.packages.${pkgs.system}.default
