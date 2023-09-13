ec36a58e54af0f105c3cea5cc665cb060e26f6fe
Ludovico Piero <lewdovico@gnuweeb.org>
2023-09-13 22:43:01 +1000

    remove unused bookmarks

--- homes/graphical/gtk/default.nix
+++ homes/graphical/gtk/default.nix
@@ -51,8 +51,6 @@ in {
         "file:///home/${username}/Music"
         "file:///home/${username}/Pictures"
         "file:///home/${username}/Videos"
-        # "file:///home/${username}/WinC"
-        "file:///home/${username}/WinD"
         "file:///home/${username}/WinE"
       ];
 
