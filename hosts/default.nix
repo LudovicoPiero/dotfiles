f9b63ee2b24c912cd5d398b7f3c94f3d12ef84a1
Vico <lewdovico@gnuweeb.org>
2023-09-14 14:13:54 +0000

    add chaotic (#54)
    * add chaotic
    
    * use fork
    
    * flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/a0ddf43b6268f1717afcda54133dea30435eb178' (2023-09-13)
      → 'github:nix-community/home-manager/d9b88b43524db1591fb3d9410a21428198d75d49' (2023-09-13)
    • Updated input 'impermanence':
        'github:nix-community/impermanence/e3a7acd113903269a1b5c8b527e84ce7ee859851' (2023-07-31)
      → 'github:nix-community/impermanence/e9643d08d0d193a2e074a19d4d90c67a874d932e' (2023-09-13)
    • Updated input 'lanzaboote':
        'github:nix-community/lanzaboote/6d6cdf59b9d1ca6a2732c57e40f7cbd0f1eb1755' (2023-08-25)
      → 'github:nix-community/lanzaboote/3dc8778c32bbdd082e91e264b9b3dc9ffab277a4' (2023-09-13)
    • Updated input 'master':
        'github:nixos/nixpkgs/c9cc42714f301310e9919a89a99484619d613dc9' (2023-09-13)
      → 'github:nixos/nixpkgs/005a10ec19f8336eaaaaacc351a403b7d928b298' (2023-09-14)
    • Updated input 'nur':
        'github:nix-community/nur/e8adba4e1f0f539c255d796950a099c2bf2b5ae6' (2023-09-13)
      → 'github:nix-community/nur/6ebb358b238e414cbe677c3563fbd4f8b4f56330' (2023-09-14)
    • Updated input 'stable':
        'github:nixos/nixpkgs/be818ff394d656cf0dc2855a2e47628ddcf68f59' (2023-09-13)
      → 'github:nixos/nixpkgs/690a206a65bcfac41ae6c67ef8b14ed0ab17857e' (2023-09-14)
    
    * flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'chaotic':
        'github:ludovicopiero/nyx/2c89b7a449cd4174ca19a50ff6fc5db0712edc9c' (2023-09-14)
      → 'github:ludovicopiero/nyx/09395c91d6bfe9ce70758fc7b1395ee6e36af2e2' (2023-09-14)

--- hosts/default.nix
+++ hosts/default.nix
@@ -15,6 +15,7 @@
       ../modules/secrets
 
       inputs.aagl.nixosModules.default
+      inputs.chaotic.nixosModules.default
       inputs.hyprland.nixosModules.default
       inputs.impermanence.nixosModule
       inputs.nh.nixosModules.default
