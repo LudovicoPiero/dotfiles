2bf644b256455549897e021b98ebbee446c21691
Ludovico Piero <lewdovico@gnuweeb.org>
2023-10-25 09:39:57 +0900

    Revert "hosts/sforza: use pkgs.linuxPackages_testing_bcachefs"
    This reverts commit 9b061096f950970b5b5a66b6411bd77f3ac063fa.

--- hosts/sforza/hardware-configuration.nix
+++ hosts/sforza/hardware-configuration.nix
@@ -3,6 +3,7 @@
   pkgs,
   lib,
   modulesPath,
+  inputs,
   ...
 }: {
   imports = [
@@ -16,7 +17,8 @@
 
   boot = {
     initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" "bcache"];
-    kernelPackages = lib.mkForce pkgs.linuxPackages_testing_bcachefs;
+    kernelPackages = lib.mkForce inputs.chaotic.packages.${pkgs.system}.linuxPackages_cachyos;
+    # kernelPackages = lib.mkForce pkgs.linuxPackages_testing_bcachefs;
     initrd.kernelModules = ["amdgpu" "dm-snapshot"];
     kernelModules = ["kvm-amd"];
     extraModulePackages = [];
