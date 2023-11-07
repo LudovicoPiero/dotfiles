b4868062bdce844b48f6ae6ff8f21e2198cc6cc8
Ludovico Piero <lewdovico@gnuweeb.org>
2023-11-07 16:43:38 +0900

    wezterm: use `Cargo.lock` from src

--- pkgs/wezterm/default.nix
+++ pkgs/wezterm/default.nix
@@ -50,7 +50,7 @@ rustPlatform.buildRustPackage rec {
   '';
 
   cargoLock = {
-    lockFile = ./Cargo.lock;
+    lockFile = "${src}/Cargo.lock";
     outputHashes = {
       "xcb-1.2.1" = "sha256-zkuW5ATix3WXBAj2hzum1MJ5JTX3+uVQ01R1vL6F1rY=";
       "xcb-imdkit-0.2.0" = "sha256-L+NKD0rsCk9bFABQF4FZi9YoqBHr4VAZeKAWgsaAegw=";
