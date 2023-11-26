35201cea9ad188b5f8fd19e770aa614d0524ef53
Ludovico Piero <lewdovico@gnuweeb.org>
2023-11-26 22:51:29 +0900

    feat(homes/hyprland): remove nofullscreenrequest and nomaximizerequest on firefox
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- homes/windowManager/hyprland/settings.nix
+++ homes/windowManager/hyprland/settings.nix
@@ -187,8 +187,6 @@ in {
     "noblur, class:^(chromium-browser)$" # wayland
     "noblur, title:^(.*(Disc|WebC)ord.*)$"
     "noblur, title:^(Open File)$"
-    "nofullscreenrequest, class:^(firefox)$, title:^(.*Extension: \(Bitwarden - Free Password Manager\).*)$"
-    "nomaximizerequest, class:^(firefox)$, title:^(.*Extension: \(Bitwarden - Free Password Manager\).*)$"
     "noshadow, class:^(firefox)$"
     "noshadow, class:^(Chromium-browser)$" # xwayland
     "noshadow, class:^(chromium-browser)$" # wayland
