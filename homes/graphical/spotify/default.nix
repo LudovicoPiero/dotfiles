7269f1f6a810d4f2e3a8bcb78cc0aa22d1a6243b
Ludovico Piero <lewdovico@gnuweeb.org>
2023-12-01 06:55:34 +0900

    feat(homes/spotify): change themes & colorscheme
    Check https://github.com/Gerg-L/spicetify-nix/commit/24d7702ee33e76f1251afcaaca497cf18d93e229 for more info
    
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- homes/graphical/spotify/default.nix
+++ homes/graphical/spotify/default.nix
@@ -10,29 +10,9 @@ in {
   programs.spicetify = {
     enable = true;
     spotifyPackage = inputs.self.packages.${pkgs.system}.spotify;
-    theme = spicePkgs.themes.fluent;
-    colorScheme = "custom";
-    customColorScheme = {
-      text = "${colors.base04}";
-      subtext = "${colors.base0D}";
-      extratext = "${colors.base01}";
-      main = "${colors.base00}";
-      sidebar = "${colors.base00}";
-      player = "${colors.base00}";
-      sec-player = "${colors.base01}";
-      card = "${colors.base0E}";
-      sec-card = "${colors.base02}";
-      shadow = "000000";
-      selected-row = "${colors.base0D}";
-      button = "${colors.base03}";
-      button-active = "${colors.base06}";
-      button-disabled = "${colors.base04}";
-      button-knob = "${colors.base0B}";
-      tab-active = "${colors.base0B}";
-      notification = "${colors.base0B}";
-      notification-error = "${colors.base0E}";
-      misc = "${colors.base0A}";
-    };
+
+    theme = spicePkgs.themes.text;
+    colorScheme = "mocha";
 
     enabledExtensions = with spicePkgs.extensions; [
       fullAppDisplay
