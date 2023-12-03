64cf0171af6558925cc767276745ec7a7d2a9ce9
Ludovico Piero <lewdovico@gnuweeb.org>
2023-12-03 18:51:58 +0900

    remove unused pkgs
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,17 +3,11 @@
 
   perSystem = {pkgs, ...}: {
     packages = {
-      geist-font = pkgs.callPackage ./geist-font {};
-
-      multicolor-sddm-theme = pkgs.callPackage ./multicolor-sddm-theme {};
-
       iosevka-q = pkgs.callPackage ./iosevka-q {};
 
       san-francisco-pro = pkgs.callPackage ./san-francisco-pro {};
 
       spotify = pkgs.callPackage ./spotify {};
-
-      wavefox = pkgs.callPackage ./wavefox {};
     };
   };
 }
