63f71a97c94ac291e47b09379a45c1e3db662e97
Ludovico Piero <lewdovico@gnuweeb.org>
2023-12-05 10:41:02 +0900

    feat(xdg-portal): enable xdgOpenUsePortal
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/graphical/xdg-portal.nix
+++ modules/graphical/xdg-portal.nix
@@ -9,6 +9,9 @@
       wlr.enable = lib.mkForce false;
       enable = true;
 
+      # Sets environment variable `NIXOS_XDG_OPEN_USE_PORTAL` to `1`
+      xdgOpenUsePortal = true;
+
       # uses the first portal implementation found in lexicographical order
       config.common.default = "*";
       extraPortals = [
