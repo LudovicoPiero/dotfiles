58bfa79e93a63b883c3c80cea7e22ef2d5238574
Ludovico Piero <lewdovico@gnuweeb.org>
2023-12-29 10:35:59 +0900

    nixos-modules: xdg-portal: override xdph to version 1.2.5
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- nixos-modules/graphical/xdg-portal/default.nix
+++ nixos-modules/graphical/xdg-portal/default.nix
@@ -30,7 +30,17 @@ in
         config.common.default = "*";
         extraPortals = [
           pkgs.xdg-desktop-portal-gtk
-          pkgs.xdg-desktop-portal-hyprland
+          (pkgs.xdg-desktop-portal-hyprland.overrideAttrs (
+            _old: rec {
+              version = "1.2.5";
+              src = pkgs.fetchFromGitHub {
+                owner = "hyprwm";
+                repo = "xdg-desktop-portal-hyprland";
+                rev = "v${version}";
+                hash = "sha256-X4o/mifI7Nhu0UKYlxx53wIC+gYDo3pVM9L2u3PE2bE=";
+              };
+            }
+          ))
         ];
       };
     };
