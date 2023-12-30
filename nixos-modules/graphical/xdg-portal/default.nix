2b8942a431b86b2865ef783f0d3f86c298bd2340
Ludovico Piero <lewdovico@gnuweeb.org>
2023-12-30 10:14:41 +0900

    nixos-modules: xdg-portal: apply patches to xdph
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- nixos-modules/graphical/xdg-portal/default.nix
+++ nixos-modules/graphical/xdg-portal/default.nix
@@ -39,6 +39,14 @@ in
                 rev = "v${version}";
                 hash = "sha256-X4o/mifI7Nhu0UKYlxx53wIC+gYDo3pVM9L2u3PE2bE=";
               };
+
+              patches =
+                [
+                  (pkgs.fetchpatch {
+                    url = "https://github.com/hyprwm/xdg-desktop-portal-hyprland/commit/576a49ef3d940d0391ba18b21e5f279ebd75e12b.patch";
+                    hash = "sha256-szXHYM/U6NQqiECNSVAE+4sogDvI01JrcGuq6R1QKUU=";
+                  })
+                ];
             }
           ))
         ];
