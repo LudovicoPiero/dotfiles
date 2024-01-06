6affefc849b2386468f2f6b590bcd17034cccffc
Ludovico Piero <lewdovico@gnuweeb.org>
2024-01-06 15:13:06 +0900

    home-modules: nvim: remove mini.animate
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- home-modules/editor/nvim/lua/custom/plugins/mini.lua
+++ home-modules/editor/nvim/lua/custom/plugins/mini.lua
@@ -2,8 +2,7 @@ return {
   "echasnovski/mini.nvim",
   version = false,
   config = function()
-    require("mini.starter").setup()
-    require("mini.animate").setup()
     require("mini.ai").setup()
+    require("mini.starter").setup()
   end,
 }
