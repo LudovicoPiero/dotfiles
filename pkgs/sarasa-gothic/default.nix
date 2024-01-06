c43be398467ed68186ee4b8b54a767ed5829bdf3
Ludovico Piero <lewdovico@gnuweeb.org>
2024-01-06 22:34:44 +0900

    pkgs: sarasa-gothic: 1.0.0 -> 1.0.3
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- pkgs/sarasa-gothic/default.nix
+++ pkgs/sarasa-gothic/default.nix
@@ -6,13 +6,13 @@
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "sarasa-gothic";
-  version = "1.0.0";
+  version = "1.0.3";
 
   src = fetchurl {
     # Use the 'ttc' files here for a smaller closure size.
     # (Using 'ttf' files gives a closure size about 15x larger, as of November 2021.)
     url = "https://github.com/be5invis/Sarasa-Gothic/releases/download/v${version}/Sarasa-TTC-${version}.7z";
-    hash = "sha256-Fktdf6ZGjFZX6M9WSCNTnrGLrN0o+qWINTRWh/6xRRQ=";
+    hash = "sha256-hL5myiTL2PkDCmWrCLx/yzbm+qDqSr6EPqY86Epw+LE=";
   };
 
   sourceRoot = ".";
