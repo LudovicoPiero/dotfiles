22bc2909cb69715b4b284397dc3a8d2c46d5ee7a
Ludovico Piero <lewdovico@gnuweeb.org>
2024-01-07 12:39:48 +0900

    *: run nixfmt
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- nixos-modules/core/gnome/default.nix
+++ nixos-modules/core/gnome/default.nix
@@ -46,21 +46,22 @@ in
         environment.systemPackages = [ pkgs.libsecret ];
         services.dbus.packages = [ pkgs.gnome.seahorse ];
 
-      systemd = {
-        user.services.pantheon-agent-polkit = {
-          description = "pantheon-agent-polkit";
-          wantedBy = ["graphical-session.target"];
-          wants = ["graphical-session.target"];
-          after = ["graphical-session.target"];
-          serviceConfig = {
-            Type = "simple";
-            ExecStart = "${pkgs.pantheon.pantheon-agent-polkit}/libexec/policykit-1-pantheon/io.elementary.desktop.agent-polkit";
-            Restart = "on-failure";
-            RestartSec = 1;
-            TimeoutStopSec = 10;
+        systemd = {
+          user.services.pantheon-agent-polkit = {
+            description = "pantheon-agent-polkit";
+            wantedBy = [ "graphical-session.target" ];
+            wants = [ "graphical-session.target" ];
+            after = [ "graphical-session.target" ];
+            serviceConfig = {
+              Type = "simple";
+              ExecStart = "${pkgs.pantheon.pantheon-agent-polkit}/libexec/policykit-1-pantheon/io.elementary.desktop.agent-polkit";
+              Restart = "on-failure";
+              RestartSec = 1;
+              TimeoutStopSec = 10;
+            };
           };
         };
-      };
-    })
-  ]);
+      })
+    ]
+  );
 }
