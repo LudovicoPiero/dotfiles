4d539d079b6d6b7722757c986644233a73865a7e
Ludovico Piero <lewdovico@gnuweeb.org>
2024-01-08 14:25:13 +0900

    nixos-modules: greetd: use gtkgreet
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- nixos-modules/graphical/greetd/default.nix
+++ nixos-modules/graphical/greetd/default.nix
@@ -35,40 +35,18 @@ in
       services.greetd.enableGnomeKeyring = true;
     };
 
-    programs.regreet = {
-      enable = true;
-      settings = {
-        background = "${self}/assets/Minato-Aqua-Dark.png";
-        GTK = {
-          cursor_theme_name = "macOS-BigSur";
-          font_name = "SF Pro Rounded 16";
-          icon_theme_name = "WhiteSur";
-          theme_name = "WhiteSur-Dark";
-        };
-      };
-    };
-
     services.greetd =
       let
         user = username;
         _ = lib.getExe;
         __ = lib.getExe';
       in
-      /* sway = "${lib.getExe pkgs.sway}";
-         swayConf = pkgs.writeText "greetd-sway-config" ''
-           output * background #000000 solid_color
-           exec "dbus-update-activation-environment --systemd WAYLAND_DISPLAY SWAYSOCK XDG_CURRENT_DESKTOP"
-           xwayland disable
-
-           exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
-         '';
-      */
       {
         enable = true;
         vt = 7;
         settings = {
           default_session = {
-            command = "${__ pkgs.dbus "dbus-run-session"} ${_ pkgs.cage} -s -- ${_ config.programs.regreet.package}";
+            command = "${__ pkgs.dbus "dbus-run-session"} ${_ pkgs.cage} -s -- ${_ pkgs.greetd.gtkgreet} -l";
             inherit user;
           };
         };
