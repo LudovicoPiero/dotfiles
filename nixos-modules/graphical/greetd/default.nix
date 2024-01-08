5c6700f8f6e40dcec11fc70f68cd9a0aef92dde0
Ludovico Piero <lewdovico@gnuweeb.org>
2024-01-08 13:10:38 +0900

    nixos-modules: greetd: use cage instead of sway
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- nixos-modules/graphical/greetd/default.nix
+++ nixos-modules/graphical/greetd/default.nix
@@ -32,21 +32,24 @@ in
     services.greetd =
       let
         user = username;
-        sway = "${lib.getExe pkgs.sway}";
-        swayConf = pkgs.writeText "greetd-sway-config" ''
-          output * background #000000 solid_color
-          exec "dbus-update-activation-environment --systemd WAYLAND_DISPLAY SWAYSOCK XDG_CURRENT_DESKTOP"
-          xwayland disable
-
-          exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
-        '';
+        _ = lib.getExe;
+        __ = lib.getExe';
       in
+      /* sway = "${lib.getExe pkgs.sway}";
+         swayConf = pkgs.writeText "greetd-sway-config" ''
+           output * background #000000 solid_color
+           exec "dbus-update-activation-environment --systemd WAYLAND_DISPLAY SWAYSOCK XDG_CURRENT_DESKTOP"
+           xwayland disable
+
+           exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
+         '';
+      */
       {
         enable = true;
         vt = 7;
         settings = {
           default_session = {
-            command = "${sway} --config ${swayConf}";
+            command = "${__ pkgs.dbus "dbus-run-session"} ${_ pkgs.cage} -- ${_ pkgs.greetd.gtkgreet} -l";
             inherit user;
           };
         };
