f016b9bc2d3bae31cc1d7f602a5091249506b28f
Ludovico Piero <lewdovico@gnuweeb.org>
2024-01-10 07:15:52 +0000

    Update waybar: 4b20f522f0b357d7140a60c70e90fafb58c6bd9f → 748fc809b51a6063e1b39bb17cc2c54e65d6291b wezterm: 4921f139d35590ab35415021221a2a6f5cf10ab3 → 1c0901957d3eb3deaa8d9406040f234ee81733e5

--- pkgs/_sources/generated.nix
+++ pkgs/_sources/generated.nix
@@ -29,35 +29,35 @@
   };
   waybar = {
     pname = "waybar";
-    version = "4b20f522f0b357d7140a60c70e90fafb58c6bd9f";
+    version = "748fc809b51a6063e1b39bb17cc2c54e65d6291b";
     src = fetchgit {
       url = "https://github.com/alexays/waybar";
-      rev = "4b20f522f0b357d7140a60c70e90fafb58c6bd9f";
+      rev = "748fc809b51a6063e1b39bb17cc2c54e65d6291b";
       fetchSubmodules = false;
       deepClone = false;
       leaveDotGit = false;
-      sha256 = "sha256-7z48B58W3YXOi41llbB/RJgm9RfzHT+rvlfpTGXQtok=";
+      sha256 = "sha256-iV8J2ucbxyaVDD6w8VwB329phA8tzGYWYYlM8JLXFq0=";
     };
-    date = "2024-01-04";
+    date = "2024-01-08";
   };
   wezterm = {
     pname = "wezterm";
-    version = "4921f139d35590ab35415021221a2a6f5cf10ab3";
+    version = "1c0901957d3eb3deaa8d9406040f234ee81733e5";
     src = fetchgit {
       url = "https://github.com/wez/wezterm";
-      rev = "4921f139d35590ab35415021221a2a6f5cf10ab3";
+      rev = "1c0901957d3eb3deaa8d9406040f234ee81733e5";
       fetchSubmodules = true;
       deepClone = false;
       leaveDotGit = false;
-      sha256 = "sha256-WXOsP2rjbT4unc7lXbxbRbCcrc89SfyVdErzFndBF9o=";
+      sha256 = "sha256-7mA51U/3iPoXvYcDy5WEY4Ve0c5ey+vsxaKo9UoGjFE=";
     };
     cargoLock."Cargo.lock" = {
-      lockFile = ./wezterm-4921f139d35590ab35415021221a2a6f5cf10ab3/Cargo.lock;
+      lockFile = ./wezterm-1c0901957d3eb3deaa8d9406040f234ee81733e5/Cargo.lock;
       outputHashes = {
         "xcb-1.2.1" = "sha256-zkuW5ATix3WXBAj2hzum1MJ5JTX3+uVQ01R1vL6F1rY=";
         "xcb-imdkit-0.2.0" = "sha256-L+NKD0rsCk9bFABQF4FZi9YoqBHr4VAZeKAWgsaAegw=";
       };
     };
-    date = "2024-01-03";
+    date = "2024-01-08";
   };
 }
