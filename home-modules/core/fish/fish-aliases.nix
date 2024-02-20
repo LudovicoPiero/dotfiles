372c8c601442711c162c008b0eee1880ae4b6faa
Ludovico Piero <lewdovico@gnuweeb.org>
2024-02-21 04:26:37 +0900

    home-modules: fish: update aliases
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- home-modules/core/fish/fish-aliases.nix
+++ home-modules/core/fish/fish-aliases.nix
@@ -9,7 +9,6 @@ let
 in
 with pkgs;
 {
-  "c" = "${_ commitizen} commit -- -s"; # Commit with Signed-off
   "cat" = "${_ bat}";
   "config" = "cd ~/.config/nixos";
   "dla" = "${_ yt-dlp} --extract-audio --audio-format mp3 --audio-quality 0 -P '${config.home.homeDirectory}/Media/Audios'"; # Download Audio
@@ -21,7 +20,7 @@ with pkgs;
   "lla" = "${_ lsd} -lA";
   "t" = "${_ lsd} -l --tree";
   "tree" = "${_ lsd} -l --tree";
-  "lg" = "lazygit";
+  "lg" = "${_ lazygit}";
   "nb" = "nix-build -E 'with import <nixpkgs> { }; callPackage ./default.nix { }'";
   "nv" = "nvim";
   "nr" = "${_ nixpkgs-review}";
@@ -31,6 +30,7 @@ with pkgs;
   "v" = "vim";
   "record" = "${_ wl-screenrec} -f ${config.xdg.userDirs.extraConfig.XDG_RECORD_DIR}/$(date '+%s').mp4";
   "record-region" = ''${_ wl-screenrec} -g "$(${_ slurp})" -f ${config.xdg.userDirs.extraConfig.XDG_RECORD_DIR}/$(date '+%s').mp4'';
+  "y" = "${_ yazi}";
   "..." = "cd ../..";
   ".." = "cd ..";
 }
