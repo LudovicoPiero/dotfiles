af35dcab7f78c434299775e1f81a66bceb66ead0
Ludovico Piero <lewdovico@gnuweeb.org>
2024-03-23 10:43:37 +0900

    nixosConfigurations: sforza: change tlp settings
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- cells/workstations/nixosConfigurations/sforza.nix
+++ cells/workstations/nixosConfigurations/sforza.nix
@@ -123,14 +123,6 @@ in {
       settings = {
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-
-        CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 100;
-        CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 50;
       };
     };
   };
