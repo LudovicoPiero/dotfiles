3ecc051cf58d2ce3d71b76dc3c2c49af7cf57a05
Ludovico Piero <lewdovico@gnuweeb.org>
2024-04-05 13:06:16 +0900

    feat: unlock keyring on start
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- cells/workstations/nixosProfiles/keyring/default.nix
+++ cells/workstations/nixosProfiles/keyring/default.nix
@@ -1,11 +1,24 @@
-{pkgs, ...}: {
-  environment.systemPackages = [pkgs.libsecret];
+{
+  pkgs,
+  config,
+  ...
+}: {
+  environment = {
+    systemPackages = [pkgs.libsecret];
+    variables.XDG_RUNTIME_DIR = "/run/user/${toString config.users.users.airi.uid}"; # set the runtime directory
+  };
   programs.dconf.enable = true;
   # Fixes the org.a11y.Bus not provided by .service file error
   services = {
     gnome.at-spi2-core.enable = true;
     gnome.gnome-keyring.enable = true;
     dbus.packages = [pkgs.gnome.seahorse];
+  };
+
+  security = {
+    pam.services.greetd.gnupg.enable = true;
+    pam.services.greetd.enableGnomeKeyring = true;
+    pam.services.swaylock.text = "auth include login";
   };
 
   systemd = {
