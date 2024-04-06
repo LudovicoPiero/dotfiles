9d8c6b507498161ebd19635bb33a0a60d665b312
Ludovico Piero <lewdovico@gnuweeb.org>
2024-04-06 22:08:21 +0900

    feat: sforza: enable dhcp
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- cells/workstations/nixosConfigurations/sforza.nix
+++ cells/workstations/nixosConfigurations/sforza.nix
@@ -72,30 +72,8 @@ in {
 
   networking = {
     hostName = "sforza";
-    useDHCP = false;
+    useDHCP = lib.mkDefault true;
     enableIPv6 = false; # L ISP
-    nameservers = [
-      "1.1.1.1"
-    ];
-    defaultGateway = {
-      address = "192.168.1.1";
-    };
-    interfaces.enp3s0 = {
-      ipv4.addresses = [
-        {
-          address = "192.168.1.98";
-          prefixLength = 24;
-        }
-      ];
-    };
-    interfaces.wlp4s0 = {
-      ipv4.addresses = [
-        {
-          address = "192.168.1.99";
-          prefixLength = 24;
-        }
-      ];
-    };
     networkmanager.enable = true;
   };
 
