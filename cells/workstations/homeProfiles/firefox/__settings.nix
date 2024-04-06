d026f3c500c60db08006e4f2a26cf2ab1e05fbec
Ludovico Piero <lewdovico@gnuweeb.org>
2024-04-07 01:04:15 +0900

    feat: firefox: enable DOH
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- cells/workstations/homeProfiles/firefox/__settings.nix
+++ cells/workstations/homeProfiles/firefox/__settings.nix
@@ -15,10 +15,10 @@
   2 is enable DOH.
   3 is no failback to system dns
   */
-  "network.trr.mode" = 0;
-  # "network.trr.default_provider_uri" = "https://doh.tiarap.org/dns-query";
-  # "network.trr.uri" = "https://doh.tiarap.org/dns-query";
-  # "network.trr.custom_uri" = "https://doh.tiarap.org/dns-query";
+  "network.trr.mode" = 2;
+  "network.trr.default_provider_uri" = "https://doh.tiarap.org/dns-query";
+  "network.trr.uri" = "https://doh.tiarap.org/dns-query";
+  "network.trr.custom_uri" = "https://doh.tiarap.org/dns-query";
   # "network.trr.bootstrapAddress" = "1.1.1.1";
 
   # Disable telemetry
