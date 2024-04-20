ca9e81c6508b147f98ecfdba6710e73275455a1e
Ludovico Piero <lewdovico@gnuweeb.org>
2024-04-20 17:16:04 +0900

    feat: wireguard: update config
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- cells/workstations/nixosProfiles/wireguard/default.nix
+++ cells/workstations/nixosProfiles/wireguard/default.nix
@@ -22,16 +22,20 @@ in
   networking.wg-quick.interfaces = {
     wg0 = {
       autostart = true;
-      address = [ "10.66.66.2/32" ];
-      dns = [ "45.76.145.144" ];
+      address = [ "10.66.66.3/32" ];
+      dns = [
+        "174.138.21.128"
+        "188.166.206.224"
+        "1.1.1.1"
+      ];
       privateKeyFile = config.sops.secrets.privateKey.path;
 
       peers = [
         {
-          publicKey = "hOxW74kF//JpljARxf4+lu+cbwgn8OtB+lXT2Tqoyhk=";
+          publicKey = "w0jyL2xL1T1lLu1IKCDS4EXfEsSOjiJio9dnmkhevws=";
           presharedKeyFile = config.sops.secrets.presharedKey.path;
           allowedIPs = [ "0.0.0.0/0" ];
-          endpoint = "45.76.145.144:51820";
+          endpoint = "64.176.44.14:51820";
           persistentKeepalive = 25;
         }
       ];
