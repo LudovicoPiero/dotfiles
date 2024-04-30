cb27727d134db09f8388872cd3a0f1706e6ffb95
Ludovico Piero <lewdovico@gnuweeb.org>
2024-04-30 16:06:34 +0900

    feat: greetd: use sway-kiosk
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- cells/workstations/nixosProfiles/greetd/default.nix
+++ cells/workstations/nixosProfiles/greetd/default.nix
@@ -18,12 +18,21 @@
       username = "airi";
       _ = lib.getExe;
       __ = lib.getExe';
+
+      swayConf = pkgs.writeText "greetd-sway-config" ''
+        output * background #000000 solid_color
+        exec "dbus-update-activation-environment --systemd WAYLAND_DISPLAY SWAYSOCK XDG_CURRENT_DESKTOP"
+        xwayland disable
+
+        exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
+      '';
     in
     {
       enable = true;
       settings = {
         default_session = {
-          command = "${__ pkgs.dbus "dbus-run-session"} ${_ pkgs.cage} -s -- ${_ pkgs.greetd.gtkgreet} -l";
+          # command = "${__ pkgs.dbus "dbus-run-session"} ${_ pkgs.cage} -s -- ${_ pkgs.greetd.gtkgreet} -l";
+          command = "${_ pkgs.sway} --config ${swayConf}";
           user = "${username}";
         };
       };
