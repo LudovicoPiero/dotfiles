0992a00e76027b07202c77408c0c22b28f5eaadb
Ludovico Piero <lewdovico@gnuweeb.org>
2024-06-18 23:45:00 +0900

    discord: fix vesktop build
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- cells/workstations/homeProfiles/discord/default.nix
+++ cells/workstations/homeProfiles/discord/default.nix
@@ -4,10 +4,12 @@
   home.packages = [
     (pkgs.vesktop.overrideAttrs (old: {
       patches = (old.patches or [ ]) ++ [ ./__readonlyFix.patch ];
-      postFixup = ''
-        wrapProgram $out/bin/vesktop \
-          --add-flags "--enable-features=UseOzonePlatform --ozone-platform=wayland --enable-accelerated-mjpeg-decode --enable-accelerated-video --ignore-gpu-blacklist --enable-native-gpu-memory-buffers --enable-gpu-rasterization --enable-gpu --enable-features=WebRTCPipeWireCapturer --enable-wayland-ime"
-      '';
+      postFixup =
+        old.postFixup
+        + ''
+          wrapProgram $out/bin/vesktop \
+            --add-flags "--enable-features=UseOzonePlatform --ozone-platform=wayland --enable-accelerated-mjpeg-decode --enable-accelerated-video --ignore-gpu-blacklist --enable-native-gpu-memory-buffers --enable-gpu-rasterization --enable-gpu --enable-features=WebRTCPipeWireCapturer --enable-wayland-ime"
+        '';
     }))
   ];
 
