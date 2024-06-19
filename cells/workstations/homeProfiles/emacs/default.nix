adce9dc89bc0ffa35d4caee7e06ac04ae5208cd1
Ludovico Piero <lewdovico@gnuweeb.org>
2024-06-19 22:32:51 +0900

    emacs: remove doom-themes
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- cells/workstations/homeProfiles/emacs/default.nix
+++ cells/workstations/homeProfiles/emacs/default.nix
@@ -382,24 +382,6 @@ in
         };
 
         # UI Stuff
-        doom-themes = {
-          enable = true;
-          config = ''
-            ;; Global settings (defaults)
-            (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-                  doom-themes-enable-italic t) ; if nil, italics is universally disabled
-            (load-theme 'doom-one t)
-            ;; Enable flashing mode-line on errors
-            (doom-themes-visual-bell-config)
-            ;; Enable custom neotree theme (all-the-icons must be installed!)
-            (doom-themes-neotree-config)
-            ;; or for treemacs users
-            (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
-            (doom-themes-treemacs-config)
-            ;; Corrects (and improves) org-mode's native fontification.
-            (doom-themes-org-config)
-          '';
-        };
         doom-modeline = {
           enable = true;
           init = ''(doom-modeline-mode 1)'';
