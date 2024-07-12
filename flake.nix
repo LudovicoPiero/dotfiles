d85b13d96b968e114510ef9381880c85d2d36b71
Ludovico Piero <lewdovico@gnuweeb.org>
2024-07-12 09:18:59 +0900

    flake: lix-module: use 2.90.0
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
 
     # Lix
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0-rc1.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
