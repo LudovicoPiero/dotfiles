1bee804f683eb4507d07cc6f9f73cb4fa8b34d9a
Ludovico Piero <lewdovico@gnuweeb.org>
2024-08-20 01:29:29 +0900

    fish: eza -> lsd
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- cells/workstations/homeProfiles/fish/__shellAliases.nix
+++ cells/workstations/homeProfiles/fish/__shellAliases.nix
@@ -14,14 +14,14 @@ with pkgs;
   "config" = "cd ${config.home.homeDirectory}/Code/nixos";
   "dla" = "${_ yt-dlp} --extract-audio --audio-format mp3 --audio-quality 0 -P '${config.home.homeDirectory}/Media/Audios'"; # Download Audio
   "dlv" = "${_ yt-dlp} --format 'best[ext=mp4]' -P '${config.home.homeDirectory}/Media/Videos'"; # Download Video
-  "ls" = "${_ eza} --icons";
-  "l" = "${_ eza} -lbF --git --icons";
-  "ll" = "${_ eza} -lbGF --git --icons";
-  "llm" = "${_ eza} -lbGF --git --sort=modified --icons";
-  "la" = "${_ eza} -lbhHigUmuSa --time-style=long-iso --git --icons";
-  "lx" = "${_ eza} -lbhHigUmuSa@ --time-style=long-iso --git --icons";
-  "t" = "${_ eza} --tree --icons";
-  "tree" = "${_ eza} --tree --icons";
+  "ls" = "${_ lsd} --icons";
+  "l" = "${_ lsd} -lbF --git --icons";
+  "ll" = "${_ lsd} -lbGF --git --icons";
+  "llm" = "${_ lsd} -lbGF --git --sort=modified --icons";
+  "la" = "${_ lsd} -lbhHigUmuSa --time-style=long-iso --git --icons";
+  "lx" = "${_ lsd} -lbhHigUmuSa@ --time-style=long-iso --git --icons";
+  "t" = "${_ lsd} --tree --icons";
+  "tree" = "${_ lsd} --tree --icons";
   "lg" = "${_ lazygit}";
   "nb" = "nix-build -E 'with import <nixpkgs> { }; callPackage ./default.nix { }'";
   "ns" = "nix-shell -p";
