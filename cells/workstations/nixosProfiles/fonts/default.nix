e963edbf517f66a655a43e80274e616664f01062
Ludovico Piero <lewdovico@gnuweeb.org>
2024-09-24 15:19:57 +0900

    treefmt
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- cells/workstations/nixosProfiles/fonts/default.nix
+++ cells/workstations/nixosProfiles/fonts/default.nix
@@ -8,7 +8,11 @@
   fonts = {
     fontDir.enable = true;
     packages = lib.attrValues {
-      inherit (inputs.ludovico-nixpkgs.packages.${pkgs.stdenv.hostPlatform.system}) san-francisco-pro sarasa-gothic iosevka-q;
+      inherit (inputs.ludovico-nixpkgs.packages.${pkgs.stdenv.hostPlatform.system})
+        san-francisco-pro
+        sarasa-gothic
+        iosevka-q
+        ;
 
       inherit (pkgs)
         emacs-all-the-icons-fonts
