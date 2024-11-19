e532fc89b680df85809498f2055f9219e4c51eae
Ludovico Piero <lewdovico@gnuweeb.org>
2024-11-19 12:15:49 +0900

    homeProfiles: theme: fix qt theme
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- cells/workstations/homeProfiles/theme/default.nix
+++ cells/workstations/homeProfiles/theme/default.nix
@@ -63,6 +63,10 @@ in
 
   home.packages = [
     run-as-service
+    pkgs.qt6Packages.qtstyleplugin-kvantum
+    pkgs.qt6Packages.qt6ct
+    pkgs.libsForQt5.qtstyleplugin-kvantum
+    pkgs.libsForQt5.qt5ct
   ];
 
   programs.hyprcursor-phinger.enable = true;
@@ -196,10 +200,74 @@ in
     };
   };
 
+  # QT
   qt = {
     enable = true;
-    platformTheme.name = "gtk3";
+    platformTheme = "qtct";
   };
+
+  # Source: https://github.com/fufexan/dotfiles/blob/b946c18a1232e9529b28348a131faeb7f85668a3/home/programs/qt.nix#L38
+  xdg.configFile =
+    let
+      defaultFont = "${config.gtk.font.name},${builtins.toString config.gtk.font.size}";
+      KvLibadwaita = pkgs.fetchFromGitHub {
+        owner = "GabePoel";
+        repo = "KvLibadwaita";
+        rev = "87c1ef9f44ec48855fd09ddab041007277e30e37";
+        hash = "sha256-K/2FYOtX0RzwdcGyeurLXAh3j8ohxMrH2OWldqVoLwo=";
+        sparseCheckout = [ "src" ];
+      };
+
+      qtctConf = {
+        Appearance = {
+          custom_palette = false;
+          icon_theme = config.gtk.iconTheme.name;
+          standard_dialogs = "xdgdesktopportal";
+          style = "kvantum";
+        };
+      };
+    in
+    {
+      # Kvantum config
+      "Kvantum" = {
+        source = "${KvLibadwaita}/src";
+        recursive = true;
+      };
+
+      "Kvantum/kvantum.kvconfig".text = ''
+        [General]
+        theme=KvLibadwaitaDark
+      '';
+
+      # qtct config
+      "qt5ct/qt5ct.conf".text =
+        let
+          default = ''"${defaultFont},-1,5,50,0,0,0,0,0"'';
+        in
+        lib.generators.toINI { } (
+          qtctConf
+          // {
+            Fonts = {
+              fixed = default;
+              general = default;
+            };
+          }
+        );
+
+      "qt6ct/qt6ct.conf".text =
+        let
+          default = ''"${defaultFont},-1,5,400,0,0,0,0,0,0,0,0,0,0,1,Regular"'';
+        in
+        lib.generators.toINI { } (
+          qtctConf
+          // {
+            Fonts = {
+              fixed = default;
+              general = default;
+            };
+          }
+        );
+    };
 
   home.file.".icons/default/index.theme".text = ''
     [icon theme]
