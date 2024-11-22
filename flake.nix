e37357896d1ac8759ffe72c86082bfe5ce90b575
Ludovico Piero <lewdovico@gnuweeb.org>
2024-11-22 12:27:47 +0900

    chore: Update flake.lock
    Flake lock file updates:
    
    • Updated input 'hyprland':
        'git+https://github.com/hyprwm/Hyprland?ref=refs/heads/main&rev=940f7aa990dbc99815bab8d355999d8277534b17' (2024-11-20)
      → 'git+https://github.com/hyprwm/Hyprland?ref=refs/heads/main&rev=b1003445953474b967464d4d0878955d37498647' (2024-11-22)
    • Updated input 'lix-module':
        'https://git.lix.systems/api/v1/repos/lix-project/nixos-module/archive/9098ac95768f7006d7e070b88bae76939f6034e6.tar.gz?narHash=sha256-6U0CyPycIBc04hbYy2hBINnVso58n/ZyywY2BD3hu%2Bs%3D' (2024-10-19)
      → 'https://git.lix.systems/api/v1/repos/lix-project/nixos-module/archive/9098ac95768f7006d7e070b88bae76939f6034e6.tar.gz?narHash=sha256-6U0CyPycIBc04hbYy2hBINnVso58n/ZyywY2BD3hu%2Bs%3D&rev=9098ac95768f7006d7e070b88bae76939f6034e6' (2024-10-19)
    • Updated input 'ludovico-nixpkgs':
        'github:LudovicoPiero/nixpackages/c908d33d18329ff7128be26bfe38d8baf53a4a9e' (2024-11-20)
      → 'github:LudovicoPiero/nixpackages/28b8b8aec1938a1520c4c453df67c324d849bc9a' (2024-11-21)
    • Updated input 'ludovico-nixpkgs/nixpkgs':
        'github:NixOS/nixpkgs/5e4fbfb6b3de1aa2872b76d49fafc942626e2add' (2024-11-15)
      → 'github:NixOS/nixpkgs/23e89b7da85c3640bbc2173fe04f4bd114342367' (2024-11-19)
    • Updated input 'ludovico-nixpkgs/rust-overlay':
        'github:oxalica/rust-overlay/8509a51241c407d583b1963d5079585a992506e8' (2024-11-20)
      → 'github:oxalica/rust-overlay/2d484c7a0db32f2700e253160bcd2aaa6cdca3ba' (2024-11-21)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/23e89b7da85c3640bbc2173fe04f4bd114342367' (2024-11-19)
      → 'github:NixOS/nixpkgs/c0451e363899513045f9d63e85ab3d8d88708e33' (2024-11-21)
    
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "xd uwu";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
