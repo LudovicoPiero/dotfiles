287959b45d14104a9ff11cd49fa332fdf7ffdc73
Ludovico Piero <lewdovico@gnuweeb.org>
2024-11-25 03:03:16 +0900

    modules/hyprland: tweak keybind & startup
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/hyprland/settings.nix
+++ modules/hyprland/settings.nix
@@ -19,10 +19,14 @@ in {
       "uwsm finalize"
       "systemctl --user restart xdg-desktop-portal-gtk.service xdg-desktop-portal.service xdg-desktop-portal-hyprland.service"
       "hyprctl setcursor ${config.gtk.cursorTheme.name} ${toString config.gtk.cursorTheme.size}"
-      "uwsm app -- waybar"
-      "fcitx5 -d --replace"
       "uwsm app -- ${_ pkgs.mako}"
       "[workspace 9 silent;noanim] uwsm app -- ${_ pkgs.thunderbird}"
+    ]
+    ++ lib.optionals (osConfig.myOptions.waybar.enable) [
+      "uwsm app -- waybar"
+    ]
+    ++ lib.optionals (osConfig.i18n.inputMethod.type == "fcitx5") [
+      "fcitx5 -d --replace"
     ]
     ++ lib.optionals (config.programs.emacs.enable && !config.services.emacs.enable) [
       "${_ config.programs.emacs.finalPackage} --fg-daemon"
@@ -68,7 +72,7 @@ in {
     dim_strength = 0.7;
 
     blur = {
-      enabled = false;
+      enabled = true;
       size = 2;
       passes = 2;
       # contrast = 0.8916;
@@ -184,77 +188,85 @@ in {
   ];
 
   "$mod" = "SUPER";
-  bind = [
-    "$mod SHIFT, C , exit ,"
-    "$mod      , Q, togglespecialworkspace"
-    "$mod SHIFT, Q, movetoworkspace, special"
-    "$mod      , D , exec , uwsm app -- vesktop"
-    "$mod SHIFT, E , exec , uwsm app -- thunar"
-    "$mod      , F , fullscreen , 0"
-    "$mod      , G , exec , uwsm app -- firefox"
-    "$mod SHIFT, G , exec , uwsm app -- chromium"
-    "$mod      , M , exec , [workspace 9 silent;tile] uwsm app --  thunderbird"
-    "$mod      , P , exec , uwsm app -- ${launcher}"
-    "$mod SHIFT, P , exec , uwsm app -- ${lib.getExe' pkgs.pass-wayland "passmenu"}"
-    "$mod      , Space , togglefloating ,"
-    "$mod      , R , togglegroup ,"
-    "$mod SHIFT, J , changegroupactive, f"
-    "$mod SHIFT, K , changegroupactive, b"
-    "$mod      , S , exec , uwsm app -- spotify"
-    "$mod      , W , killactive ,"
-    "$mod      , X , exec , ${powermenu}"
-    "$mod      , Return , exec , uwsm app -- '${osConfig.myOptions.vars.terminal}'"
+  bind =
+    [
+      "$mod SHIFT, C , exit ,"
+      "$mod      , Q, togglespecialworkspace"
+      "$mod SHIFT, Q, movetoworkspace, special"
+      "$mod SHIFT, E , exec , uwsm app -- thunar"
+      "$mod      , F , fullscreen , 0"
+      "$mod SHIFT, G , exec , uwsm app -- chromium"
+      "$mod      , M , exec , [workspace 9 silent;tile] uwsm app --  thunderbird"
+      "$mod      , P , exec , uwsm app -- ${launcher}"
+      "$mod SHIFT, P , exec , uwsm app -- ${lib.getExe' pkgs.pass-wayland "passmenu"}"
+      "$mod      , Space , togglefloating ,"
+      "$mod      , R , togglegroup ,"
+      "$mod SHIFT, J , changegroupactive, f"
+      "$mod SHIFT, K , changegroupactive, b"
+      "$mod      , W , killactive ,"
+      "$mod      , X , exec , ${powermenu}"
+      "$mod      , Return , exec , uwsm app -- '${osConfig.myOptions.vars.terminal}'"
 
-    "$mod      , E , exec , uwsm app -- \"${emacs}\""
-    "ALT       , E , exec , uwsm app -- \"emacsclient -c -eval '(dired nil)'\""
+      ", print, exec , uwsm app --  wl-ocr"
+      "CTRL   , Print , exec , uwsm app -- ${_ pkgs.grimblast} save area - | ${_ pkgs.swappy} -f -"
+      "ALT    , Print , exec , uwsm app -- ${_ pkgs.grimblast} --notify --cursor copysave output ~/Pictures/Screenshots/$(date +'%F_%H:%M:%S.png')"
 
-    ", print, exec , uwsm app --  wl-ocr"
-    "CTRL   , Print , exec , uwsm app -- ${_ pkgs.grimblast} save area - | ${_ pkgs.swappy} -f -"
-    "ALT    , Print , exec , uwsm app -- ${_ pkgs.grimblast} --notify --cursor copysave output ~/Pictures/Screenshots/$(date +'%F_%H:%M:%S.png')"
+      # Dwindle Keybind
+      "$mod , h , resizeactive , -20 0"
+      "$mod , l , resizeactive , 20 0"
+      "$mod , k , movefocus , u"
+      "$mod , j , movefocus , d"
 
-    # Dwindle Keybind
-    "$mod , h , resizeactive , -20 0"
-    "$mod , l , resizeactive , 20 0"
-    "$mod , k , movefocus , u"
-    "$mod , j , movefocus , d"
+      "$mod , left , movewindow , l"
+      "$mod , right , movewindow , r"
+      "$mod , up , movewindow , u"
+      "$mod , down , movewindow , d"
+      "$mod SHIFT , h , movewindow , l"
+      "$mod SHIFT , l , movewindow , r"
+      "$mod SHIFT , k , movewindow , u"
+      "$mod SHIFT , j , movewindow , d"
 
-    "$mod , left , movewindow , l"
-    "$mod , right , movewindow , r"
-    "$mod , up , movewindow , u"
-    "$mod , down , movewindow , d"
-    "$mod SHIFT , h , movewindow , l"
-    "$mod SHIFT , l , movewindow , r"
-    "$mod SHIFT , k , movewindow , u"
-    "$mod SHIFT , j , movewindow , d"
+      "$mod , 1 , workspace , 1"
+      "$mod , 2 , workspace , 2"
+      "$mod , 3 , workspace , 3"
+      "$mod , 4 , workspace , 4"
+      "$mod , 5 , workspace , 5"
+      "$mod , 6 , workspace , 6"
+      "$mod , 7 , workspace , 7"
+      "$mod , 8 , workspace , 8"
+      "$mod , 9 , workspace , 9"
+      "$mod , 0 , workspace , 10"
 
-    "$mod , 1 , workspace , 1"
-    "$mod , 2 , workspace , 2"
-    "$mod , 3 , workspace , 3"
-    "$mod , 4 , workspace , 4"
-    "$mod , 5 , workspace , 5"
-    "$mod , 6 , workspace , 6"
-    "$mod , 7 , workspace , 7"
-    "$mod , 8 , workspace , 8"
-    "$mod , 9 , workspace , 9"
-    "$mod , 0 , workspace , 10"
+      "$mod SHIFT , 1 , movetoworkspacesilent , 1"
+      "$mod SHIFT , 2 , movetoworkspacesilent , 2"
+      "$mod SHIFT , 3 , movetoworkspacesilent , 3"
+      "$mod SHIFT , 4 , movetoworkspacesilent , 4"
+      "$mod SHIFT , 5 , movetoworkspacesilent , 5"
+      "$mod SHIFT , 6 , movetoworkspacesilent , 6"
+      "$mod SHIFT , 7 , movetoworkspacesilent , 7"
+      "$mod SHIFT , 8 , movetoworkspacesilent , 8"
+      "$mod SHIFT , 9 , movetoworkspacesilent , 9"
+      "$mod SHIFT , 0 , movetoworkspacesilent , 10"
 
-    "$mod SHIFT , 1 , movetoworkspacesilent , 1"
-    "$mod SHIFT , 2 , movetoworkspacesilent , 2"
-    "$mod SHIFT , 3 , movetoworkspacesilent , 3"
-    "$mod SHIFT , 4 , movetoworkspacesilent , 4"
-    "$mod SHIFT , 5 , movetoworkspacesilent , 5"
-    "$mod SHIFT , 6 , movetoworkspacesilent , 6"
-    "$mod SHIFT , 7 , movetoworkspacesilent , 7"
-    "$mod SHIFT , 8 , movetoworkspacesilent , 8"
-    "$mod SHIFT , 9 , movetoworkspacesilent , 9"
-    "$mod SHIFT , 0 , movetoworkspacesilent , 10"
-
-    ", XF86AudioNext , exec , ${pkgs.playerctl}/bin/playerctl next"
-    ", XF86AudioPrev , exec , ${pkgs.playerctl}/bin/playerctl previous"
-    ", XF86AudioPlay , exec , ${pkgs.playerctl}/bin/playerctl play-pause"
-    ", XF86AudioPause , exec , ${pkgs.playerctl}/bin/playerctl pause"
-    ", XF86AudioStop , exec , ${pkgs.playerctl}/bin/playerctl stop"
-  ];
+      ", XF86AudioNext , exec , ${pkgs.playerctl}/bin/playerctl next"
+      ", XF86AudioPrev , exec , ${pkgs.playerctl}/bin/playerctl previous"
+      ", XF86AudioPlay , exec , ${pkgs.playerctl}/bin/playerctl play-pause"
+      ", XF86AudioPause , exec , ${pkgs.playerctl}/bin/playerctl pause"
+      ", XF86AudioStop , exec , ${pkgs.playerctl}/bin/playerctl stop"
+    ]
+    ++ lib.optionals (osConfig.myOptions.firefox.enable) [
+      "$mod      , G , exec , uwsm app -- firefox"
+    ]
+    ++ lib.optionals (osConfig.myOptions.discord.enable) [
+      "$mod      , D , exec , uwsm app -- vesktop"
+    ]
+    ++ lib.optionals (osConfig.myOptions.spotify.enable) [
+      "$mod      , S , exec , uwsm app -- spotify"
+    ]
+    ++ lib.optionals (config.programs.emacs.enable) [
+      "$mod      , E , exec , uwsm app -- \"${emacs}\""
+      "ALT       , E , exec , uwsm app -- \"emacsclient -c -eval '(dired nil)'\""
+    ];
 
   binde = [
     ", XF86AudioRaiseVolume , exec , ${pkgs.alsa-utils}/bin/amixer -q set Master 5%+"
