e38e87232caa03751393a25d526ef20d511136e3
Ludovico Piero <lewdovico@gnuweeb.org>
2024-11-26 22:24:13 +0900

    modules/shared/users: add more groups
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/shared/users.nix
+++ modules/shared/users.nix
@@ -1,4 +1,8 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   sops = {
     secrets."users/userPassword".neededForUsers = true;
     secrets."users/rootPassword".neededForUsers = true;
@@ -11,10 +15,15 @@
       hashedPasswordFile = config.sops.secrets."users/userPassword".path;
       uid = 1001;
       isNormalUser = true;
-      extraGroups = [
-        "video"
-        "wheel"
-      ];
+      extraGroups =
+        [
+          "seat"
+          "video"
+          "wheel"
+        ]
+        ++ lib.optional config.virtualisation.libvirtd.enable "libvirtd"
+        ++ lib.optional config.virtualisation.docker.enable "docker"
+        ++ lib.optional config.networking.networkmanager.enable "networkmanager";
     };
   };
 }
