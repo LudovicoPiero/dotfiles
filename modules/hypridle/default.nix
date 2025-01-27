5d3b1aa244dac2e12f48f8ac746b835826ee9b04
Ludovico Piero <lewdovico@gnuweeb.org>
2025-01-27 09:52:24 +0900

    modules/hypridle: set timeout to 300
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/hypridle/default.nix
+++ modules/hypridle/default.nix
@@ -32,11 +32,11 @@ in {
 
           listener = [
             {
-              timeout = 600;
+              timeout = 300;
               on-timeout = "hyprlock";
             }
             {
-              timeout = 650;
+              timeout = 350;
               on-timeout = "hyprctl dispatch dpms off";
               on-resume = "hyprctl dispatch dpms on";
             }
