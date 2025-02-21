56978baddab02ef289a408036788083044232cd0
Ludovico Piero <lewdovico@gnuweeb.org>
2025-02-22 05:00:31 +0900

    fix(firefox,floorp): force extensions settings
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/firefox/extensions.nix
+++ modules/firefox/extensions.nix
@@ -9,29 +9,32 @@
     ublock-origin
   ];
   settings = {
-    "uBlock0@raymondhill.net".settings = {
-      advancedUserEnabled = true;
-      selectedFilterLists = [
-        "user-filters"
-        "ublock-filters"
-        "ublock-badware"
-        "ublock-privacy"
-        "ublock-unbreak"
-        "ublock-quick-fixes"
-        "easylist"
-        "easyprivacy"
-        "urlhaus-1"
-        "plowe-0"
-        "adguard-spyware-url"
-        "fanboy-cookiemonster"
-        "ublock-cookies-easylist"
-        "easylist-annoyances"
-        "easylist-chat"
-        "easylist-newsletters"
-        "easylist-notifications"
-        "ublock-annoyances"
-        "IDN-0"
-      ];
+    "uBlock0@raymondhill.net" = {
+      force = true;
+      settings = {
+        advancedUserEnabled = true;
+        selectedFilterLists = [
+          "user-filters"
+          "ublock-filters"
+          "ublock-badware"
+          "ublock-privacy"
+          "ublock-unbreak"
+          "ublock-quick-fixes"
+          "easylist"
+          "easyprivacy"
+          "urlhaus-1"
+          "plowe-0"
+          "adguard-spyware-url"
+          "fanboy-cookiemonster"
+          "ublock-cookies-easylist"
+          "easylist-annoyances"
+          "easylist-chat"
+          "easylist-newsletters"
+          "easylist-notifications"
+          "ublock-annoyances"
+          "IDN-0"
+        ];
+      };
     };
   };
 }
