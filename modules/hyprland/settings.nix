88533201b824b13404b97a75cc7c66f6d9595db5
Ludovico Piero <lewdovico@gnuweeb.org>
2025-03-11 16:30:34 +0900

    feat(modules/hyprland): change animations
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/hyprland/settings.nix
+++ modules/hyprland/settings.nix
@@ -41,23 +41,46 @@ in
   ];
 
   monitor = [
-    "eDP-1, 1366x768@60, 1366x0 ,1"
-    "HDMI-A-1, 1920x1080@180, 0x0, 1.5"
+    "eDP-1, 1366x768@60, auto-right, 1"
+    "HDMI-A-1, 1920x1080@180, auto-left, 1"
   ];
 
   animations = {
     enabled = true;
 
-    bezier = [ "myBezier, 0.05, 0.9, 0.1, 1.05" ];
+    bezier = [
+      "myBezier, 0.05, 0.9, 0.1, 1.05"
+      "easeOutQuint, 0.23, 1, 0.32,1"
+      "easeInOutCubic, 0.65, 0.05, 0.36, 1"
+      "linear, 0, 0, 1, 1"
+      "almostLinear, 0.5, 0.5, 0.75, 1.0"
+      "quick, 0.15, 0, 0.1, 1"
+    ];
 
     animation = [
-      "windows, 1, 7, myBezier"
-      "windowsOut, 1, 7, default, popin 80%"
-      "windowsMove, 1, 2, default, popin 80%"
-      "border, 1, 10, default"
-      "borderangle, 1, 8, default"
-      "fade, 1, 7, default"
-      "workspaces, 1, 6, default"
+      # "windows, 1, 7, myBezier"
+      # "windowsOut, 1, 7, default, popin 80%"
+      # "windowsMove, 1, 2, default, popin 80%"
+      # "border, 1, 10, default"
+      # "borderangle, 1, 8, default"
+      # "fade, 1, 7, default"
+      # "workspaces, 1, 6, default"
+      "global, 1, 10, default"
+      "border, 1, 5.39, easeOutQuint"
+      "windows, 1, 4.79, easeOutQuint"
+      "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
+      "windowsOut, 1, 1.49, linear, popin 87%"
+      "fadeIn, 1, 1.73, almostLinear"
+      "fadeOut, 1, 1.46, almostLinear"
+      "fade, 1, 3.03, quick"
+      "layers, 1, 3.81, easeOutQuint"
+      "layersIn, 1, 4, easeOutQuint, fade"
+      "layersOut, 1, 1.5, linear, fade"
+      "fadeLayersIn, 1, 1.79, almostLinear"
+      "fadeLayersOut, 1, 1.39, almostLinear"
+      "workspaces, 1, 1.94, almostLinear, fade"
+      "workspacesIn, 1, 1.21, almostLinear, fade"
+      "workspacesOut, 1, 1.94, almostLinear, fade"
     ];
   };
 
@@ -148,6 +171,15 @@ in
   workspace = [
     "w[tv1], gapsout:0, gapsin:0"
     "f[1], gapsout:0, gapsin:0"
+    "1, monitor:HDMI-A-1"
+    "2, monitor:HDMI-A-1"
+    "3, monitor:HDMI-A-1"
+    "4, monitor:HDMI-A-1"
+    "5, monitor:HDMI-A-1"
+    "6, monitor:HDMI-A-1"
+    "7, monitor:HDMI-A-1"
+    "8, monitor:HDMI-A-1"
+    "9, monitor:HDMI-A-1"
   ];
 
   windowrulev2 = [
