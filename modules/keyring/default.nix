c0af1097148c6c8cd9527c9d4adf8e8881653a54
Ludovico Piero <lewdovico@gnuweeb.org>
2025-03-15 06:23:34 +0900

    feat(modules/keyring): pantheon-agent-polkit -> hyprpolkitagent
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/keyring/default.nix
+++ modules/keyring/default.nix
@@ -36,20 +36,11 @@ in
     };
     security.polkit.enable = true;
 
-    systemd = {
-      user.services.pantheon-agent-polkit = {
-        description = "pantheon-agent-polkit";
-        wantedBy = [ "graphical-session.target" ];
-        wants = [ "graphical-session.target" ];
-        after = [ "graphical-session.target" ];
-        serviceConfig = {
-          Type = "simple";
-          ExecStart = "${pkgs.pantheon.pantheon-agent-polkit}/libexec/policykit-1-pantheon/io.elementary.desktop.agent-polkit";
-          Restart = "on-failure";
-          RestartSec = 1;
-          TimeoutStopSec = 10;
-        };
-      };
+    systemd.packages = [ pkgs.hyprpolkitagent ];
+    systemd.user.services.hyprpolkitagent = {
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
     };
   };
 }
