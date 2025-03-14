e96d149c38c96a1ba27cb3eea0d6948cec7aa12f
Ludovico Piero <lewdovico@gnuweeb.org>
2025-03-14 10:52:43 +0900

    fix(modules/fcitx5): whoopsie
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/fcitx5/default.nix
+++ modules/fcitx5/default.nix
@@ -46,7 +46,7 @@ in
           addons = with pkgs; [
             fcitx5-mozc
             fcitx5-gtk
-            inputs.self.packages.${system}.catppuccin-fcitx5 # Theme
+            inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.catppuccin-fcitx5 # Theme
             libsForQt5.fcitx5-qt
           ];
         };
