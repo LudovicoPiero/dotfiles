159778c9218933afeaae8171e86b6ce9e1558857
Ludovico Piero <lewdovico@gnuweeb.org>
2025-03-15 21:30:16 +0900

    modules: update security settings
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/shared/security.nix
+++ modules/shared/security.nix
@@ -4,90 +4,86 @@
   ...
 }:
 {
-  # sets hidepid=2 on /proc (make process info visible only to owning user)
-  # NOTE Was removed on nixpkgs-unstable because it doesn't do anything
-  # security.hideProcessInformation = true;
-  # Prevent replacing the running kernel w/o reboot
-  # security.protectKernelImage = true;
-
   boot = {
-    # tmpfs = /tmp is mounted in ram. Doing so makes temp file management speedy
-    # on ssd systems, and volatile! Because it's wiped on reboot.
+    # tmpfs = /tmp is mounted in RAM for speed and volatility.
     tmp.useTmpfs = lib.mkDefault true;
-
-    # If not using tmpfs, which is naturally purged on reboot, we must clean it
-    # /tmp ourselves. /tmp should be volatile storage!
+    # Clean /tmp on boot if tmpfs is not in use.
     tmp.cleanOnBoot = lib.mkDefault (!config.boot.tmp.useTmpfs);
 
-    # Fix a security hole in place for backwards compatibility. See desc in
-    # nixpkgs/nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix
+    # Disable systemd-boot editor to fix a backwards compatibility security issue.
     loader.systemd-boot.editor = false;
 
     kernel.sysctl = {
-      # The Magic SysRq key is a key combo that allows users connected to the
-      # system console of a Linux kernel to perform some low-level commands.
-      # Disable it, since we don't need it, and is a potential security concern.
+      # Disable Magic SysRq key for security.
       "kernel.sysrq" = 0;
-
-      # Disable NMI watchdog
+      # Disable NMI watchdog.
       "kernel.nmi_watchdog" = 0;
-
-      # To hide any kernel messages from the console
+      # Hide kernel messages from the console.
       "kernel.printk" = "3 3 3 3";
-
-      # Only swap when absolutely necessary
+      # Minimize swap usage.
       "vm.swappiness" = "1";
 
+      # Restrict kernel pointer exposure to mitigate potential kernel info leaks.
+      "kernel.kptr_restrict" = 2;
+
+      # Limit ptrace scope to prevent unprivileged processes from debugging others.
+      "kernel.yama.ptrace_scope" = 1;
+
+      # Protect against hardlink and symlink attacks.
+      "fs.protected_hardlinks" = 1;
+      "fs.protected_symlinks" = 1;
+
+      # Disable kexec loading to prevent replacing the running kernel without a reboot.
+      "kernel.kexec_load_disabled" = 1;
+
+      # Disable core dumps for setuid programs to protect sensitive information.
+      "fs.suid_dumpable" = 0;
+
+      # Restrict performance event usage to mitigate potential side-channel attacks.
+      "kernel.perf_event_paranoid" = 2;
+
       ## TCP hardening
-      # Prevent bogus ICMP errors from filling up logs.
+      # Ignore bogus ICMP error responses.
       "net.ipv4.icmp_ignore_bogus_error_responses" = 1;
-      # Reverse path filtering causes the kernel to do source validation of
-      # packets received from all interfaces. This can mitigate IP spoofing.
+      # Enable reverse path filtering.
       "net.ipv4.conf.default.rp_filter" = 1;
       "net.ipv4.conf.all.rp_filter" = 1;
-      # Do not accept IP source route packets (we're not a router)
+      # Do not accept source route packets.
       "net.ipv4.conf.all.accept_source_route" = 0;
       "net.ipv6.conf.all.accept_source_route" = 0;
-      # Don't send ICMP redirects (again, we're on a router)
+      # Disable sending redirects.
       "net.ipv4.conf.all.send_redirects" = 0;
       "net.ipv4.conf.default.send_redirects" = 0;
-      # Refuse ICMP redirects (MITM mitigations)
+      # Refuse and disable ICMP redirects.
       "net.ipv4.conf.all.accept_redirects" = 0;
       "net.ipv4.conf.default.accept_redirects" = 0;
       "net.ipv4.conf.all.secure_redirects" = 0;
       "net.ipv4.conf.default.secure_redirects" = 0;
       "net.ipv6.conf.all.accept_redirects" = 0;
       "net.ipv6.conf.default.accept_redirects" = 0;
-      # Protects against SYN flood attacks
+      # Protect against SYN flood attacks.
       "net.ipv4.tcp_syncookies" = 1;
-      # Incomplete protection again TIME-WAIT assassination
+      # Incomplete protection for TIME-WAIT assassination.
       "net.ipv4.tcp_rfc1337" = 1;
-
-      ## TCP optimization
-      # TCP Fast Open is a TCP extension that reduces network latency by packing
-      # data in the sender’s initial TCP SYN. Setting 3 = enable TCP Fast Open for
-      # both incoming and outgoing connections:
+      # Enable TCP Fast Open.
       "net.ipv4.tcp_fastopen" = 3;
-      # Bufferbloat mitigations + slight improvement in throughput & latency
+      # Use BBR for congestion control and CAKE for queueing.
       "net.ipv4.tcp_congestion_control" = "bbr";
       "net.core.default_qdisc" = "cake";
     };
 
+    # Enable BBR kernel module.
     kernelModules = [ "tcp_bbr" ];
 
+    # Blacklist unnecessary or potentially insecure kernel modules.
     blacklistedKernelModules = [
-      # Novideo
       "nvidia"
       "nvidia-drm"
       "nvidia-modeset"
       "nouveau"
-
-      # Obscure network protocols
       "ax25"
       "netrom"
       "rose"
-
-      # Old or rare or insufficiently audited filesystems
       "adfs"
       "affs"
       "bfs"
@@ -124,6 +120,6 @@
     ];
   };
 
-  # So we don't have to do this later...
+  # Automatically accept ACME terms.
   security.acme.acceptTerms = true;
 }
