519d31f21aea1eb5f8d78c6c22a729e342a7e043
Ludovico Piero <lewdovico@gnuweeb.org>
2025-03-15 10:54:40 +0900

    feat(modules/wezterm): use opacity from vars
    Require some hacky trick that I stole from stylix xd
    
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/wezterm/default.nix
+++ modules/wezterm/default.nix
@@ -92,114 +92,130 @@ in
               };
             };
           };
-          extraConfig = ''
-            return {
-              font = wezterm.font_with_fallback({
-                "${osConfig.myOptions.vars.mainFont}",
-                "Material Design Icons",
-                "Noto Color Emoji",
-              }),
-              enable_wayland = true,
-              enable_scroll_bar = false,
-              enable_kitty_keyboard = true,
-              check_for_updates = false,
-              default_cursor_style = "SteadyBlock",
-              enable_tab_bar = true,
-              hide_tab_bar_if_only_one_tab = true,
-              scrollback_lines = 10000,
-              adjust_window_size_when_changing_font_size = false,
-              audible_bell = "Disabled",
-              use_fancy_tab_bar = false,
-              clean_exit_codes = { 130 },
-              window_background_opacity = "${toString osConfig.myOptions.vars.opacity}",
-              color_scheme = "${colorScheme.slug}",
-              window_frame = {
-                active_titlebar_bg = "#${palette.base03}",
-                active_titlebar_fg = "#${palette.base05}",
-                active_titlebar_border_bottom = "#${palette.base03}",
-                border_left_color = "#${palette.base01}",
-                border_right_color = "#${palette.base01}",
-                border_bottom_color = "#${palette.base01}",
-                border_top_color = "#${palette.base01}",
-                button_bg = "#${palette.base01}",
-                button_fg = "#${palette.base05}",
-                button_hover_bg = "#${palette.base05}",
-                button_hover_fg = "#${palette.base03}",
-                inactive_titlebar_bg = "#${palette.base01}",
-                inactive_titlebar_fg = "#${palette.base05}",
-                inactive_titlebar_border_bottom = "#${palette.base03}",
-              },
-              colors = {
-                tab_bar = {
-                  background = "#${palette.base01}",
-                  inactive_tab_edge = "#${palette.base01}",
-                  active_tab = {
-                    bg_color = "#${palette.base00}",
-                    fg_color = "#${palette.base05}",
-                  },
-                  inactive_tab = {
-                    bg_color = "#${palette.base03}",
-                    fg_color = "#${palette.base05}",
-                  },
-                  inactive_tab_hover = {
-                    bg_color = "#${palette.base05}",
-                    fg_color = "#${palette.base00}",
-                  },
-                  new_tab = {
-                    bg_color = "#${palette.base03}",
-                    fg_color = "#${palette.base05}",
-                  },
-                  new_tab_hover = {
-                    bg_color = "#${palette.base05}",
-                    fg_color = "#${palette.base00}",
-                  },
-                },
-              },
-              command_palette_bg_color = "#${palette.base01}",
-              command_palette_fg_color = "#${palette.base05}",
-              command_palette_font_size = 12.0,
-
-              leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
-              keys = {
-                { key = "UpArrow",   mods = "SHIFT",  action = wezterm.action({ ScrollByLine = -1 }) },
-                { key = "DownArrow", mods = "SHIFT",  action = wezterm.action({ ScrollByLine = 1 }) },
-                { key = "h",         mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
-                { key = "l",         mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
-                { key = "j",         mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
-                { key = "k",         mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
-                {
-                  key = "v",
-                  mods = "LEADER",
-                  action = wezterm.action.SplitPane({
-                    top_level = true,
-                    direction = "Down",
-                    size = { Percent = 30 },
-                  }),
-                },
-                {
-                  key = ";",
-                  mods = "LEADER",
-                  action = wezterm.action.SplitPane({
-                    top_level = true,
-                    direction = "Right",
-                    size = { Percent = 30 },
-                  }),
-                },
-                { key = "c", mods = "LEADER",      action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
-                { key = "1", mods = "LEADER",      action = wezterm.action({ ActivateTab = 0 }) },
-                { key = "2", mods = "LEADER",      action = wezterm.action({ ActivateTab = 1 }) },
-                { key = "3", mods = "LEADER",      action = wezterm.action({ ActivateTab = 2 }) },
-                { key = "4", mods = "LEADER",      action = wezterm.action({ ActivateTab = 3 }) },
-                { key = "5", mods = "LEADER",      action = wezterm.action({ ActivateTab = 4 }) },
-                { key = "6", mods = "LEADER",      action = wezterm.action({ ActivateTab = 5 }) },
-                { key = "7", mods = "LEADER",      action = wezterm.action({ ActivateTab = 6 }) },
-                { key = "8", mods = "LEADER",      action = wezterm.action({ ActivateTab = 7 }) },
-                { key = "9", mods = "LEADER",      action = wezterm.action({ ActivateTab = -1 }) },
-                { key = "a", mods = "LEADER|CTRL", action = wezterm.action({ SendString = "\x01" }) },
-              },
-            }
-          '';
         };
+
+        # Stolen from stylix
+        # https://github.com/danth/stylix/blob/master/modules/wezterm/hm.nix
+        xdg.configFile."wezterm/wezterm.lua".text = lib.mkForce ''
+          local wezterm = require("wezterm")
+          wezterm.add_to_config_reload_watch_list(wezterm.config_dir)
+
+          local function stylix_wrapped_config()
+              ${config.programs.wezterm.extraConfig}
+          end
+
+          local stylix_base_config = wezterm.config_builder()
+          local stylix_user_config = stylix_wrapped_config()
+          stylix_base_config = {
+            font = wezterm.font_with_fallback({
+              "${osConfig.myOptions.vars.mainFont}",
+              "Material Design Icons",
+              "Noto Color Emoji",
+            }),
+            enable_wayland = true,
+            enable_scroll_bar = false,
+            enable_kitty_keyboard = true,
+            check_for_updates = false,
+            default_cursor_style = "SteadyBlock",
+            enable_tab_bar = true,
+            hide_tab_bar_if_only_one_tab = true,
+            scrollback_lines = 10000,
+            adjust_window_size_when_changing_font_size = false,
+            audible_bell = "Disabled",
+            use_fancy_tab_bar = false,
+            clean_exit_codes = { 130 },
+            window_background_opacity = ${toString osConfig.myOptions.vars.opacity},
+            color_scheme = "${colorScheme.slug}",
+            window_frame = {
+              active_titlebar_bg = "#${palette.base03}",
+              active_titlebar_fg = "#${palette.base05}",
+              active_titlebar_border_bottom = "#${palette.base03}",
+              border_left_color = "#${palette.base01}",
+              border_right_color = "#${palette.base01}",
+              border_bottom_color = "#${palette.base01}",
+              border_top_color = "#${palette.base01}",
+              button_bg = "#${palette.base01}",
+              button_fg = "#${palette.base05}",
+              button_hover_bg = "#${palette.base05}",
+              button_hover_fg = "#${palette.base03}",
+              inactive_titlebar_bg = "#${palette.base01}",
+              inactive_titlebar_fg = "#${palette.base05}",
+              inactive_titlebar_border_bottom = "#${palette.base03}",
+            },
+            colors = {
+              tab_bar = {
+                background = "#${palette.base01}",
+                inactive_tab_edge = "#${palette.base01}",
+                active_tab = {
+                  bg_color = "#${palette.base00}",
+                  fg_color = "#${palette.base05}",
+                },
+                inactive_tab = {
+                  bg_color = "#${palette.base03}",
+                  fg_color = "#${palette.base05}",
+                },
+                inactive_tab_hover = {
+                  bg_color = "#${palette.base05}",
+                  fg_color = "#${palette.base00}",
+                },
+                new_tab = {
+                  bg_color = "#${palette.base03}",
+                  fg_color = "#${palette.base05}",
+                },
+                new_tab_hover = {
+                  bg_color = "#${palette.base05}",
+                  fg_color = "#${palette.base00}",
+                },
+              },
+            },
+            command_palette_bg_color = "#${palette.base01}",
+            command_palette_fg_color = "#${palette.base05}",
+            command_palette_font_size = 12.0,
+
+            leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
+            keys = {
+              { key = "UpArrow",   mods = "SHIFT",  action = wezterm.action({ ScrollByLine = -1 }) },
+              { key = "DownArrow", mods = "SHIFT",  action = wezterm.action({ ScrollByLine = 1 }) },
+              { key = "h",         mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+              { key = "l",         mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+              { key = "j",         mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+              { key = "k",         mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+              {
+                key = "v",
+                mods = "LEADER",
+                action = wezterm.action.SplitPane({
+                  top_level = true,
+                  direction = "Down",
+                  size = { Percent = 30 },
+                }),
+              },
+              {
+                key = ";",
+                mods = "LEADER",
+                action = wezterm.action.SplitPane({
+                  top_level = true,
+                  direction = "Right",
+                  size = { Percent = 30 },
+                }),
+              },
+              { key = "c", mods = "LEADER",      action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+              { key = "1", mods = "LEADER",      action = wezterm.action({ ActivateTab = 0 }) },
+              { key = "2", mods = "LEADER",      action = wezterm.action({ ActivateTab = 1 }) },
+              { key = "3", mods = "LEADER",      action = wezterm.action({ ActivateTab = 2 }) },
+              { key = "4", mods = "LEADER",      action = wezterm.action({ ActivateTab = 3 }) },
+              { key = "5", mods = "LEADER",      action = wezterm.action({ ActivateTab = 4 }) },
+              { key = "6", mods = "LEADER",      action = wezterm.action({ ActivateTab = 5 }) },
+              { key = "7", mods = "LEADER",      action = wezterm.action({ ActivateTab = 6 }) },
+              { key = "8", mods = "LEADER",      action = wezterm.action({ ActivateTab = 7 }) },
+              { key = "9", mods = "LEADER",      action = wezterm.action({ ActivateTab = -1 }) },
+              { key = "a", mods = "LEADER|CTRL", action = wezterm.action({ SendString = "\x01" }) },
+            },
+          }
+          for key, value in pairs(stylix_user_config) do
+              stylix_base_config[key] = value
+          end
+          return stylix_base_config
+        '';
       }; # For Home-Manager options
   };
 }
