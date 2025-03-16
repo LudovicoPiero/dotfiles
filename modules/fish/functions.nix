28555c0dc907d9306943893af2311bc049bc1c2d
Ludovico Piero <lewdovico@gnuweeb.org>
2025-03-17 05:31:47 +0900

    fish: add more functions
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/fish/functions.nix
+++ modules/fish/functions.nix
@@ -81,5 +81,49 @@ with pkgs;
     end
   '';
 
-  "watchLive" = "${_ streamlink} --player ${_ mpv} --twitch-low-latency  $argv best";
+  "watchLive" = ''
+    if test (count $argv) -ge 2
+        set quality $argv[2]
+    else
+        set quality "best"
+    end
+
+    ${_ streamlink} --player ${_ mpv} $argv[1] $quality
+  '';
+
+  "mkcd" = ''
+    ${__ uutils-coreutils-noprefix "mkdir"} -p $argv[1]
+    if test -d "$argv[1]"
+        cd $argv[1]
+    end
+  '';
+
+  extract = ''
+    switch "$argv[1]"
+        case '*.tar.bz2'
+            ${__ gnutar "tar"} xvjf "$argv[1]"
+        case '*.tar.gz'
+            ${__ gnutar "tar"} xvzf "$argv[1]"
+        case '*.bz2'
+            ${__ bzip2 "bunzip2"} "$argv[1]"
+        case '*.rar'
+            ${_ unrar} x "$argv[1]"
+        case '*.gz'
+            ${__ gzip "gunzip"} "$argv[1]"
+        case '*.tar'
+            ${__ gnutar "tar"} xvf "$argv[1]"
+        case '*.tbz2'
+            ${__ gnutar "tar"} xvjf "$argv[1]"
+        case '*.tgz'
+            ${__ gnutar "tar"} xvzf "$argv[1]"
+        case '*.zip'
+            ${_ unzip} "$argv[1]"
+        case '*.Z'
+            ${__ gzip "uncompress"} "$argv[1]"
+        case '*.7z'
+            ${__ p7zip "7z"} x "$argv[1]"
+        case '*'
+            echo "Cannot extract '$argv[1]' via extract"
+    end
+  '';
 }
