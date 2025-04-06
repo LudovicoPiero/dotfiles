d5c89ac473fcc1950889c23e4e2ec619bc62a683
Ludovico Piero <lewdovico@gnuweeb.org>
2025-04-07 04:19:33 +0900

    {firefox,floorp}: remove betterfox
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/floorp/default.nix
+++ modules/floorp/default.nix
@@ -37,12 +37,6 @@ in
             }
             // (
               let
-                betterfox = pkgs.fetchFromGitHub {
-                  owner = "yokoffing";
-                  repo = "Betterfox";
-                  rev = "135.0";
-                  hash = "sha256-5fD8ffAyIgQYJ0Z/bMEpqf17YghVQNaK+giZ1Tyk6/Q=";
-                };
                 lepton = pkgs.fetchFromGitHub {
                   owner = "black7375";
                   repo = "Firefox-UI-Fix";
@@ -55,34 +49,24 @@ in
                 userContent = ''@import "${lepton}/css/leptonContentESR.css";'';
                 extraConfig = ''
                   ${builtins.readFile "${lepton}/user.js"}
-                  ${builtins.readFile "${betterfox}/user.js"}
-                  ${builtins.readFile "${betterfox}/Fastfox.js"}
-                  ${builtins.readFile "${betterfox}/Peskyfox.js"}
-                  ${builtins.readFile "${betterfox}/Securefox.js"}
-                  ${builtins.readFile "${betterfox}/Smoothfox.js"}
 
-                  /* Betterfox overrides */
-                  // PREF: disable address and credit card manager
-                  user_pref("extensions.formautofill.addresses.enabled", false);
-                  user_pref("extensions.formautofill.creditCards.enabled", false);
+                  // // PREF: disable address and credit card manager
+                  // user_pref("extensions.formautofill.addresses.enabled", false);
+                  // user_pref("extensions.formautofill.creditCards.enabled", false);
 
-                  // PREF: enable HTTPS-Only Mode
-                  // Warn me before loading sites that don't support HTTPS
-                  // when using Private Browsing windows.
-                  user_pref("dom.security.https_only_mode_pbm", true);
-                  user_pref("dom.security.https_only_mode_error_page_user_suggestions", true);
+                  // // PREF: enable HTTPS-Only Mode
+                  // // Warn me before loading sites that don't support HTTPS
+                  // // when using Private Browsing windows.
+                  // user_pref("dom.security.https_only_mode_pbm", true);
+                  // user_pref("dom.security.https_only_mode_error_page_user_suggestions", true);
 
-                  // PREF: disable Firefox Sync
-                  user_pref("identity.fxaccounts.enabled", false);
+                  // // PREF: disable the Firefox View tour from popping up
+                  // user_pref("browser.firefox-view.feature-tour", "{\"screen\":\"\",\"complete\":true}");
 
-                  // PREF: disable the Firefox View tour from popping up
-                  user_pref("browser.firefox-view.feature-tour", "{\"screen\":\"\",\"complete\":true}");
-
-                  // Fix Strict ETP issue
-                  user_pref("browser.contentblocking.category", "standard");
+                  // // Fix Strict ETP issue
+                  // user_pref("browser.contentblocking.category", "standard");
 
                   /* Custom User.js */
-                  user_pref("browser.tabs.firefox-view-next", false);
                   user_pref("privacy.sanitize.sanitizeOnShutdown", false);
                   user_pref("privacy.clearOnShutdown.cache", false);
                   user_pref("privacy.clearOnShutdown.cookies", false);
