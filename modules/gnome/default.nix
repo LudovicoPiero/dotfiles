cccad06e82870ecb8e81e4c9607c9e9be0f94ad1
Ludovico Piero <lewdovico@gnuweeb.org>
2025-04-09 14:36:33 +0900

    gnome: cleanup
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/gnome/default.nix
+++ modules/gnome/default.nix
@@ -10,6 +10,57 @@ let
     mkIf
     ;
 
+  excludedPackages = with pkgs; [
+    orca
+    evince
+    # file-roller
+    geary
+    gnome-disk-utility
+    # seahorse
+    # sushi
+    # sysprof
+    #
+    # gnome-shell-extensions
+    #
+    # adwaita-icon-theme
+    # nixos-background-info
+    # gnome-backgrounds
+    # gnome-bluetooth
+    # gnome-color-manager
+    # gnome-control-center
+    gnome-tour # GNOME Shell detects the .desktop file on first log-in.
+    gnome-user-docs
+    glib # for gsettings program
+    # gnome-menus
+    # gtk3.out # for gtk-launch program
+    # xdg-user-dirs # Update user dirs as described in https://freedesktop.org/wiki/Software/xdg-user-dirs/
+    # xdg-user-dirs-gtk # Used to create the default bookmarks
+    #
+    baobab
+    epiphany
+    gnome-text-editor
+    gnome-calculator
+    gnome-calendar
+    gnome-characters
+    # gnome-clocks
+    gnome-console
+    gnome-contacts
+    gnome-font-viewer
+    gnome-logs
+    gnome-maps
+    gnome-music
+    # gnome-system-monitor
+    gnome-weather
+    # loupe
+    # nautilus
+    gnome-connections
+    simple-scan
+    snapshot
+    totem
+    yelp
+    gnome-software
+  ];
+
   cfg = config.myOptions.gnome;
 in
 {
@@ -19,57 +70,7 @@ in
 
   config = mkIf cfg.enable {
     services.xserver.desktopManager.gnome.enable = true;
-
-    environment.gnome.excludePackages = with pkgs; [
-      orca
-      evince
-      # file-roller
-      geary
-      gnome-disk-utility
-      # seahorse
-      # sushi
-      # sysprof
-      #
-      # gnome-shell-extensions
-      #
-      # adwaita-icon-theme
-      # nixos-background-info
-      # gnome-backgrounds
-      # gnome-bluetooth
-      # gnome-color-manager
-      # gnome-control-center
-      gnome-tour # GNOME Shell detects the .desktop file on first log-in.
-      gnome-user-docs
-      glib # for gsettings program
-      # gnome-menus
-      # gtk3.out # for gtk-launch program
-      # xdg-user-dirs # Update user dirs as described in https://freedesktop.org/wiki/Software/xdg-user-dirs/
-      # xdg-user-dirs-gtk # Used to create the default bookmarks
-      #
-      baobab
-      epiphany
-      gnome-text-editor
-      gnome-calculator
-      gnome-calendar
-      gnome-characters
-      # gnome-clocks
-      gnome-console
-      gnome-contacts
-      gnome-font-viewer
-      gnome-logs
-      gnome-maps
-      gnome-music
-      # gnome-system-monitor
-      gnome-weather
-      # loupe
-      # nautilus
-      gnome-connections
-      simple-scan
-      snapshot
-      totem
-      yelp
-      gnome-software
-    ];
+    environment.gnome.excludePackages = excludedPackages;
 
     home-manager.users.${config.vars.username} =
       { config, osConfig, ... }:
@@ -115,8 +116,10 @@ in
               arcmenu.extensionUuid
               # blur-my-shell.extensionUuid
               caffeine.extensionUuid
+              clipboard-indicator.extensionUuid
               dash-to-panel.extensionUuid
               kimpanel.extensionUuid
+              system-monitor.extensionUuid
               user-themes.extensionUuid
             ];
           };
@@ -159,8 +162,10 @@ in
           arcmenu
           # blur-my-shell
           caffeine
+          clipboard-indicator
           dash-to-panel
           kimpanel
+          system-monitor
           user-themes
         ];
       };
