a6501b47e973cba1bd1c3a603f8b010346efb7a0
Ludovico Piero <lewdovico@gnuweeb.org>
2025-05-05 03:02:14 +0900

    mako: move settings
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/mako/default.nix
+++ modules/mako/default.nix
@@ -27,21 +27,23 @@ in
         services.mako = {
           enable = true;
 
-          font = "${osConfig.myOptions.fonts.terminal.name} ${toString osConfig.myOptions.fonts.size}";
-          backgroundColor = "#${palette.base00}";
-          borderColor = "#${palette.base0E}";
-          textColor = "#${palette.base05}";
-          progressColor = "over #${palette.base02}";
+          settings = {
+            font = "${osConfig.myOptions.fonts.terminal.name} ${toString osConfig.myOptions.fonts.size}";
+            backgroundColor = "#${palette.base00}";
+            borderColor = "#${palette.base0E}";
+            textColor = "#${palette.base05}";
+            progressColor = "over #${palette.base02}";
 
-          anchor = "top-right";
-          borderRadius = "5";
-          borderSize = "2";
-          padding = "20";
-          defaultTimeout = "5000";
-          layer = "top";
-          height = "100";
-          width = "300";
-          format = "<b>%s</b>\\n%b";
+            anchor = "top-right";
+            borderRadius = "5";
+            borderSize = "2";
+            padding = "20";
+            defaultTimeout = "5000";
+            layer = "top";
+            height = "100";
+            width = "300";
+            format = "<b>%s</b>\\n%b";
+          };
         };
       }; # For Home-Manager options
   };
