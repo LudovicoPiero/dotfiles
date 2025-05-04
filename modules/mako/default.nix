6d402c051f86d4bda36524ea281a655945dac40f
Ludovico Piero <lewdovico@gnuweeb.org>
2025-05-05 02:18:54 +0900

    mako: update config
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/mako/default.nix
+++ modules/mako/default.nix
@@ -34,29 +34,14 @@ in
           progressColor = "over #${palette.base02}";
 
           anchor = "top-right";
-          borderRadius = 5;
-          borderSize = 2;
+          borderRadius = "5";
+          borderSize = "2";
           padding = "20";
-          defaultTimeout = 5000;
+          defaultTimeout = "5000";
           layer = "top";
-          height = 100;
-          width = 300;
+          height = "100";
+          width = "300";
           format = "<b>%s</b>\\n%b";
-
-          extraConfig = ''
-            [urgency=low]
-            border-color=#${palette.base0B}
-            background-color=#${palette.base01}
-            default-timeout=3000
-
-            [urgency=high]
-            background-color=#${palette.base01}
-            border-color=#${palette.base0B}
-            default-timeout=10000
-
-            [mode=dnd]
-            invisible=1
-          '';
         };
       }; # For Home-Manager options
   };
