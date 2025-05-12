9ad4e99ddc451fce64ccf6d16e4a20d62dfdf9d3
Ludovico Piero <lewdovico@gnuweeb.org>
2025-05-12 14:08:22 +0900

    fish: starship -> pure
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/fish/default.nix
+++ modules/fish/default.nix
@@ -66,80 +66,23 @@ in
             function fish_greeting
             end
 
-            set --universal async_prompt_functions starship_prompt
 
-            ${_ nix-your-shell} fish | source
+            set --universal pure_check_for_new_release false
+            set --universal pure_enable_nixdevshell true
+            set --universal pure_show_prefix_root_prompt true
+
             ${_ zoxide} init fish | source
+            ${_ fzf} --fish | source
             ${_ direnv} hook fish | source
-            ${_ starship} init fish | source
+            ${_ nix-your-shell} fish | source
           '';
 
           plugins = {
             inherit (pkgs.fishPlugins) async-prompt;
-          };
-        };
-
-        starship = {
-          enable = true;
-          settings = lib.mkDefault {
-            format = lib.concatStrings [
-              "$username"
-              "$hostname"
-              "$directory"
-              "$git_branch"
-              "$git_state"
-              "$git_status"
-              " "
-              "$cmd_duration"
-              "$line_break"
-              "$nix_shell"
-              "$python"
-              "$character"
-            ];
-
-            directory.style = "blue";
-
-            character = {
-              success_symbol = "[❯](purple)";
-              error_symbol = "[❯](red)";
-              vimcmd_symbol = "[❮](green)";
-            };
-
-            git_branch = {
-              format = "[$branch]($style)";
-              style = "bright-black";
-            };
-
-            git_status = {
-              format = "[[(*$conflicted $untracked $modified $staged $renamed $deleted)](218) ($ahead_behind $stashed)]($style)";
-              style = "cyan";
-              conflicted = "";
-              untracked = "";
-              modified = "";
-              staged = "";
-              renamed = "";
-              deleted = "";
-              stashed = "≡";
-            };
-
-            git_state = {
-              format = "\([$state( $progress_current/$progress_total)]($style)\)";
-              style = "bright-black";
-            };
-
-            cmd_duration = {
-              format = "[$duration]($style)";
-              style = "yellow";
-            };
-
-            python = {
-              format = "[$virtualenv]($style) ";
-              style = "bright-black";
-            };
-
-            nix_shell = {
-              format = "[$name]($style)";
-              style = "bright-black";
+            pure = pkgs.fishPlugins.pure.overrideAttrs {
+              nativeCheckInputs = [ ];
+              checkPlugins = [ ];
+              checkPhase = "";
             };
           };
         };
