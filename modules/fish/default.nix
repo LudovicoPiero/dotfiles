3478839fac52e18c74ede0f4a2bb4543baf1c1b4
Ludovico Piero <lewdovico@gnuweeb.org>
2025-05-13 02:08:24 +0900

    fish: pure -> tide
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/fish/default.nix
+++ modules/fish/default.nix
@@ -62,14 +62,54 @@ in
         fish = {
           enable = true;
 
+          #NOTE: get default tide config using `set -U | grep tide_`
           config = with pkgs; ''
             function fish_greeting
             end
 
+            set -x tide_character_color brgreen
+            set -x tide_character_color_failure brred
+            set -x tide_character_icon ❯
+            set -x tide_character_vi_icon_default ❮
+            set -x tide_character_vi_icon_replace ▶
+            set -x tide_character_vi_icon_visual V
 
-            set --universal pure_check_for_new_release false
-            set --universal pure_enable_nixdevshell true
-            set --universal pure_show_prefix_root_prompt true
+            set -x tide_status_bg_color normal
+            set -x tide_status_bg_color_failure normal
+            set -x tide_status_color green
+            set -x tide_status_color_failure red
+            set -x tide_status_icon ✔
+            set -x tide_status_icon_failure ✘
+
+            set -x tide_prompt_add_newline_before true
+            set -x tide_prompt_color_frame_and_connection 6C6C6C
+            set -x tide_prompt_color_separator_same_color 949494
+            set -x tide_prompt_min_cols 34
+            set -x tide_prompt_pad_items false
+            set -x tide_prompt_transient_enabled true
+
+            set -x tide_left_prompt_items pwd git newline character
+            set -x tide_left_prompt_frame_enabled false
+            set -x tide_left_prompt_prefix
+            set -x tide_left_prompt_separator_diff_color " "
+            set -x tide_left_prompt_separator_same_color " "
+            set -x tide_left_prompt_suffix " "
+
+            set -x tide_right_prompt_frame_enabled false
+            set -x tide_right_prompt_items status cmd_duration context jobs direnv node python rustc java php pulumi ruby go gcloud kubectl distrobox toolbox terraform aws nix_shell crystal elixir zig
+            set -x tide_right_prompt_prefix " "
+            set -x tide_right_prompt_separator_diff_color " "
+            set -x tide_right_prompt_separator_same_color " "
+            set -x tide_right_prompt_suffix ""
+
+            set -x tide_pwd_bg_color normal
+            set -x tide_pwd_color_anchors brcyan
+            set -x tide_pwd_color_dirs cyan
+            set -x tide_pwd_color_truncated_dirs magenta
+            set -x tide_pwd_icon
+            set -x tide_pwd_icon_home
+            set -x tide_pwd_icon_unwritable 
+            set -x tide_pwd_markers .bzr .citc .git .hg .node-version .python-version .ruby-version .shorten_folder_marker .svn .terraform Cargo.toml composer.json CVS go.mod package.json build.zig
 
             ${_ zoxide} init fish | source
             ${_ fzf} --fish | source
@@ -78,11 +118,13 @@ in
           '';
 
           plugins = {
-            inherit (pkgs.fishPlugins) async-prompt;
-            pure = pkgs.fishPlugins.pure.overrideAttrs {
-              nativeCheckInputs = [ ];
-              checkPlugins = [ ];
-              checkPhase = "";
+            tide = pkgs.fishPlugins.tide.overrideAttrs {
+              src = pkgs.fetchFromGitHub {
+                owner = "IlanCosman";
+                repo = "tide";
+                rev = "44c521ab292f0eb659a9e2e1b6f83f5f0595fcbd";
+                hash = "sha256-85iU1QzcZmZYGhK30/ZaKwJNLTsx+j3w6St8bFiQWxc=";
+              };
             };
           };
         };
