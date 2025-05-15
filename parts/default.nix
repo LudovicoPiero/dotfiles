bb0d6153676b33b03f5955e2d7eab30216ddf38f
Ludovico Piero <lewdovico@gnuweeb.org>
2025-05-15 09:43:17 +0900

    parts: add formatter script
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- parts/default.nix
+++ parts/default.nix
@@ -7,9 +7,18 @@
   ];
 
   perSystem =
-    { pkgs, ... }:
+    { pkgs, lib, ... }:
     {
-      formatter = pkgs.nixfmt-rfc-style;
+      formatter = pkgs.writeShellScriptBin "formatter" ''
+        echo "Formatting *.nix files (excluding packages/)..."
+        fd . -t f -e nix --exclude packages/ -x ${lib.getExe' pkgs.nixfmt-rfc-style "nixfmt"} -s '{}'
+
+        echo "Formatting *.html files..."
+        fd . -t f -e html -x ${lib.getExe pkgs.nodePackages.prettier} --write '{}'
+
+        echo "Formatting *.sh files..."
+        fd . -t f -e sh -x ${lib.getExe pkgs.shfmt} -w -i 2 '{}'
+      '';
 
       devShells.default = pkgs.mkShell {
         name = "UwU Shell";
