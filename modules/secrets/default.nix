2a5cf09c16638ebcd1fd09dc1c18f708ff594723
Ludovico Piero <lewdovico@gnuweeb.org>
2025-05-23 15:10:14 +0900

    secrets: move sshkey and keys.txt to /Persist
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/secrets/default.nix
+++ modules/secrets/default.nix
@@ -26,8 +26,8 @@ in
     sops = {
       defaultSopsFile = ../../secrets/secrets.yaml;
       defaultSopsFormat = "yaml";
-      age.sshKeyPaths = [ "${config.vars.homeDirectory}/.ssh/id_ed25519_sops" ];
-      age.keyFile = "${config.vars.homeDirectory}/.config/sops/age/keys.txt";
+      age.sshKeyPaths = [ "/Persist/ssh/id_ed25519_sops" ];
+      age.keyFile = "/Persist/sops/age/keys.txt";
     };
   };
 }
