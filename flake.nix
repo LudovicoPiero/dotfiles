13c480aa9a1510de99fc00866a5bc74db598560d
Ludovico Piero <lewdovico@gnuweeb.org>
2025-06-02 02:57:56 +0900

    Revert "flake: remove hyprland's nixpkgs follows"
    This reverts commit 52969119d616c393d7fa91f2f075f1a4bfa0d351.

--- flake.nix
+++ flake.nix
@@ -28,8 +28,7 @@
 
     # Hyprland
     hyprland.url = "github:hyprwm/Hyprland";
-    #TODO
-    # hyprland.inputs.nixpkgs.follows = "nixpkgs";
+    hyprland.inputs.nixpkgs.follows = "nixpkgs";
 
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
