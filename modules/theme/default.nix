42a43b9a68a1544966c60ebfc61aed6f42331e57
Ludovico Piero <lewdovico@gnuweeb.org>
2025-06-03 01:48:28 +0900

    theme: cleanup settings
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/theme/default.nix
+++ modules/theme/default.nix
@@ -115,59 +115,17 @@ in
 
           inherit (cfg.gtk) cursorTheme theme iconTheme;
 
-          gtk2 = {
-            configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
-            extraConfig = ''
-              gtk-cursor-theme-name="${config.gtk.cursorTheme.name}"
-              gtk-cursor-theme-size=${toString config.gtk.cursorTheme.size}
-              gtk-toolbar-style=GTK_TOOLBAR_BOTH
-              gtk-toolbar-icon-size=GTK_ICON_SIZE_LARGE_TOOLBAR
-              gtk-button-images=1
-              gtk-menu-images=1
-              gtk-enable-event-sounds=1
-              gtk-enable-input-feedback-sounds=1
-              gtk-xft-antialias=1
-              gtk-xft-hinting=1
-              gtk-xft-hintstyle="hintfull"
-              gtk-xft-rgba="rgb"
-            '';
-          };
-
-          gtk3 = {
-            bookmarks = [
-              "file://${config.home.homeDirectory}/Code"
-              "file://${config.home.homeDirectory}/Media"
-              "file://${config.home.homeDirectory}/Documents"
-              "file://${config.home.homeDirectory}/Downloads"
-              # "file://${config.home.homeDirectory}/Games"
-              "file://${config.home.homeDirectory}/Music"
-              "file://${config.home.homeDirectory}/Pictures"
-              "file://${config.home.homeDirectory}/Videos"
-              "file://${config.home.homeDirectory}/WinE"
-            ];
-
-            extraConfig = {
-              gtk-application-prefer-dark-theme = 1;
-              gtk-cursor-theme-name = config.gtk.cursorTheme.name;
-              gtk-cursor-theme-size = config.gtk.cursorTheme.size;
-              gtk-font-name = osConfig.mine.fonts.main.name;
-              gtk-toolbar-style = "GTK_TOOLBAR_BOTH";
-              gtk-toolbar-icon-size = "GTK_ICON_SIZE_LARGE_TOOLBAR";
-              gtk-button-images = 1;
-              gtk-menu-images = 1;
-              gtk-enable-event-sounds = 1;
-              gtk-enable-input-feedback-sounds = 1;
-              gtk-xft-antialias = 1;
-              gtk-xft-hinting = 1;
-              gtk-xft-hintstyle = "hintfull";
-              gtk-xft-rgba = "rgb";
-            };
-          };
-
-          gtk4.extraConfig = {
-            gtk-application-prefer-dark-theme = 1;
-            gtk-font-name = osConfig.mine.fonts.main.name;
-          };
+          gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+          gtk3.bookmarks = [
+            "file://${config.home.homeDirectory}/Code"
+            "file://${config.home.homeDirectory}/Media"
+            "file://${config.home.homeDirectory}/Documents"
+            "file://${config.home.homeDirectory}/Downloads"
+            "file://${config.home.homeDirectory}/Music"
+            "file://${config.home.homeDirectory}/Pictures"
+            "file://${config.home.homeDirectory}/Videos"
+            "file://${config.home.homeDirectory}/WinE"
+          ];
         };
 
         qt = {
