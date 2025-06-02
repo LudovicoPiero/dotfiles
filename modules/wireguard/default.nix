2fb88d35648b030d41d815c2891c995acec314d5
Ludovico Piero <lewdovico@gnuweeb.org>
2025-06-03 04:33:50 +0900

    wireguard: change server
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/wireguard/default.nix
+++ modules/wireguard/default.nix
@@ -17,20 +17,20 @@ in
 
     networking.wg-quick.interfaces = {
       wg0 = {
-        autostart = false;
+        autostart = true;
         address = [ "10.66.66.3/32" ];
         dns = [
           "45.90.28.49"
-          "45.90.30.49"
+          "1.0.0.1"
         ];
         privateKeyFile = config.sops.secrets.privateKey.path;
 
         peers = [
           {
-            publicKey = "Bnw+4BTkjhJDpSZ9uFylkpDoqOFY5GWtcaAqGvakNjk=";
+            publicKey = "jZ4mHEdMx0XHlEGRntWXtt0tpYz3feoVUbbJQejYQwA=";
             presharedKeyFile = config.sops.secrets.presharedKey.path;
             allowedIPs = [ "0.0.0.0/0" ];
-            endpoint = "139.180.209.122:51820";
+            endpoint = "167.179.64.176:51820";
             persistentKeepalive = 25;
           }
         ];
