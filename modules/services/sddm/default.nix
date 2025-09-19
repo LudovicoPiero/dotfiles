d67d67102df26cec15c4329922a87132c4be632e
Ludovico Piero <lewdovico@gnuweeb.org>
2025-09-19 14:01:22 +0900

    sddm: remove patch
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/services/sddm/default.nix
+++ modules/services/sddm/default.nix
@@ -26,16 +26,6 @@ in
       services.displayManager.gdm.enable = mkForce false;
       services.displayManager.sddm = {
         enable = true;
-        package = lib.mkForce (
-          pkgs.kdePackages.sddm.overrideAttrs (old: {
-            patches = (old.patches or [ ]) ++ [
-              (pkgs.fetchpatch {
-                url = "https://patch-diff.githubusercontent.com/raw/sddm/sddm/pull/1779.patch";
-                hash = "sha256-8QP9Y8V9s8xrc+MIUlB7iHVNHbntGkw0O/N510gQ+bE=";
-              })
-            ];
-          })
-        );
 
         wayland.enable = true;
         theme = lib.mkForce "";
