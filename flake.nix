945da0154af2955276b81a09d2e8183500eaf430
Ludovico Piero <lewdovico@gnuweeb.org>
2025-10-11 23:47:27 +0900

    shared: lix -> nixVersions.git
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- flake.nix
+++ flake.nix
@@ -93,19 +93,6 @@
     };
     programsdb.inputs.nixpkgs.follows = "nixpkgs";
 
-    lix = {
-      type = "tarball";
-      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
-      flake = false;
-    };
-
-    lix-module = {
-      type = "tarball";
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
-    };
-    lix-module.inputs.nixpkgs.follows = "nixpkgs";
-    lix-module.inputs.lix.follows = "lix";
-
     ludovico-nixvim = {
       type = "github";
       owner = "LudovicoPiero";
