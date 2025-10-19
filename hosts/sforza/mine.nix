a69b81008bcc899148b11951d7a79de4006f6d67
Ludovico Piero <lewdovico@gnuweeb.org>
2025-10-19 22:27:19 +0900

    sforza/mine: Change color scheme to gruvbox-material-dark-hard
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- hosts/sforza/mine.nix
+++ hosts/sforza/mine.nix
@@ -8,7 +8,7 @@
   vars = {
     # List of available color schemes:
     # https://github.com/tinted-theming/schemes/blob/spec-0.11/base16/
-    colorScheme = "catppuccin-macchiato";
+    colorScheme = "gruvbox-material-dark-hard";
     email = "lewdovico@gnuweeb.org";
     isALaptop = true;
     opacity = 0.88;
