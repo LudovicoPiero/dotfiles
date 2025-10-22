4836ca92bccab706ef890dae884a34cffb3be437
Ludovico Piero <lewdovico@gnuweeb.org>
2025-10-23 02:33:47 +0900

    Fix fcitx5-qt build by applying upstream patches
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/services/fcitx5/default.nix
+++ modules/services/fcitx5/default.nix
@@ -72,8 +72,22 @@ in
             fcitx5-hangul # Korean
 
             # Input methods Module
-            fcitx5-gtk
-            libsForQt5.fcitx5-qt
+            (libsForQt5.fcitx5-qt.overrideAttrs (old: {
+              patches = old.patches or [ ] ++ [
+                (pkgs.fetchpatch2 {
+                  url = "https://github.com/fcitx/fcitx5-qt/commit/46a07a85d191fd77a1efc39c8ed43d0cd87788d2.patch?full_index=1";
+                  hash = "sha256-qv8Rj6YoFdMQLOB2R9LGgwCHKdhEji0Sg67W37jSIac=";
+                })
+                (pkgs.fetchpatch2 {
+                  url = "https://github.com/fcitx/fcitx5-qt/commit/6ac4fdd8e90ff9c25a5219e15e83740fa38c9c71.patch?full_index=1";
+                  hash = "sha256-x0OdlIVmwVuq2TfBlgmfwaQszXLxwRFVf+gEU224uVA=";
+                })
+                (pkgs.fetchpatch2 {
+                  url = "https://github.com/fcitx/fcitx5-qt/commit/1d07f7e8d6a7ae8651eda658f87ab0c9df08bef4.patch?full_index=1";
+                  hash = "sha256-22tKD7sbsTJcNqur9/Uf+XAvMvA7tzNQ9hUCMm+E+E0=";
+                })
+              ];
+            }))
 
             # Theme
             inputs.ludovico-pkgs.packages.${pkgs.stdenv.hostPlatform.system}.catppuccin-fcitx5
