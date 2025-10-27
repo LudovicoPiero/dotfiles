8131236fba5e803106f1fef9758515193997a31e
Ludovico Piero <lewdovico@gnuweeb.org>
2025-10-27 20:49:02 +0900

    {fish,zsh}: tweak config
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/programs/fish/default.nix
+++ modules/programs/fish/default.nix
@@ -29,23 +29,19 @@ in
   };
 
   config = mkIf cfg.enable {
-    users.users.root.shell = pkgs.fish;
-    users.users.${config.vars.username}.shell = pkgs.fish;
+    users.users.${config.vars.username} = {
+      shell = pkgs.fish;
+      ignoreShellProgramCheck = true;
+    };
     sops.secrets."shells/githubToken" = {
       mode = "0444";
     };
     environment.pathsToLink = [ "/share/fish" ];
     programs = {
       fish = {
-        enable = true;
+        enable = false;
         useBabelfish = true;
         shellAliases = mkForce { };
-        interactiveShellInit = ''
-          . ${config.sops.secrets."shells/githubToken".path}
-        ''
-        + optionalString (!config.vars.withGui && config.vars.isALaptop) ''
-          ${pkgs.util-linux}/bin/setterm -blank 1 --powersave on
-        '';
       };
 
       bat = {
@@ -75,57 +71,64 @@ in
           enable = true;
 
           #NOTE: get default tide config using `set -U | grep tide_`
-          interactiveShellInit = with pkgs; ''
-            function fish_greeting
-            end
+          interactiveShellInit =
+            with pkgs;
+            ''
+              function fish_greeting
+              end
 
-            set -x tide_character_color brgreen
-            set -x tide_character_color_failure brred
-            set -x tide_character_icon ❯
-            set -x tide_character_vi_icon_default ❮
-            set -x tide_character_vi_icon_replace ▶
-            set -x tide_character_vi_icon_visual V
+              . ${osConfig.sops.secrets."shells/githubToken".path}
 
-            set -x tide_status_bg_color normal
-            set -x tide_status_bg_color_failure normal
-            set -x tide_status_color green
-            set -x tide_status_color_failure red
-            set -x tide_status_icon ✔
-            set -x tide_status_icon_failure ✘
+              set -x tide_character_color brgreen
+              set -x tide_character_color_failure brred
+              set -x tide_character_icon ❯
+              set -x tide_character_vi_icon_default ❮
+              set -x tide_character_vi_icon_replace ▶
+              set -x tide_character_vi_icon_visual V
 
-            set -x tide_prompt_add_newline_before true
-            set -x tide_prompt_color_frame_and_connection 6C6C6C
-            set -x tide_prompt_color_separator_same_color 949494
-            set -x tide_prompt_min_cols 34
-            set -x tide_prompt_pad_items false
-            set -x tide_prompt_transient_enabled true
+              set -x tide_status_bg_color normal
+              set -x tide_status_bg_color_failure normal
+              set -x tide_status_color green
+              set -x tide_status_color_failure red
+              set -x tide_status_icon ✔
+              set -x tide_status_icon_failure ✘
 
-            set -x tide_left_prompt_items pwd git newline character
-            set -x tide_left_prompt_frame_enabled false
-            set -x tide_left_prompt_prefix
-            set -x tide_left_prompt_separator_diff_color " "
-            set -x tide_left_prompt_separator_same_color " "
-            set -x tide_left_prompt_suffix " "
+              set -x tide_prompt_add_newline_before true
+              set -x tide_prompt_color_frame_and_connection 6C6C6C
+              set -x tide_prompt_color_separator_same_color 949494
+              set -x tide_prompt_min_cols 34
+              set -x tide_prompt_pad_items false
+              set -x tide_prompt_transient_enabled true
 
-            set -x tide_right_prompt_frame_enabled false
-            set -x tide_right_prompt_items status context jobs direnv node python rustc java php pulumi ruby go gcloud kubectl distrobox toolbox terraform aws nix_shell crystal elixir zig
-            set -x tide_right_prompt_prefix " "
-            set -x tide_right_prompt_separator_diff_color " "
-            set -x tide_right_prompt_separator_same_color " "
-            set -x tide_right_prompt_suffix ""
+              set -x tide_left_prompt_items pwd git newline character
+              set -x tide_left_prompt_frame_enabled false
+              set -x tide_left_prompt_prefix
+              set -x tide_left_prompt_separator_diff_color " "
+              set -x tide_left_prompt_separator_same_color " "
+              set -x tide_left_prompt_suffix " "
 
-            set -x tide_pwd_bg_color normal
-            set -x tide_pwd_color_anchors brcyan
-            set -x tide_pwd_color_dirs cyan
-            set -x tide_pwd_color_truncated_dirs magenta
-            set -x tide_pwd_icon
-            set -x tide_pwd_icon_home
-            set -x tide_pwd_icon_unwritable 
-            set -x tide_pwd_markers .bzr .citc .git .hg .node-version .python-version .ruby-version .shorten_folder_marker .svn .terraform Cargo.toml composer.json CVS go.mod package.json build.zig
+              set -x tide_right_prompt_frame_enabled false
+              set -x tide_right_prompt_items status context jobs direnv node python rustc java php pulumi ruby go gcloud kubectl distrobox toolbox terraform aws nix_shell crystal elixir zig
+              set -x tide_right_prompt_prefix " "
+              set -x tide_right_prompt_separator_diff_color " "
+              set -x tide_right_prompt_separator_same_color " "
+              set -x tide_right_prompt_suffix ""
 
-            ${_ zoxide} init fish | source
-            ${_ nix-your-shell} fish | source
-          '';
+              set -x tide_pwd_bg_color normal
+              set -x tide_pwd_color_anchors brcyan
+              set -x tide_pwd_color_dirs cyan
+              set -x tide_pwd_color_truncated_dirs magenta
+              set -x tide_pwd_icon
+              set -x tide_pwd_icon_home
+              set -x tide_pwd_icon_unwritable 
+              set -x tide_pwd_markers .bzr .citc .git .hg .node-version .python-version .ruby-version .shorten_folder_marker .svn .terraform Cargo.toml composer.json CVS go.mod package.json build.zig
+
+              ${_ zoxide} init fish | source
+              ${_ nix-your-shell} fish | source
+            ''
+            + optionalString (!osConfig.vars.withGui && osConfig.vars.isALaptop) ''
+              ${pkgs.util-linux}/bin/setterm -blank 1 --powersave on
+            '';
 
           plugins = [
             {
