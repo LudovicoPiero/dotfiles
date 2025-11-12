c1275997c64070b792963519fde596b116da03cd
Ludovico Piero <lewdovico@gnuweeb.org>
2025-11-12 15:35:47 +0900

    modules/theme: change theme to tokyonight-moon
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/shared/theme/default.nix
+++ modules/shared/theme/default.nix
@@ -6,13 +6,13 @@ in
   options.mine.theme.colorScheme = {
     name = mkOption {
       type = types.str;
-      default = "catppuccin-mocha";
+      default = "tokyonight-moon";
       description = "Name of the active color scheme.";
     };
 
     author = mkOption {
       type = types.str;
-      default = "https://github.com/catppuccin/catppuccin";
+      default = "https://github.com/folke/tokyonight.nvim";
       description = "Color scheme author or source.";
     };
 
@@ -20,22 +20,22 @@ in
       type = types.attrsOf types.str;
       description = "Color palette used by themable applications.";
       default = {
-        base00 = "1e1e2e"; # base
-        base01 = "181825"; # mantle
-        base02 = "313244"; # surface0
-        base03 = "45475a"; # surface1
-        base04 = "585b70"; # surface2
-        base05 = "cdd6f4"; # text
-        base06 = "f5e0dc"; # rosewater
-        base07 = "b4befe"; # lavender
-        base08 = "f38ba8"; # red
-        base09 = "fab387"; # peach
-        base0A = "f9e2af"; # yellow
-        base0B = "a6e3a1"; # green
-        base0C = "94e2d5"; # teal
-        base0D = "89b4fa"; # blue
-        base0E = "cba6f7"; # mauve
-        base0F = "f2cdcd"; # flamingo
+        base00 = "222436"; # background
+        base01 = "1e2030"; # darker background
+        base02 = "191B29"; # darkest background
+        base03 = "2f334d"; # highlight background
+        base04 = "3b4261"; # gutter / weak foreground
+        base05 = "c8d3f5"; # foreground text
+        base06 = "828bb8"; # dimmed foreground
+        base07 = "fca7ea"; # purple / accent
+        base08 = "ff757f"; # red
+        base09 = "ff966c"; # orange
+        base0A = "ffc777"; # yellow
+        base0B = "c3e88d"; # green
+        base0C = "86e1fc"; # cyan
+        base0D = "82aaff"; # blue
+        base0E = "c099ff"; # magenta
+        base0F = "4fd6be"; # teal
       };
     };
   };
