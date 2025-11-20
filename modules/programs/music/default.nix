446ebeba4ce96be2bd825706b3ef9e4c92947e9d
Ludovico Piero <lewdovico@gnuweeb.org>
2025-11-20 23:27:22 +0900

    modules/music: change rmpc config
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/programs/music/default.nix
+++ modules/programs/music/default.nix
@@ -26,73 +26,356 @@ in
     services.playerctld.enable = true;
     hj = {
       packages = with pkgs; [ rmpc ];
-      xdg.config.files."rmpc/config.ron".text = ''
-        #![enable(implicit_some)]
-        #![enable(unwrap_newtypes)]
-        #![enable(unwrap_variant_newtypes)]
-        (
-            address: "127.0.0.1:6600",
-            password: None,
-            theme: None,
-            cache_dir: "${config.hj.xdg.cache.directory}/rmpc",
-            lyrics_dir: "${config.hj.xdg.config.directory}/rmpc/lyrics",
-            on_song_change: None,
-            volume_step: 5,
-            max_fps: 30,
-            scrolloff: 0,
-            wrap_navigation: false,
-            enable_mouse: true,
-            enable_config_hot_reload: true,
-            status_update_interval_ms: 1000,
-            select_current_song_on_change: false,
-            browser_song_sort: [Disc, Track, Artist, Title],
-            album_art: (
-                method: Auto,
-                max_size_px: (width: 600, height: 600),
-                disabled_protocols: ["http://", "https://"],
-            ),
-        )
-      '';
 
-      xdg.config.files."mpd/mpd.conf".text = ''
-        music_directory "${config.vars.homeDirectory}/Media/Music"
-        playlist_directory "${config.hj.xdg.config.directory}/mpd/playlists"
-        state_file "${config.hj.xdg.state.directory}/mpd/state"
-        sticker_file "${config.hj.xdg.state.directory}/mpd/sticker.sql"
-        db_file "${config.hj.xdg.state.directory}/mpd/database"
+      # Source: https://github.com/ericmckevitt/rmpc-config
+      xdg.config.files = {
+        # Config.ron
+        "rmpc/config.ron".text = ''
+          #![enable(implicit_some)]
+          #![enable(unwrap_newtypes)]
+          #![enable(unwrap_variant_newtypes)]
+          (
+              address: "127.0.0.1:6600",
+              cache_dir: Some("${config.hj.xdg.cache.directory}/rmpc"),
+              lyrics_dir: Some("${config.hj.xdg.config.directory}/rmpc/lyrics"),
+              password: None,
+              theme: "catppuccin_mocha",
+              volume_step: 5,
+              max_fps: 30,
+              scrolloff: 0,
+              wrap_navigation: false,
+              enable_mouse: true,
+              status_update_interval_ms: 1000,
+              select_current_song_on_change: false,
+              browser_column_widths: [20, 38, 42],
+              album_art: (
+                  method: Auto,
+                  max_size_px: (width: 900, height: 900),
+                  disabled_protocols: ["http://", "https://"],
+                  vertical_align: Top,
+                  horizontal_align: Center,
+              ),
+              keybinds: (
+                  global: {
+                      ":":       CommandMode,
+                      ",":       VolumeDown,
+                      "s":       Stop,
+                      ".":       VolumeUp,
+                      "<Tab>":   NextTab,
+                      "<S-Tab>": PreviousTab,
+                      "1":       SwitchToTab("Queue"),
+                      "2":       SwitchToTab("Artists"),
+                      "3":       SwitchToTab("Albums"),
+                      "4":       SwitchToTab("Search"),
+                      "5":       SwitchToTab("Directories"),
+                      "6":       SwitchToTab("Lyrics"),
+                      "q":       Quit,
+                      ">":       NextTrack,
+                      "p":       TogglePause,
+                      "<":       PreviousTrack,
+                      "f":       SeekForward,
+                      "z":       ToggleRepeat,
+                      "x":       ToggleRandom,
+                      "c":       ToggleConsume,
+                      "v":       ToggleSingle,
+                      "b":       SeekBack,
+                      "~":       ShowHelp,
+                      "I":       ShowCurrentSongInfo,
+                      "O":       ShowOutputs,
+                      "P":       ShowDecoders,
+                  },
+                  navigation: {
+                      "k":         Up,
+                      "j":         Down,
+                      "h":         Left,
+                      "l":         Right,
+                      "<Up>":      Up,
+                      "<Down>":    Down,
+                      "<Left>":    Left,
+                      "<Right>":   Right,
+                      "<C-k>":     PaneUp,
+                      "<C-j>":     PaneDown,
+                      "<C-h>":     PaneLeft,
+                      "<C-l>":     PaneRight,
+                      "<C-u>":     UpHalf,
+                      "N":         PreviousResult,
+                      "a":         Add,
+                      "A":         AddAll,
+                      "r":         Rename,
+                      "n":         NextResult,
+                      "g":         Top,
+                      "<Space>":   Select,
+                      "<C-Space>": InvertSelection,
+                      "G":         Bottom,
+                      "<CR>":      Confirm,
+                      "i":         FocusInput,
+                      "J":         MoveDown,
+                      "<C-d>":     DownHalf,
+                      "/":         EnterSearch,
+                      "<C-c>":     Close,
+                      "<Esc>":     Close,
+                      "K":         MoveUp,
+                      "D":         Delete,
+                  },
+                  queue: {
+                      "D":       DeleteAll,
+                      "<CR>":    Play,
+                      "<C-s>":   Save,
+                      "a":       AddToPlaylist,
+                      "d":       Delete,
+                      "i":       ShowInfo,
+                      "C":       JumpToCurrent,
+                  },
+              ),
+              search: (
+                  case_sensitive: false,
+                  mode: Contains,
+                  tags: [
+                      (value: "any",         label: "Any Tag"),
+                      (value: "artist",      label: "Artist"),
+                      (value: "album",       label: "Album"),
+                      (value: "title",       label: "Title"),
+                      (value: "filename",    label: "Filename"),
+                      (value: "genre",       label: "Genre"),
+                      (value: "albumartist", label: "Featured"),
+                  ],
+              ),
+              artists: (
+                  album_display_mode: SplitByDate,
+                  album_sort_by: Date,
+              ),
+              tabs: [
+                  (
+                      name: "Queue",
+                      pane: Split(
+                          direction: Horizontal,
+                          panes: [(size: "20%", pane: Pane(AlbumArt)), (size: "80%", pane: Pane(Queue))],
+                      ),
+                  ),
+                  (
+                      name: "Artists",
+                      pane: Pane(Artists),
+                  ),
+                  (
+                      name: "Albums",
+                      pane: Pane(Albums),
+                  ),
+                  (
+                      name: "Search",
+                      pane: Pane(Search),
+                  ),
+                  (
+                      name: "Directories",
+                      pane: Pane(Directories),
+                  ),
+                  (
+                      name: "Lyrics",
+                      pane: Split(
+                          direction: Vertical,
+                          panes: [(size: "25%", pane: Pane(AlbumArt)), (size: "70%", pane: Pane(Lyrics), vertical_align: Bottom)],
+                      ),
+                  ),
+              ],
+          )
+        '';
 
-        auto_update "yes"
-        volume_normalization "no"
-        restore_paused "yes"
-        filesystem_charset "UTF-8"
-        replaygain "off"
-        audio_buffer_size "8192"
+        # catppuccin_mocha.ron
+        "rmpc/themes/catppuccin_mocha.ron".text = ''
+          #![enable(implicit_some)]
+          #![enable(unwrap_newtypes)]
+          #![enable(unwrap_variant_newtypes)]
+          (
+              default_album_art_path: None,
+              show_song_table_header: true,
+              draw_borders: true,
+              browser_column_widths: [20, 38, 42],
+              background_color: "#1e1e2e",
+              text_color: "#cdd6f4",
+              header_background_color: None,
+              modal_background_color: "#11111b",
+              tab_bar: (
+                  enabled: true,
+                  active_style: (fg: "black", bg: "blue", modifiers: "Bold"),
+                  inactive_style: (),
+              ),
+              highlighted_item_style: (fg: "#eba0ac", modifiers: "Bold"),
+              current_item_style: (fg: "black", bg: "blue", modifiers: "Bold"),
+              borders_style: (fg: "blue"),
+              highlight_border_style: (fg: "blue"),
+              symbols: (song: "󰎇", dir: "󰉋", marker: " ", ellipsis: "..."),
+              progress_bar: (
+                  symbols: ["", "", " "],
+                  track_style: (fg: "#1e2030"),
+                  elapsed_style: (fg: "#eba0ac"),
+                  thumb_style: (fg: "#eba0ac", bg: "#1e2030"),
+              ),
+              scrollbar: (
+                  symbols: ["│", "█", "▲", "▼"],
+                  track_style: (),
+                  ends_style: (),
+                  thumb_style: (fg: "blue"),
+              ),
+              song_table_format: [ // view in the queue
+                  (
+                      prop: (kind: Property(Artist),
+                          default: (kind: Text("Unknown"))
+                      ),
+                      width: "15%",
+                  ),
+                  (
+                      prop: (kind: Property(Title),
+                          default: (kind: Text("Unknown"))
+                      ),
+                      width: "55%",
+                  ),
+                  (
+                      prop: (kind: Property(Album), style: (fg: "white"),
+                          default: (kind: Text("Unknown Album"), style: (fg: "white"))
+                      ),
+                      width: "20%",
+                  ),
+                  (
+                      prop: (kind: Sticker("playCount"), default: (kind: Text("0"))),
+                      width: "9",
+                      alignment: Right,
+                      label: "Playcount"
+                  ),
+                  (
+                      prop: (kind: Property(Duration),
+                          default: (kind: Text("-"))
+                      ),
+                      width: "10%",
+                      alignment: Right,
+                  ),
+              ],
+              layout: Split(
+                  direction: Vertical,
+                  panes: [
+                      (
+                          pane: Pane(Header),
+                          size: "2",
+                      ),
+                      (
+                          pane: Pane(Tabs),
+                          size: "3",
+                      ),
+                      (
+                          pane: Pane(TabContent),
+                          size: "100%",
+                      ),
+                      (
+                          pane: Pane(ProgressBar),
+                          size: "1",
+                      ),
+                  ],
+              ),
+              header: (
+                  rows: [
+                      (
+                          left: [
+                              (kind: Text("["), style: (fg: "yellow", modifiers: "Bold")),
+                              (kind: Property(Status(StateV2(playing_label: "Playing", paused_label: "Paused", stopped_label: "Stopped"))), style: (fg: "yellow", modifiers: "Bold")),
+                              (kind: Text("]"), style: (fg: "yellow", modifiers: "Bold"))
+                          ],
+                          center: [
+                              (kind: Property(Song(Title)), style: (modifiers: "Bold"),
+                                  default: (kind: Text("No Song"), style: (modifiers: "Bold"))
+                              )
+                          ],
+                          right: [
+                              (kind: Property(Widget(Volume)), style: (fg: "blue"))
+                          ]
+                      ),
+                      (
+                          left: [
+                              (kind: Property(Status(Elapsed))),
+                              (kind: Text(" / ")),
+                              (kind: Property(Status(Duration))),
+                              (kind: Text(" (")),
+                              (kind: Property(Status(Bitrate))),
+                              (kind: Text(" kbps)"))
+                          ],
+                          center: [
+                              (kind: Property(Song(Artist)), style: (fg: "yellow", modifiers: "Bold"),
+                                  default: (kind: Text("Unknown"), style: (fg: "yellow", modifiers: "Bold"))
+                              ),
+                              (kind: Text(" - ")),
+                              (kind: Property(Song(Album)),
+                                  default: (kind: Text("Unknown Album"))
+                              )
+                          ],
+                          right: [
+                              (
+                                  kind: Property(Widget(States(
+                                      active_style: (fg: "white", modifiers: "Bold"),
+                                      separator_style: (fg: "white")))
+                                  ),
+                                  style: (fg: "dark_gray")
+                              ),
+                          ]
+                      ),
+                  ],
+              ),
+              browser_song_format: [
+                  (
+                      kind: Group([
+                          (kind: Property(Track)), // track #
+                          (kind: Text(" ")),
+                      ])
+                  ),
+                  (
+                      kind: Group([
+                          (kind: Property(Artist)),
+                          (kind: Text(" - ")),
+                          (kind: Property(Title)),
+                      ]),
+                      default: (kind: Property(Filename))
+                  ),
+              ],
+          )
+        '';
 
-        audio_output {
-          type                "pipewire"
-          name                "PipeWire"
-          format "44100:24:2"
-        }
+        # mpd.conf
+        "mpd/mpd.conf".text = ''
+          music_directory "${config.vars.homeDirectory}/Media/Music"
+          playlist_directory "${config.hj.xdg.config.directory}/mpd/playlists"
+          state_file "${config.hj.xdg.state.directory}/mpd/state"
+          sticker_file "${config.hj.xdg.state.directory}/mpd/sticker.sql"
+          db_file "${config.hj.xdg.state.directory}/mpd/database"
 
-        audio_output {
-          type                "fifo"
-          name                "Visualiser"
-          path                "/tmp/mpd.fifo"
-          format              "44100:16:2"
-        }
+          auto_update "yes"
+          volume_normalization "no"
+          restore_paused "yes"
+          filesystem_charset "UTF-8"
+          replaygain "off"
+          audio_buffer_size "8192"
 
-        audio_output {
-          type                "httpd"
-          name                "lossless"
-          encoder             "flac"
-          port                "8000"
-          max_clients         "8"
-          format              "44100:16:2"
-        }
+          audio_output {
+            type                "pipewire"
+            name                "PipeWire"
+            format "44100:24:2"
+          }
 
-        bind_to_address "127.0.0.1"
-        port "6600"
-      '';
+          audio_output {
+            type                "fifo"
+            name                "Visualiser"
+            path                "/tmp/mpd.fifo"
+            format              "44100:16:2"
+          }
+
+          audio_output {
+            type                "httpd"
+            name                "lossless"
+            encoder             "flac"
+            port                "8000"
+            max_clients         "8"
+            format              "44100:16:2"
+          }
+
+          bind_to_address "127.0.0.1"
+          port "6600"
+        '';
+      };
     };
 
     services.mpd.enable = lib.mkForce false;
