1c7d8b5a5f99fd883017ad0477b7ff5300116fae
Ludovico Piero <lewdovico@gnuweeb.org>
2025-11-24 18:50:02 +0900

    packages/fcitx5: support wounded corners in catppuccin theme
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- packages/catppuccin-fcitx5/default.nix
+++ packages/catppuccin-fcitx5/default.nix
@@ -2,22 +2,35 @@
   lib,
   stdenvNoCC,
   sources,
+
+  withRoundedCorners ? true,
 }:
 
 stdenvNoCC.mkDerivation {
   inherit (sources.catppuccin-fcitx5) pname version src;
 
+  dontConfigure = true;
   dontBuild = true;
 
+  # Modify files here, before installation
+  postPatch = lib.optionalString withRoundedCorners ''
+    # Uncomments the rounded corner settings in theme.conf
+    find src -name theme.conf -exec sed -i -E 's/^# (Image=(panel|highlight).svg)/\1/' {} +
+  '';
+
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/share/fcitx5/themes
-    cp -r ./src/* $out/share/fcitx5/themes
+    cp -r src/* $out/share/fcitx5/themes/
+
+    runHook postInstall
   '';
 
   meta = {
     description = "Soothing pastel theme for Fcitx5";
     homepage = "https://github.com/catppuccin/fcitx5";
-    license = with lib.licenses; [ mit ];
-    maintainers = with lib.maintainers; [ ludovicopiero ];
+    license = lib.licenses.mit;
+    maintainers = lib.maintainers.ludovicopiero;
   };
 }
