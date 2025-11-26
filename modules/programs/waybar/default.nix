0a75e31b1b8c5181f7fc17128a32d2abd0db5426
Ludovico Piero <lewdovico@gnuweeb.org>
2025-11-26 14:24:24 +0900

    modules/waybar: move style file and inline config JSON
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/programs/waybar/default.nix
+++ modules/programs/waybar/default.nix
@@ -5,8 +5,12 @@
   ...
 }:
 let
-  inherit (lib) mkEnableOption mkIf getExe;
-  _ = getExe;
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    getExe
+    getExe'
+    ;
 
   cfg = config.mine.waybar;
 
@@ -16,233 +20,8 @@ let
     en=$(LC_TIME=en_US.UTF-8 date "+%A, %B %-d, %Y")
     printf '{"text":"| DATE: %s ","tooltip":"%s"}\n' "$ja" "$en"
   '';
-
-  # The Waybar configuration set
-  waybarSettings = {
-    layer = "top";
-    position = "bottom";
-    height = 32;
-    spacing = 6;
-    fixed-center = true;
-    ipc = true;
-
-    modules-left = [
-      "custom/menu"
-      "hyprland/workspaces"
-      "niri/workspaces"
-      "dwl/tags"
-      # "ext/workspaces"
-      # "dwl/window"
-    ];
-    modules-center = [ "hyprland/submap" ];
-    modules-right = [
-      "tray"
-      "idle_inhibitor"
-      "mpd"
-      "pulseaudio"
-      "bluetooth"
-      "network"
-      "battery"
-      "clock"
-      "custom/date"
-      "custom/power"
-    ];
-
-    "sway/workspaces" = {
-      format = "{name}";
-    };
-
-    "custom/tailscale" = {
-      format = "| TAILSCALE";
-      exec = "ip link show tailscale0 >/dev/null 2>&1 && echo '{\"class\": \"connected\"}' || echo '{}'";
-      return-type = "json";
-      interval = 5;
-    };
-
-    "custom/wireguard" = {
-      format = "| WIREGUARD";
-      exec = "echo '{\"class\": \"connected\"}'";
-      exec-if = "test -d /proc/sys/net/ipv4/conf/wg0";
-      return-type = "json";
-      interval = 5;
-    };
-
-    backlight = {
-      interval = 2;
-      format = "| BACKLIGHT: {percent}%";
-      on-scroll-up = "${_ pkgs.light} -A 5%";
-      on-scroll-down = "${_ pkgs.light} -U 5%";
-    };
-
-    battery = {
-      interval = 60;
-      format = "| BAT: {capacity}%";
-      format-charging = "| BAT+: {capacity}%";
-      format-plugged = "| BAT(AC): {capacity}%";
-      format-full = "| BAT: FULL";
-      format-alt = "| BAT: {time}";
-      tooltip = true;
-    };
-
-    bluetooth = {
-      format = "| BT {status}";
-      format-on = "| BT {status}";
-      format-off = "| BT OFF";
-      format-disabled = "| BT DIS";
-      format-connected = "| BT {device_alias}";
-      format-connected-battery = "| BT {device_battery_percentage}%";
-      tooltip = true;
-    };
-
-    clock = {
-      format = "| TIME: {:%I:%M %p}";
-      tooltip-format = "{calendar}";
-      calendar = {
-        mode = "year";
-        format = {
-          months = "<span color='#ffead3'><b>{}</b></span>";
-          days = "<span color='#ecc6d9'><b>{}</b></span>";
-          weeks = "<span color='#99ffdd'><b>W{}</b></span>";
-          weekdays = "<span color='#ffcc66'><b>{}</b></span>";
-          today = "<span color='#ff6699'><b><u>{}</u></b></span>";
-        };
-      };
-    };
-
-    "custom/date" = {
-      return-type = "json";
-      interval = 3600;
-      exec = "${_ waybar-date}";
-    };
-
-    cpu = {
-      interval = 5;
-      format = "| CPU: {usage}%";
-    };
-
-    "custom/menu" = {
-      format = "MENU";
-      tooltip = false;
-      on-click = "${_ pkgs.fuzzel}";
-    };
-
-    "custom/power" = {
-      format = "POWER";
-      tooltip = false;
-      on-click = "${_ pkgs.wleave}";
-    };
-
-    "custom/disk_home" = {
-      format = "| HOME: {}";
-      tooltip-format = "Size of /home";
-      interval = 30;
-      exec = "df -h --output=avail /dev/disk/by-label/HOME | tail -1 | tr -d ' '";
-    };
-
-    "custom/disk_root" = {
-      format = "| ROOT: {}";
-      tooltip-format = "Size of /";
-      interval = 30;
-      exec = "df -h --output=avail / | tail -1 | tr -d ' '";
-    };
-
-    memory = {
-      interval = 10;
-      format = "| MEM: {used:0.1f}G";
-    };
-
-    mpd = {
-      interval = 2;
-      format = "| MPD: {stateIcon} {artist} - {title}";
-      artist-len = 8;
-      title-len = 12;
-      format-disconnected = "| MPD DISCONNECTED";
-      format-paused = "| MPD PAUSED: {artist} - {title}";
-      format-stopped = "| MPD STOPPED";
-      state-icons = {
-        paused = "PAUSE";
-        playing = "PLAY";
-      };
-      tooltip-format = "MPD (connected)";
-      tooltip-format-disconnected = "MPD (disconnected)";
-      on-click = "${_ pkgs.mpc} toggle";
-      on-click-middle = "${_ pkgs.mpc} prev";
-      on-click-right = "${_ pkgs.mpc} next";
-      on-scroll-up = "${_ pkgs.mpc} seek +00:00:01";
-      on-scroll-down = "${_ pkgs.mpc} seek -00:00:01";
-    };
-
-    network = {
-      interval = 5;
-      format-wifi = "| NET: DOWN {bandwidthDownBits} UP {bandwidthUpBits}";
-      format-ethernet = "| IP LEAK: {ipaddr}/{cidr}";
-      format-linked = "| IP LEAK: (No IP)";
-      format-disconnected = "| NET: DISCONNECTED";
-      format-alt = "| IP LEAK: {ipaddr}/{cidr}";
-    };
-
-    pulseaudio = {
-      format = "| VOL: {volume}% {format_source}";
-      format-muted = "| VOL: MUTE {format_source}";
-      format-bluetooth = "| VOL: BT {volume}% {format_source}";
-      format-bluetooth-muted = "| VOL: BT MUTE {format_source}";
-      format-source = "MIC: {volume}%";
-      format-source-muted = "MIC: MUTE";
-      scroll-step = 5;
-      on-click = "${_ pkgs.ponymix} -N -t sink toggle";
-      on-click-right = "${_ pkgs.ponymix} -N -t source toggle";
-    };
-
-    idle_inhibitor = {
-      format = "| {icon}";
-      format-icons = {
-        activated = "INHIBIT ON";
-        deactivated = "INHIBIT OFF";
-      };
-    };
-
-    "hyprland/submap" = {
-      format = "| {}";
-      max-length = 8;
-      tooltip = false;
-    };
-
-    "niri/workspaces" = {
-      format = "{value}";
-      on-click = "activate";
-      tooltip = false;
-    };
-
-    "dwl/tags" = {
-      num-tags = 9;
-      disable-click = false;
-    };
-
-    "dwl/window" = {
-      format = "[{layout}]";
-    };
-
-    "ext/workspaces" = {
-      format = "{name}";
-      sort-by-id = true;
-      on-click = "activate";
-    };
-
-    "hyprland/workspaces" = {
-      format = "{icon}";
-      on-click = "activate";
-      on-scroll-up = "${config.mine.hyprland.package}/bin/hyprctl dispatch workspace e-1";
-      on-scroll-down = "${config.mine.hyprland.package}/bin/hyprctl dispatch workspace e+1";
-    };
-
-    tray = {
-      icon-size = 16;
-      spacing = 10;
-    };
-  };
 in
 {
-  imports = [ ./_style.nix ];
   options.mine.waybar = {
     enable = mkEnableOption "waybar";
   };
@@ -251,8 +30,205 @@ in
     hj = {
       packages = [ pkgs.waybar ];
       xdg.config.files."waybar/config.jsonc" = {
-        generator = lib.generators.toJSON { };
-        value = waybarSettings;
+        text = ''
+          {
+            "modules-center": [
+                "hyprland/submap"
+            ],
+            "modules-left": [
+                "custom/menu",
+                "hyprland/workspaces",
+                "niri/workspaces",
+                "dwl/tags"
+            ],
+            "modules-right": [
+                "tray",
+                "idle_inhibitor",
+                "mpd",
+                "pulseaudio",
+                "bluetooth",
+                "network",
+                "battery",
+                "clock",
+                "custom/date",
+                "custom/power"
+            ],
+            "position": "bottom",
+            "spacing": 6,
+            "ipc": true,
+            "layer": "top",
+            "fixed-center": true,
+            "height": 32,
+            "backlight": {
+                "format": "| BACKLIGHT: {percent}%",
+                "interval": 2,
+                "on-scroll-down": "${getExe pkgs.light} -U 5%",
+                "on-scroll-up": "${getExe pkgs.light} -A 5%"
+            },
+
+            "battery": {
+                "format": "| BAT: {capacity}%",
+                "format-alt": "| BAT: {time}",
+                "format-charging": "| BAT+: {capacity}%",
+                "format-full": "| BAT: FULL",
+                "format-plugged": "| BAT(AC): {capacity}%",
+                "interval": 60,
+                "tooltip": true
+            },
+            "bluetooth": {
+                "format": "| BT {status}",
+                "format-connected": "| BT {device_alias}",
+                "format-connected-battery": "| BT {device_battery_percentage}%",
+                "format-disabled": "| BT DIS",
+                "format-off": "| BT OFF",
+                "format-on": "| BT {status}",
+                "tooltip": true
+            },
+            "clock": {
+                "calendar": {
+                    "format": {
+                        "days": "<span color='#ecc6d9'><b>{}</b></span>",
+                        "months": "<span color='#ffead3'><b>{}</b></span>",
+                        "today": "<span color='#ff6699'><b><u>{}</u></b></span>",
+                        "weekdays": "<span color='#ffcc66'><b>{}</b></span>",
+                        "weeks": "<span color='#99ffdd'><b>W{}</b></span>"
+                    },
+                    "mode": "year"
+                },
+                "format": "| TIME: {:%I:%M %p}",
+                "tooltip-format": "{calendar}"
+            },
+            "cpu": {
+                "format": "| CPU: {usage}%",
+                "interval": 5
+            },
+            "custom/date": {
+                "exec": "${getExe waybar-date}",
+                "interval": 3600,
+                "return-type": "json"
+            },
+            "custom/disk_home": {
+                "exec": "df -h --output=avail /dev/disk/by-label/HOME | tail -1 | tr -d ' '",
+                "format": "| HOME: {}",
+                "interval": 30,
+                "tooltip-format": "Size of /home"
+            },
+            "custom/disk_root": {
+                "exec": "df -h --output=avail / | tail -1 | tr -d ' '",
+                "format": "| ROOT: {}",
+                "interval": 30,
+                "tooltip-format": "Size of /"
+            },
+            "custom/menu": {
+                "format": "MENU",
+                "on-click": "${getExe pkgs.fuzzel}",
+                "tooltip": false
+            },
+            "custom/power": {
+                "format": "POWER",
+                "on-click": "${getExe pkgs.wleave}",
+                "tooltip": false
+            },
+            "custom/tailscale": {
+                "exec": "ip link show tailscale0 >/dev/null 2>&1 && echo '{\"class\": \"connected\"}' || echo '{}'",
+                "format": "| TAILSCALE",
+                "interval": 5,
+                "return-type": "json"
+            },
+            "custom/wireguard": {
+                "exec": "echo '{\"class\": \"connected\"}'",
+                "exec-if": "test -d /proc/sys/net/ipv4/conf/wg0",
+                "format": "| WIREGUARD",
+                "interval": 5,
+                "return-type": "json"
+            },
+            "dwl/tags": {
+                "disable-click": false,
+                "num-tags": 9
+            },
+            "dwl/window": {
+                "format": "[{layout}]"
+            },
+            "ext/workspaces": {
+                "format": "{name}",
+                "on-click": "activate",
+                "sort-by-id": true
+            },
+            "hyprland/submap": {
+                "format": "| {}",
+                "max-length": 8,
+                "tooltip": false
+            },
+            "hyprland/workspaces": {
+                "format": "{icon}",
+                "on-click": "activate",
+                "on-scroll-down": "${getExe' pkgs.hyprland "hyprctl"} dispatch workspace e+1",
+                "on-scroll-up": "${getExe' pkgs.hyprland "hyprctl"} dispatch workspace e-1"
+            },
+            "idle_inhibitor": {
+                "format": "| {icon}",
+                "format-icons": {
+                    "activated": "INHIBIT ON",
+                    "deactivated": "INHIBIT OFF"
+                }
+            },
+            "memory": {
+                "format": "| MEM: {used:0.1f}G",
+                "interval": 10
+            },
+            "mpd": {
+                "artist-len": 8,
+                "format": "| MPD: {stateIcon} {artist} - {title}",
+                "format-disconnected": "| MPD DISCONNECTED",
+                "format-paused": "| MPD PAUSED: {artist} - {title}",
+                "format-stopped": "| MPD STOPPED",
+                "interval": 2,
+                "on-click": "${getExe pkgs.mpc} toggle",
+                "on-click-middle": "${getExe pkgs.mpc} prev",
+                "on-click-right": "${getExe pkgs.mpc} next",
+                "on-scroll-down": "${getExe pkgs.mpc} seek -00:00:01",
+                "on-scroll-up": "${getExe pkgs.mpc} seek +00:00:01",
+                "state-icons": {
+                    "paused": "PAUSE",
+                    "playing": "PLAY"
+                },
+                "title-len": 12,
+                "tooltip-format": "MPD (connected)",
+                "tooltip-format-disconnected": "MPD (disconnected)"
+            },
+            "network": {
+                "format-alt": "| IP LEAK: {ipaddr}/{cidr}",
+                "format-disconnected": "| NET: DISCONNECTED",
+                "format-ethernet": "| IP LEAK: {ipaddr}/{cidr}",
+                "format-linked": "| IP LEAK: (No IP)",
+                "format-wifi": "| NET: DOWN {bandwidthDownBits} UP {bandwidthUpBits}",
+                "interval": 5
+            },
+            "niri/workspaces": {
+                "format": "{value}",
+                "on-click": "activate",
+                "tooltip": false
+            },
+            "pulseaudio": {
+                "format": "| VOL: {volume}% {format_source}",
+                "format-bluetooth": "| VOL: BT {volume}% {format_source}",
+                "format-bluetooth-muted": "| VOL: BT MUTE {format_source}",
+                "format-muted": "| VOL: MUTE {format_source}",
+                "format-source": "MIC: {volume}%",
+                "format-source-muted": "MIC: MUTE",
+                "on-click": "${getExe pkgs.ponymix} -N -t sink toggle",
+                "on-click-right": "${getExe pkgs.ponymix} -N -t source toggle",
+                "scroll-step": 5
+            },
+            "sway/workspaces": {
+                "format": "{name}"
+            },
+            "tray": {
+                "icon-size": 16,
+                "spacing": 10
+            }
+          }
+        '';
       };
     };
   };
