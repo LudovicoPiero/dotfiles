680d4b1d4be1eba9758acd936bfc48ed7b81d5eb
Ludovico Piero <lewdovico@gnuweeb.org>
2025-12-01 21:46:31 +0900

    hosts/sforza: enable dev documentation
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- hosts/sforza/configuration.nix
+++ hosts/sforza/configuration.nix
@@ -16,6 +16,17 @@
     ./mine.nix
   ];
 
+  # Enable documentation for developers (includes man 3)
+  documentation = {
+    enable = true;
+    man.generateCaches = true;
+    dev.enable = true;
+  };
+  environment.systemPackages = [
+    pkgs.man-pages
+    pkgs.man-pages-posix
+  ];
+
   services.tailscale.enable = true;
   services.tailscale.useRoutingFeatures = "client";
   systemd.user.services.tailscale-systray = {
