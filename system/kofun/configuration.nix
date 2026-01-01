028909f960686737b406d16a06a8d9479fc94a8f
Ludovico Piero <lewdovico@gnuweeb.org>
2026-01-02 06:41:49 +0900

    system/kofun: add more user groups based on enabled services
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- system/kofun/configuration.nix
+++ system/kofun/configuration.nix
@@ -1,4 +1,10 @@
-{ pkgs, inputs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
 
 {
   imports = [ ./hardware-configuration.nix ];
@@ -61,7 +67,14 @@
   users.users.airi = {
     shell = pkgs.fish;
     isNormalUser = true;
-    extraGroups = [ "wheel" ];
+    extraGroups = [
+      "seat"
+      "video"
+      "wheel"
+    ]
+    ++ lib.optional config.virtualisation.libvirtd.enable "libvirtd"
+    ++ lib.optional config.virtualisation.docker.enable "docker"
+    ++ lib.optional config.networking.networkmanager.enable "networkmanager";
     packages = with pkgs; [
       tmux
       tree
