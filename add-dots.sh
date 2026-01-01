84626e7525bc14dc3cea0ff56bb24f096f9c12e3
Ludovico Piero <lewdovico@gnuweeb.org>
2026-01-01 14:31:07 +0700

    add-dots: support `$HOME` directory
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- add-dots.sh
+++ add-dots.sh
@@ -2,34 +2,43 @@
 
 DOTFILES="$HOME/dotfiles"
 
-# 1. Pick the poison (Corrected)
-# We group the find commands { ...; } and pipe the output to fzf,
-# then capture the FINAL selection into the variable.
-FILE=$({
-  find ~/.config -maxdepth 1 -mindepth 1 -type d
-  find ~ -maxdepth 1 -type f -name ".*"
-} |
-  fzf --prompt="Pick a config to steal: " --height=40% --reverse)
+# 1. Pick the file (Updated to filter out garbage)
+# We exclude history files, Xauthority, and .git folders
+FILE=$( {
+    find ~/.config -maxdepth 1 -mindepth 1 -type d; \
+    find ~ -maxdepth 1 -type f -name ".*" \
+         ! -name .bash_history \
+         ! -name .zsh_history \
+         ! -name .lesshst \
+         ! -name .viminfo \
+         ! -name .Xauthority \
+         ! -name .ICEauthority \
+         ! -name .xsession-errors \
+         ! -name .wget-hsts; \
+    } | fzf --prompt="Pick a config to steal: " --height=40% --reverse )
 
-# Exit if user hits ESC
+# Exit if cancelled
 [ -z "$FILE" ] && exit 0
 
 FILE=$(realpath "$FILE")
 BASENAME=$(basename "$FILE")
 
 # 2. Name the package
+# TIP: If you chose .gtkrc-2.0, type "gtk" here to group them!
 read -e -p "Package name [$BASENAME]: " PKG
 PKG=${PKG:-$BASENAME}
 
 # 3. Calculate paths
 if [[ "$FILE" == *"$HOME/.config"* ]]; then
-  # Keep it inside .config structure
-  DEST_DIR="$DOTFILES/$PKG/.config"
-  FINAL_PATH="$DEST_DIR/$BASENAME"
+    # Case A: Inside .config (e.g. ~/.config/nvim)
+    # Target: ~/dotfiles/nvim/.config/nvim
+    DEST_DIR="$DOTFILES/$PKG/.config"
+    FINAL_PATH="$DEST_DIR/$BASENAME"
 else
-  # It's a root dotfile (like .zshrc)
-  DEST_DIR="$DOTFILES/$PKG"
-  FINAL_PATH="$DEST_DIR/$BASENAME"
+    # Case B: Root dotfile (e.g. ~/.gtkrc-2.0)
+    # Target: ~/dotfiles/gtk/.gtkrc-2.0
+    DEST_DIR="$DOTFILES/$PKG"
+    FINAL_PATH="$DEST_DIR/$BASENAME"
 fi
 
 # 4. Move and Stow
@@ -39,6 +48,6 @@ mv "$FILE" "$FINAL_PATH"
 
 echo "Stowing $PKG..."
 cd "$DOTFILES" || exit
-stow "$PKG"
+stow -R "$PKG"
 
 echo "Done. $PKG is now assimilated."
