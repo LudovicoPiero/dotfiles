64620d93ae170e8f9790ade9ca7af031a51ca2ec
Ludovico Piero <lewdovico@gnuweeb.org>
2026-01-05 03:44:44 +0900

    iosevka-q: 33.3.6 -> 34.0.0
    Diff: https://github.com/be5invis/iosevka/compare/v33.3.6...v34.0.0

--- packages/iosevka-q/default.nix
+++ packages/iosevka-q/default.nix
@@ -91,16 +91,16 @@
 let
   #NOTE: Moved here because newer version of nix-update require to do this
   pname = "Iosevka${toString set}";
-  version = "33.3.6";
+  version = "34.0.0";
 
   src = fetchFromGitHub {
     owner = "be5invis";
     repo = "iosevka";
     rev = "v${version}";
-    hash = "sha256-/Bex4N+3xnYwteO85UaqrIKL5qGnYgSJYO9ET/WEUjM=";
+    hash = "sha256-fASlzL/7pVDIs5wCkEUJaU0r0Gy5YGZ9kxiAskZHWcI=";
   };
 
-  npmDepsHash = "sha256-6TTcXFf9z3ebL4l+++0DS26BJVnwzIi7hU2R1H0DF44=";
+  npmDepsHash = "sha256-uujfgTv2QEhywQNmglZusgikGEZvVtWL/lYFq6Q1VFc=";
 in
 
 assert (privateBuildPlan != null) -> set != null;
