f5505c768d6f29e904294a51bc8602cb3e2214a8
Ludovico Piero <lewdovico@gnuweeb.org>
2026-01-08 15:11:30 +0900

    modules/xdg-portal: fix niri config
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/xdg-portal.nix
+++ modules/xdg-portal.nix
@@ -21,27 +21,26 @@ in
         pkgs.xdg-desktop-portal-gtk # Fallback / File chooser
       ]
       ++ (optional (config.programs.niri.enable or false
-      ) pkgs.xdg-desktop-portal-wlr);
+      ) pkgs.xdg-desktop-portal-gnome);
 
       # Portal Configuration
       config = {
         # 'common' applies to all desktops unless overridden
         common = {
           default = [ "gtk" ];
-          "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
-
-          "org.freedesktop.impl.portal.ScreenCast" = [ "wlr" ];
-          "org.freedesktop.impl.portal.ScreenShot" = [ "wlr" ];
+          "org.freedesktop.impl.portal.Secret" = "gnome-keyring";
         };
 
         niri = {
-          "org.freedesktop.impl.portal.ScreenCast" = [ "wlr" ];
-          "org.freedesktop.impl.portal.ScreenShot" = [ "wlr" ];
+          "org.freedesktop.impl.portal.Access" = "gtk";
+          "org.freedesktop.impl.portal.FileChooser" = "gtk";
+          "org.freedesktop.impl.portal.Notification" = "gtk";
+          "org.freedesktop.impl.portal.Secret" = "gnome-keyring";
         };
 
         hyprland = {
-          "org.freedesktop.impl.portal.ScreenCast" = [ "hyprland" ];
-          "org.freedesktop.impl.portal.ScreenShot" = [ "hyprland" ];
+          "org.freedesktop.impl.portal.ScreenCast" = "hyprland";
+          "org.freedesktop.impl.portal.ScreenShot" = "hyprland";
         };
       };
     };
