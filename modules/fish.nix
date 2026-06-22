b737fea93efd164aabeec09deb872dded4083174
Ludovico Piero <lewdovico@gnuweeb.org>
2026-06-22 22:01:15 +0700

    fish: add cool stuff
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- modules/fish.nix
+++ modules/fish.nix
@@ -1,16 +1,50 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }:
+let
+  _ = lib.getExe;
+in
+{
   programs.fish.enable = true;
   users.users.rei.shell = pkgs.fish;
 
   hm = {
+    home.packages = [ pkgs.zoxide ];
     programs.fish = {
       enable = true;
+
+      interactiveShellInit = ''
+        # Disable greeting
+        function fish_greeting
+        end
+
+        set -gx EDITOR nvim
+
+        ${_ pkgs.starship} init fish | source
+        ${_ pkgs.nix-your-shell} fish | source
+        ${_ pkgs.zoxide} init fish | source
+        ${_ pkgs.direnv} hook fish | source
+
+        set -p fish_function_path ${pkgs.fishPlugins.fzf-fish}/share/fish/vendor_functions.d
+        set -p fish_complete_path ${pkgs.fishPlugins.fzf-fish}/share/fish/vendor_completions.d
+        source ${pkgs.fishPlugins.fzf-fish}/share/fish/vendor_conf.d/fzf.fish
+
+        if functions -q fzf_configure_bindings
+          fzf_configure_bindings --directory=\eF --git_log=\cg --history=\cr --variables=\cv
+        end
+
+        # Key bindings (Old fish behavior)
+        bind alt-backspace backward-kill-word
+        bind ctrl-alt-h backward-kill-word
+        bind ctrl-backspace backward-kill-token
+        bind alt-delete kill-word
+        bind ctrl-delete kill-token
+      '';
 
       shellAliases = {
         v = "nvim";
         c = "cd $HOME/Code/dotfiles/";
 
         l = "${pkgs.eza}/bin/eza --icons=always --git -l";
+        t = "${pkgs.eza}/bin/eza --icons=always --git --tree";
         ls = "${pkgs.eza}/bin/eza --icons=always --git";
         la = "${pkgs.eza}/bin/eza --icons=always --git -a";
         ll = "${pkgs.eza}/bin/eza --icons=always --git -l";
