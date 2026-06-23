9982f98cee26d03cd9d0ddcdb254b8daff28d5ad
Ludovico Piero <lewdovico@gnuweeb.org>
2026-06-24 01:35:51 +0700

    flake: use unstable
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS Configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
 
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
@@ -10,7 +10,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
